jnorthrup/jmodelica

commit f246c97c1c91979fb8db7d5736929aaefc1d1a17

Added docstrings for the Ipopt initialization algorithm, see #1068.
git-svn-id: https://svn.jmodelica.org@2045 7ae51c65-13d5-4921-9d80-6511ddd1419c

trunk/Python/src/jmodelica/algorithm_drivers.py
@@ -429,15 +429,58 @@ class IpoptInitResult(JMResultBase):
     pass
 
 class IpoptInitializationAlgOptions(OptionBase):
-    """ Options for the IpoptInitialization initialize algorithm. 
+    """
+    Options for the IPOPT-based initialization algorithm.
 
-            write_scaled_result --
-                Set this parameter to True to write the result to file without
-                taking scaling into account. If the value of scaled is False,
-                then the variable scaling factors of the model are used to
-                reproduced the unscaled variable values.
-                Default: False
+    Initialization algorithm options::
 
+        stat --
+            Solve a static optimization problem.
+            Default: False
+
+        result_file_name --
+            Specifies the name of the file where the optimization
+            result is written. Setting this option to an empty
+            string results in a default file name that is based
+            on the name of the optimization class.
+            Default: Empty string
+            
+        result_format --
+            Specifies in which format to write the result. Currently
+            only textual mode is supported.
+            Default: 'txt'
+
+        write_scaled_result --
+            Set this parameter to True to write the result to file without
+            taking scaling into account. If the value of scaled is False,
+            then the variable scaling factors of the model are used to
+            reproduced the unscaled variable values.
+            Default: False
+
+    Options are set by using the syntax for dictionaries::
+
+        >>> opts = my_model.initialize_options()
+        >>> opts['stat'] = True
+        
+    In addition, IPOPT options can be provided in the option
+    IPOPT_options. For a complete list of IPOPT options, please
+    consult the IPOPT documentation available at
+    http://www.coin-or.org/Ipopt/documentation/).
+
+    Some commonly used IPOPT options are provided by default::
+
+        max_iter --
+           Maximum number of iterations.
+           Default: 3000
+                      
+        derivative_test --
+           Check the correctness of the NLP derivatives. Valid values are
+           'none', 'first-order', 'second-order', 'only-second-order'.
+           Default: 'none'
+
+    IPOPT options are set using the syntax for dictionaries::
+
+        >>> opts['IPOPT_options']['max_iter'] = 200
     """
     def __init__(self, *args, **kw):
         _defaults= {
@@ -445,7 +488,8 @@ class IpoptInitializationAlgOptions(OptionBase):
             'result_file_name':'', 
             'result_format':'txt',
             'write_scaled_result':False,
-            'IPOPT_options':{}
+            'IPOPT_options':{'max_iter':3000,
+                             'derivative_test':'none'
             }
         super(IpoptInitializationAlgOptions,self).__init__(_defaults)
         # for those key-value-sets where the value is a dict, don't 
@@ -457,7 +501,8 @@ class IpoptInitializationAlg(AlgorithmBase):
     """ Initialization of a model using Ipopt. """
     
     def __init__(self, model, options):
-        """ Create an initialization algorithm using IpoptInitialization.
+        """
+        Create an initialization algorithm using IpoptInitialization.
         
         Parameters::
         
@@ -474,10 +519,9 @@ class IpoptInitializationAlg(AlgorithmBase):
                 * help(jmodelica.algorithm_drivers.IpoptInitializationAlgOptions)
                 
                 Valid values are: 
-                - A dict which gives IpoptInitializationAlgOptions with 
-                  default values on all options except the ones listed 
-                  in the dict. Empty dict will thus give all options 
-                  with default values.
+                - A dict that overrides some or all of the default values
+                  provided by CollocationLagrangePolynomialsAlgOptions. An empty
+                  dict will thus give all options with default values.
                 - IpoptInitializationAlgOptions object.
         """
         self.model = model
@@ -662,10 +706,9 @@ class AssimuloFMIAlg(AlgorithmBase):
                 * help(jmodelica.algorithm_drivers.AssimuloFMIAlgOptions)
                 
                 Valid values are: 
-                - A dict which gives AssimuloFMIAlgOptions with default 
-                  values on all options except the ones listed in the 
-                  dict. Empty dict will thus give all options with 
-                  default values.
+                - A dict that overrides some or all of the default values
+                  provided by CollocationLagrangePolynomialsAlgOptions. An empty
+                  dict will thus give all options with default values.
                 - AssimuloFMIAlgOptions object.
         """
         self.model = model
@@ -1185,8 +1228,6 @@ class CollocationLagrangePolynomialsAlg(AlgorithmBase):
     The algorithm is based on orthogonal collocation and
     relies on the solver IPOPT for solving a non-linear programming
     problem. 
-
-    Optimization algorithm using CollocationLagrangePolynomials method.
     """
     
     def __init__(self, 

trunk/docbook/UsersGuide/Optimization.xml
@@ -470,6 +470,16 @@ x1 = res['x1']</programlisting>
 
                 <entry>'txt'</entry>
               </row>
+
+              <row>
+                <entry><literal>write_scaled_result</literal></entry>
+
+                <entry>Write the scaled optimization result if set to true.
+                This option is only applicable when automatic variable scaling
+                is enabled. Only for debugging use.</entry>
+
+                <entry>False</entry>
+              </row>
             </tbody>
           </tgroup>
         </table>
@@ -1960,7 +1970,7 @@ plt.show()
     pumps.</para>
 
     <para>The Modelica model for the system is located in <link
-    xlink:href="https://svn.jmodelica.org/tags/1.3b1/Python/src/jmodelica/examples/files/QuadTankPack.mo">QuadTankPack.mo</link>.
+    xlink:href="https://svn.jmodelica.org/tags/1.3b1/Python/src/jmodelica/examples/files/QuadTankPack.mo">QuadTankPack.mop</link>.
     Download the file to your working directory and open it in a text editor.
     Locate the class <literal>QuadTankPack.QuadTank</literal> and make sure
     you understand the model. In particular, notice that all model variables
@@ -1977,9 +1987,10 @@ plt.show()
     <programlisting>from scipy.io.matlab.mio import loadmat
 import matplotlib.pyplot as plt
 import numpy as N
-from jmodelica import simulate
-from jmodelica import optimize
-from jmodelica.compiler import OptimicaCompiler</programlisting>
+
+from jmodelica.jmi import compile_jmu
+from jmodelica.jmi import JMUModel
+</programlisting>
 
     <para>into the file. Next, we enter code to open the data file, extract
     the measurement time series and plot the measurements:</para>
@@ -2035,26 +2046,36 @@ plt.grid()
 plt.show()  </programlisting>
 
     <para>You should now see two plots showing the measurement state profiles
-    and the control input profiles:</para>
+    and the control input profiles similar to <xref
+    linkend="opt-fig-qt_data" /> and <xref
+    linkend="opt-fig-qtinput" />.</para>
 
-    <figure>
-      <title>Measured state profiles</title>
+    <figure xml:id="opt-fig-qt_data">
+      <title></title>
 
       <mediaobject>
         <imageobject>
           <imagedata fileref="images/qt_x.svg" scalefit="1" width="60%"></imagedata>
         </imageobject>
       </mediaobject>
+
+      <caption>
+        <para>Measured state profiles.</para>
+      </caption>
     </figure>
 
-    <figure>
-      <title>Input profiles used in estimation experiment</title>
+    <figure xml:id="opt-fig-qtinput">
+      <title></title>
 
       <mediaobject>
         <imageobject>
           <imagedata fileref="images/qt_u.svg" scalefit="1" width="60%"></imagedata>
         </imageobject>
       </mediaobject>
+
+      <caption>
+        <para>Control inputs used in the identification experiment.</para>
+      </caption>
     </figure>
 
     <para>In order to evaluate the accuracy of nominal model parameter values,

commit 23deeed29d03f7a489d77efae1807f8739797ea5

Fixed minor error introduced in r2045.

trunk/Python/src/jmodelica/algorithm_drivers.py
@@ -489,7 +489,7 @@ class IpoptInitializationAlgOptions(OptionBase):
             'result_format':'txt',
             'write_scaled_result':False,
             'IPOPT_options':{'max_iter':3000,
-                             'derivative_test':'none'
+                             'derivative_test':'none'}
             }
         super(IpoptInitializationAlgOptions,self).__init__(_defaults)
         # for those key-value-sets where the value is a dict, don't 