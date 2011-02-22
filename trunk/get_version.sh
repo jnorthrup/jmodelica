jnorthrup/jmodelica

commit 25838bb677a33c29ef4ec7173b523b4b17a5dedf

Updated get_version.sh to be much faster.
git-svn-id: https://svn.jmodelica.org@2398 7ae51c65-13d5-4921-9d80-6511ddd1419c

trunk/get_version.sh
@@ -6,7 +6,7 @@ case ${TYPE} in
         echo ${DIR} | cut -d/ -f2
         ;;
     trunk)
-        svnversion $1 | sed -e 's/^.*://' -e 's/[^0-9]*//g' -e 's/^/r/'
+        svn info | sed -n -e 's/Revision: \(\|[0-9]*:\)\([0-9]*\)$/r\2/p'
         ;;
     *)
         echo unknown

commit 205c2190a80521db596856997a2543f8fa7fe58c

Fixed minor bug introduced in r2398.

trunk/get_version.sh
@@ -6,7 +6,7 @@ case ${TYPE} in
         echo ${DIR} | cut -d/ -f2
         ;;
     trunk)
-        svn info | sed -n -e 's/Revision: \(\|[0-9]*:\)\([0-9]*\)$/r\2/p'
+        svn info $1 | sed -n -e 's/Revision: \(\|[0-9]*:\)\([0-9]*\)$/r\2/p'
         ;;
     *)
         echo unknown