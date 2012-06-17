jnorthrup/jmodelica

commit 3b571e4db254a4135ea51dffb3fba4a37428fce1

Support for temporary directory creation implemented, see  #2048
git-svn-id: https://svn.jmodelica.org@3550 7ae51c65-13d5-4921-9d80-6511ddd1419c

FMILibrary/trunk/src/Util/src/JM/jm_portability.c
@@ -14,10 +14,11 @@
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
+#include <stdlib.h>
 #include <stdio.h>
 
-#include "JM/jm_types.h"
-#include "JM/jm_portability.h"
+#include <JM/jm_types.h>
+#include <JM/jm_portability.h>
 
 #ifdef WIN32
 #include <direct.h>
@@ -104,4 +105,66 @@ jm_status_enu_t jm_portability_set_current_working_directory(char* cwd)
 	} else {
 		return jm_status_error;
 	}
+}
+
+#ifdef WIN32
+#define MAX_TEMP_DIR_NAME_LENGTH 262
+TCHAR jm_temp_dir_buffer[MAX_TEMP_DIR_NAME_LENGTH];
+#endif
+
+const char* jm_get_system_temp_dir() {
+#ifdef WIN32
+	if(!GetTempPath(MAX_TEMP_DIR_NAME_LENGTH, jm_temp_dir_buffer)) return 0;
+	return jm_temp_dir_buffer;
+#else
+	return "/tmp/"
+#endif
+}
+
+#ifdef WIN32
+#include <io.h>
+#define mktemp _mktemp
+#endif
+ 
+char* jm_mktemp(char* tmplt) {
+	return mktemp(tmplt);
+}
+
+#ifdef WIN32
+#include <direct.h>
+#define MKDIR(dir) _mkdir(dir)
+#else
+#include <sys/stat.h>
+#define MKDIR(dir) mkdir(dir, S_IRWXU | S_IRWXG | S_IROTH | S_IXOTH)
+#endif
+
+jm_status_enu_t jm_mkdir(jm_callbacks* cb, const char* dir) {
+	if(MKDIR(dir)) {
+		jm_log_fatal(cb,"JMPRT","Could not create directory %s", dir);
+		return jm_status_error;
+	}
+	else
+		return jm_status_success;
+}
+
+
+jm_status_enu_t jm_rmdir(jm_callbacks* cb, const char* dir) {
+#if WIN32
+	const char* fmt_cmd = "rmdir /s /q %s";
+#else
+    const char* fmt_cmd = "rm -rf %s";
+#endif
+    char * buf = (char*)cb->calloc(sizeof(char), strlen(dir)+strlen(fmt_cmd)+1);
+	if(!buf) {
+	    jm_log_error(cb,"JMPRT","Could not allocate memory");
+		return jm_status_error;
+	}
+    sprintf(buf, fmt_cmd, dir);
+    jm_log_verbose(cb,"JMPRT","Removing %s", dir);
+    if(system(buf)) {
+	    jm_log_error(cb,"JMPRT","Error removing %s", dir);
+		return jm_status_error;
+	}
+    cb->free(buf);
+	return jm_status_success;
 }

FMILibrary/trunk/CMakeLists.txt
@@ -228,7 +228,7 @@ if(FMILIB_GENERATE_BUILD_STAMP)
 		COMMAND ${CMAKE_COMMAND}  -E touch ${FMILibrary_BINARY_DIR}/config_fmilib.c
 		DEPENDS ${FMILIB_SUBLIBS}
 	)
-	add_library(fmilib_timestamp STATIC ${FMILibrary_BINARY_DIR}/config_fmilib.c "${FMILibrary_BINARY_DIR}/config_fmilib.h")
+	add_library(fmilib_timestamp STATIC ${FMILibrary_BINARY_DIR}/config_fmilib.c "${FMILibrary_BINARY_DIR}/fmilib_config.h")
 #	add_dependencies(fmilib_timestamp ${FMILIB_SUBLIBS} expat minizip zlib ${FMILIBRARYHOME}/Config.cmake/config_stamp.cmake)
 	
 	set(FMILIB_SUBLIBS ${FMILIB_SUBLIBS} fmilib_timestamp)

FMILibrary/trunk/Config.cmake/config_fmilib.c.cmake
@@ -23,6 +23,5 @@ const char * fmilib_build_stamp =
 
 	
 const char* fmilib_get_build_stamp(void) {
-	FMILIB_TRACE("Inside fmilib_get_build_stamp\n");
 	return fmilib_build_stamp;
 }

FMILibrary/trunk/src/Util/include/JM/jm_portability.h
@@ -17,6 +17,7 @@
 #ifndef JM_PORTABILITY_H_
 #define JM_PORTABILITY_H_
 #include <fmilib_config.h>
+#include "jm_callbacks.h"
 
 /* Include platform dependent headers */
 #if defined(_MSC_VER) || defined(WIN32)
@@ -39,9 +40,10 @@
 */
 /** \addtogroup jm_portability Handling platform specific defines and functions
 @{*/
-/** Load a dll/so library into the process and return a handle. */
+/** \brief Load a dll/so library into the process and return a handle. */
 DLL_HANDLE		jm_portability_load_dll_handle		(const char* dll_file_path);
-/** Unload a Dll and release the handle*/
+
+/** \brief Unload a Dll and release the handle*/
 jm_status_enu_t jm_portability_free_dll_handle		(DLL_HANDLE dll_handle);
 
 /** \brief A function pointer as returned when DLL symbol is loaded.*/
@@ -51,16 +53,38 @@ jm_status_enu_t jm_portability_free_dll_handle		(DLL_HANDLE dll_handle);
 typedef void* jm_dll_function_ptr; 
 #endif
 
-/** Find a function in the Dll and return a function pointer */
+/** \brief Find a function in the Dll and return a function pointer */
 jm_status_enu_t jm_portability_load_dll_function	(DLL_HANDLE dll_handle, char* dll_function_name, jm_dll_function_ptr* dll_function_ptrptr);
-/** Return error associated with Dll handling */
-char*			jm_portability_get_last_dll_error	(void);
 
-/** Get current working directory name */
+/** \brief Return error associated with Dll handling */
+char* jm_portability_get_last_dll_error	(void);
+
+/** \brief Get current working directory name */
 jm_status_enu_t jm_portability_get_current_working_directory(char* buffer, size_t len);
 
-/** Set current working directory*/
+/** \brief Set current working directory*/
 jm_status_enu_t jm_portability_set_current_working_directory(char* cwd);
+
+/** \brief Get system-wide temporary directory */
+const char* jm_get_system_temp_dir();
+
+/** 
+	\brief Create a unique file name 
+	\param tmplt File name template ending with XXXXXX.
+	\return A pointer to the modified template. The function returns NULL if 
+	template is badly formed or no more unique names can be created from the given template.
+*/
+char* jm_mktemp(char* tmplt);
+
+/**
+	\brief Make a directory.
+*/
+jm_status_enu_t jm_mkdir(jm_callbacks* cb, const char* dir);
+
+/**
+\brief Remove directory and all it contents.
+*/
+jm_status_enu_t jm_rmdir(jm_callbacks* cb, const char* dir);
 
 /*@}*/
 #endif /* End of header file JM_PORTABILITY_H_ */

commit 540f85aa38221df77db81b576fe1418c6602fb81

Fixed a bug on Linux introduced in r3550, see  #2048

FMILibrary/trunk/src/Util/src/JM/jm_portability.c
@@ -16,6 +16,9 @@
 
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
+
+#include <fmilib_config.h>
 
 #include <JM/jm_types.h>
 #include <JM/jm_portability.h>
@@ -117,13 +120,15 @@ const char* jm_get_system_temp_dir() {
 	if(!GetTempPath(MAX_TEMP_DIR_NAME_LENGTH, jm_temp_dir_buffer)) return 0;
 	return jm_temp_dir_buffer;
 #else
-	return "/tmp/"
+	return "/tmp/";
 #endif
 }
 
 #ifdef WIN32
 #include <io.h>
 #define mktemp _mktemp
+#else
+char* mktemp(char*);
 #endif
  
 char* jm_mktemp(char* tmplt) {