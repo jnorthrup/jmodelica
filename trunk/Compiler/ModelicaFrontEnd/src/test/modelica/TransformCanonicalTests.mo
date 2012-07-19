jnorthrup/jmodelica

commit 7ca035bccc1d920bc3df4472437e788f6173b8f3

Updated the handing of if equations with parameter guard expressions, so that such constructs are handled by branch elimination rather than conversion to if expressions. No tests failed with this update, which means that we could improve coverage. Remains to add tests and review update, see #2092.
git-svn-id: https://svn.jmodelica.org@3649 7ae51c65-13d5-4921-9d80-6511ddd1419c

trunk/Compiler/ModelicaFrontEnd/src/jastadd/ContentsCheck.jadd
@@ -153,15 +153,13 @@ aspect ContentCheck {
 			boolean inWhen = inWhen();
 			boolean balanced = inWhen ? isBalancedAssignments() : isBalancedEquations();
 			boolean functionCallEqu = hasFunctionCallEquations(); 
-			if (!balanced || functionCallEqu) {
-				if (hasOnlyParamTests())
-					setEliminateOnParamTest();
-				else if (!balanced)
-					error(inWhen ? UNBALANCED_ERROR_WHEN : UNBALANCED_ERROR);
-				else
-					compliance("If equations that has non-parameter tests and contains " + 
-							"function calls using multiple outputs are not supported");
-			}
+			if (hasOnlyParamTests())
+				setEliminateOnParamTest();
+			else if (!balanced)
+				error(inWhen ? UNBALANCED_ERROR_WHEN : UNBALANCED_ERROR);
+			if (functionCallEqu)
+				compliance("If equations that has non-parameter tests and contains " + 
+						"function calls using multiple outputs are not supported");
 		}
 	}
 	

commit 867f5068ef69550ad73ae9effb60c53f4d640988

Fixed a bug introduced in r3649 and added a test, see #2092.

trunk/Compiler/ModelicaFrontEnd/src/jastadd/ContentsCheck.jadd
@@ -153,13 +153,16 @@ aspect ContentCheck {
 			boolean inWhen = inWhen();
 			boolean balanced = inWhen ? isBalancedAssignments() : isBalancedEquations();
 			boolean functionCallEqu = hasFunctionCallEquations(); 
-			if (hasOnlyParamTests())
+			if (hasOnlyParamTests()) {
 				setEliminateOnParamTest();
-			else if (!balanced)
-				error(inWhen ? UNBALANCED_ERROR_WHEN : UNBALANCED_ERROR);
-			if (functionCallEqu)
-				compliance("If equations that has non-parameter tests and contains " + 
+			} else {
+				if (!balanced) {
+					error(inWhen ? UNBALANCED_ERROR_WHEN : UNBALANCED_ERROR);
+				} else if (functionCallEqu) {
+					compliance("If equations that has non-parameter tests and contains " + 
 						"function calls using multiple outputs are not supported");
+				}
+			}
 		}
 	}
 	

trunk/Compiler/ModelicaFrontEnd/src/test/modelica/TransformCanonicalTests.mo
@@ -3536,6 +3536,35 @@ equation
     end if;
 end IfEqu23;
 
+model IfEqu24  "Test delay equation"
+	 annotation(JModelica(unitTesting = JModelica.UnitTesting(testCase={
+     JModelica.UnitTesting.TransformCanonicalTestCase(
+         name="IfEqu24",
+         description="Check correct elimination of if equation branches.",
+         flatModel="
+fclass TransformCanonicalTests.IfEqu24
+ parameter Boolean use_delay = false /* false */;
+ Real x1(start = 1);
+ Real x2(start = 1);
+initial equation 
+ x1 = 1;
+equation
+ der(x1) = sin(time);
+ 0 = x1 - ( x2 ) + 2;
+end TransformCanonicalTests.IfEqu24;
+")})));
+		
+  parameter Boolean use_delay=false;
+  Real x1(start = 1); 
+  Real x2(start = 1);
+equation
+  der(x1) = sin(time);
+  if use_delay then
+    der(x2) = (x1 - x2) /100;
+  else
+    0 = x1 - x2 + 2;
+  end if;
+end IfEqu24;
 
 model IfExpLeft1
  annotation(JModelica(unitTesting = JModelica.UnitTesting(testCase={