jnorthrup/jmodelica

commit a87ddd9e08d7bc6a02b7c5df8add8adb4820fbc7

#3938 Partial variability propagation.
git-svn-id: https://svn.jmodelica.org@6805 7ae51c65-13d5-4921-9d80-6511ddd1419c

trunk/Compiler/ModelicaMiddleEnd/src/jastadd/optimizations/VariabilityPropagation.jrag
@@ -19,19 +19,60 @@ aspect VariabilityPropagation {
 	
 	public boolean FAbstractEquation.taggedForRemoval = false;
 	public boolean FAbstractVariable.taggedForRemoval = false;
+	public boolean FFunctionCallLeft.taggedForRemoval = false;
+	
+    public void FAbstractEquation.cleanEquations(ArrayList<FAbstractEquation> l) {
+        if (!taggedForRemoval) {
+            l.add(this);
+        }
+    }
+    public void FFunctionCallEquation.cleanEquations(ArrayList<FAbstractEquation> l) {
+        super.cleanEquations(l);
+        for (FFunctionCallLeft left : getLefts()) {
+            left.cleanEquations();
+        }
+    }
+    public void FFunctionCallLeft.cleanEquations() {
+        if (taggedForRemoval) {
+            setFExpOpt(new Opt());
+            taggedForRemoval = false;
+        }
+    }
+    
 	
 	/**
 	 * Checks if equation is eligible for var. prop.
 	 */
-	inh boolean FAbstractEquation.canPropagate();
-	eq Root.getChild().canPropagate() = false;
-	eq FClass.getFAbstractEquation().canPropagate() = true;
-	eq FIfWhenElseEquation.getChild().canPropagate() = false;
-	
-	syn boolean FEquation.canPropagate() =
-			super.canPropagate() && variables().size() == 1 && !containsActiveAnnotations();
-	syn boolean FFunctionCallEquation.canPropagate() =
-			super.canPropagate() && variables().size() > 0 && variables().size() == numScalarEquations() && !containsActiveAnnotations();
+    syn boolean FAbstractEquation.canPropagate() =
+            !inConditionalClause() && !containsActiveAnnotations();
+    eq FEquation.canPropagate() = super.canPropagate() && variables().size() == 1;
+    eq FFunctionCallEquation.canPropagate() {
+        getCall().flushAllRecursiveClearFinal();
+        return super.canPropagate() && variables().size() > 0 && numScalarEquations() > 0 && 
+                ((variables().size() == numScalarEquations() && getCall().variability().parameterOrLess()) 
+                        || potentialPartialVariability().constantVariability());
+    }
+    
+    inh boolean FAbstractEquation.inConditionalClause();
+    eq Root.getChild().inConditionalClause() = false;
+    eq FClass.getFAbstractEquation().inConditionalClause() = false;
+    eq FIfWhenElseEquation.getChild().inConditionalClause() = true;
+    
+    /**
+     * If any arg is constant return constant. Otherwise returns combined variability.
+     */
+    syn FTypePrefixVariability FFunctionCallEquation.potentialPartialVariability() {
+        FTypePrefixVariability res = fContinuous(); 
+        if (getCall() instanceof FFunctionCall) {
+            for (FExp exp : ((FFunctionCall)getCall()).getArgs()) {
+                if (exp.variability().constantVariability()) {
+                    return fConstant();
+                }
+            }
+            res = getCall().variability();
+        }
+        return res;
+    }
 	
 	/**
 	 * Checks if left hand side variable(s) is eligible for var. prop.
@@ -128,9 +169,7 @@ aspect VariabilityPropagation {
 		// Clean equations
 		ArrayList<FAbstractEquation> newEquations = new ArrayList<FAbstractEquation>();
 		for (FAbstractEquation equation: equations) {
-			if (!equation.taggedForRemoval) {
-				newEquations.add(equation);
-			}
+			equation.cleanEquations(newEquations);
 		}
 		setFAbstractEquationList(new List<FAbstractEquation>(newEquations));
 		
@@ -208,51 +247,61 @@ aspect VariabilityPropagation {
 	public void FFunctionCallEquation.variabilityPropagation(
 			LinkedHashSet<FAbstractEquation> worklist,
 			List<FAbstractEquation> parameterEquations) {
-		
-		FTypePrefixVariability callVariability = getCall().variability();
-		FExp literals[] = null;
-	
-		if (!canPropagate()) {
-			return;
-		}
-		
-		for (FFunctionCallLeft left : getLefts()) {
-			if (left.hasFExp() && !left.getFExp().canPropagate()) {
-				return;
-			}
-		}
-		
-		if (!callVariability.parameterOrLess()) {
-			return;
-		}
-		
-		if (callVariability.constantVariability()) {
-			try {
-				CValue[] val = getCall().cevalFunction(defaultVariableEvaluator());
-				literals = new FExp[val.length];
-				for (int i = 0; i < val.length && i < getNumLeft(); i++)
-					if (getLeft(i).hasFExp())
-						literals[i] = val[i].buildLiteral();
-			} catch (ConstantEvaluationException e) {
-				// If we can't evaluate right now, set it as parameter
-				callVariability = fParameter();
-			}
-		}
-
-		if (callVariability.parameterVariability()) {
-			parameterEquations.add(this);
-		}
-		
-		this.taggedForRemoval = true;
-		int i = 0;
-		for (FFunctionCallLeft left : getLefts()) {
-			if (left.hasFExp()) {
-				left.getFExp().variabilityPropagation(worklist, callVariability, 
-						callVariability.constantVariability() ? literals[i] : null, parameterEquations);				
-			}
-			i++;
-		}		
-	}
+        
+        if (!canPropagate()) {
+            return;
+        }
+        
+        boolean canBeParam = getCall().variability().parameterOrLess();
+        for (FFunctionCallLeft left : getLefts()) {
+            if (left.hasFExp() && !left.getFExp().canPropagate()) {
+                canBeParam = false;
+            }
+        }
+        FTypePrefixVariability variability = potentialPartialVariability();
+        boolean eval = variability.constantVariability();
+        
+        CValue[] val = null;
+        if (eval) {
+            val = getCall().evaluateSafe(defaultVariableEvaluator());
+        }
+        
+        boolean allRemoved = true;
+        int i = 0;
+        for (FFunctionCallLeft left : getLefts()) {
+            if (left.hasFExp()) {
+                if (left.getFExp().canPropagate()) {
+                    if (eval) {
+                        try {
+                            left.getFExp().variabilityPropagation(worklist, variability, val[i].buildLiteral(), parameterEquations);
+                            left.taggedForRemoval = true;
+                        } catch (ConstantEvaluationException e) {
+                            
+                        }
+                    }
+                    
+                    if (!left.taggedForRemoval) {
+                        if (canBeParam) {
+                            left.getFExp().variabilityPropagation(worklist, fParameter(), null, parameterEquations);
+                        }
+                        allRemoved = false;
+                    }
+                } else {
+                    allRemoved = false;
+                }
+            }
+            i++;
+        }
+        
+        if (allRemoved) {
+            this.taggedForRemoval = true;
+        } else {
+            if (canBeParam) {
+                parameterEquations.add(this);
+                this.taggedForRemoval = true;
+            }
+        }
+    }
 	
 	public void FExp.variabilityPropagation(
 			LinkedHashSet<FAbstractEquation> worklist,

trunk/Compiler/ModelicaMiddleEnd/src/test/VariabilityPropagationTests.mo
@@ -326,9 +326,9 @@ equation
 			flatModel="
 fclass VariabilityPropagationTests.Output2
  output Real a;
- Real b;
+ constant Real b = 2;
 equation
- (a, b) = VariabilityPropagationTests.Output2.f();
+ (a, ) = VariabilityPropagationTests.Output2.f();
 
 public
  function VariabilityPropagationTests.Output2.f
@@ -725,6 +725,7 @@ equation
 Tests that parameters in function call equations are folded. 
 Also tests that when it is constant and can't evaluate, variability is propagated as parameter.
 ",
+            inline_functions="none",
 			flatModel="
 fclass VariabilityPropagationTests.FunctionCallEquation4
  constant Real a[1,1] = 1;
@@ -802,6 +803,7 @@ model FunctionCallEquation5
 		TransformCanonicalTestCase(
 			name="FunctionCallEquation5",
 			description="Tests evaluation of matrix multiplication in function.",
+			inline_functions="none",
 			flatModel="
 fclass VariabilityPropagationTests.FunctionCallEquation5
  constant Real a[1,1] = 1;
@@ -815,6 +817,267 @@ fclass VariabilityPropagationTests.FunctionCallEquation5
 end VariabilityPropagationTests.FunctionCallEquation5;
 ")})));
 end FunctionCallEquation5;
+
+    function fp
+        input Real i1;
+        input Real i2;
+        output Real o1 = i1;
+        output Real o2 = i2;
+    algorithm
+    end fp;
+
+model FunctionCallEquationPartial1
+    Real x1,x2;
+  equation
+    (x1,x2) = fp(time,7);
+    
+    annotation(__JModelica(UnitTesting(tests={
+        TransformCanonicalTestCase(
+            name="FunctionCallEquationPartial1",
+            description="Tests evaluation of matrix multiplication in function.",
+            inline_functions="none",
+            flatModel="
+fclass VariabilityPropagationTests.FunctionCallEquationPartial1
+ Real x1;
+ constant Real x2 = 7;
+equation
+ (x1, ) = VariabilityPropagationTests.fp(time, 7);
+
+public
+ function VariabilityPropagationTests.fp
+  input Real i1;
+  input Real i2;
+  output Real o1;
+  output Real o2;
+ algorithm
+  o1 := i1;
+  o2 := i2;
+  return;
+ end VariabilityPropagationTests.fp;
+
+end VariabilityPropagationTests.FunctionCallEquationPartial1;
+")})));
+end FunctionCallEquationPartial1;
+
+model FunctionCallEquationPartial2
+    Real x1,x2,x3;
+  equation
+    x3 = 7;
+    (x1,x2) = fp(time,x3);
+    
+    annotation(__JModelica(UnitTesting(tests={
+        TransformCanonicalTestCase(
+            name="FunctionCallEquationPartial2",
+            description="Tests evaluation of matrix multiplication in function.",
+            inline_functions="none",
+            flatModel="
+fclass VariabilityPropagationTests.FunctionCallEquationPartial2
+ Real x1;
+ constant Real x2 = 7.0;
+ constant Real x3 = 7;
+equation
+ (x1, ) = VariabilityPropagationTests.fp(time, 7.0);
+
+public
+ function VariabilityPropagationTests.fp
+  input Real i1;
+  input Real i2;
+  output Real o1;
+  output Real o2;
+ algorithm
+  o1 := i1;
+  o2 := i2;
+  return;
+ end VariabilityPropagationTests.fp;
+
+end VariabilityPropagationTests.FunctionCallEquationPartial2;
+")})));
+end FunctionCallEquationPartial2;
+
+model FunctionCallEquationPartial3
+    Real x1,x2,x3;
+  equation
+    (x1,x2) = fp(time,x3);
+    x3 = 7;
+    
+    annotation(__JModelica(UnitTesting(tests={
+        TransformCanonicalTestCase(
+            name="FunctionCallEquationPartial3",
+            description="Tests evaluation of matrix multiplication in function.",
+            inline_functions="none",
+            flatModel="
+fclass VariabilityPropagationTests.FunctionCallEquationPartial3
+ Real x1;
+ constant Real x2 = 7.0;
+ constant Real x3 = 7;
+equation
+ (x1, ) = VariabilityPropagationTests.fp(time, 7.0);
+
+public
+ function VariabilityPropagationTests.fp
+  input Real i1;
+  input Real i2;
+  output Real o1;
+  output Real o2;
+ algorithm
+  o1 := i1;
+  o2 := i2;
+  return;
+ end VariabilityPropagationTests.fp;
+
+end VariabilityPropagationTests.FunctionCallEquationPartial3;
+")})));
+end FunctionCallEquationPartial3;
+
+model FunctionCallEquationPartial4
+    Real x1,x2,x3,x4,x5;
+  equation
+    (x1,x2) = fp(x4,x5);
+    (x3,x4) = fp(x1,x2);
+    x5 = 7;
+    
+    annotation(__JModelica(UnitTesting(tests={
+        TransformCanonicalTestCase(
+            name="FunctionCallEquationPartial4",
+            description="Tests evaluation of matrix multiplication in function.",
+            inline_functions="none",
+            flatModel="
+fclass VariabilityPropagationTests.FunctionCallEquationPartial4
+ constant Real x1 = 7.0;
+ constant Real x2 = 7.0;
+ constant Real x3 = 7.0;
+ constant Real x4 = 7.0;
+ constant Real x5 = 7;
+end VariabilityPropagationTests.FunctionCallEquationPartial4;
+")})));
+end FunctionCallEquationPartial4;
+
+model FunctionCallEquationPartial5
+    function fp
+        input Real i1;
+        input Real i2;
+        input Real i3;
+        input Real i4 = 13;
+        output Real o1 = i1;
+        output Real o2 = i2;
+        output Real o3 = i3;
+        output Real o4 = i4;
+    algorithm
+    end fp;
+    Real x1,x2,x3,x4,x5,x6;
+  equation
+    (x1,x2,x3) = fp(x4, x5, x6);
+    x4 = 3;
+    x5 = x4 + x1;
+    x6 = x4 + x1 + x2;
+    
+    annotation(__JModelica(UnitTesting(tests={
+        TransformCanonicalTestCase(
+            name="FunctionCallEquationPartial5",
+            description="Tests evaluation of matrix multiplication in function.",
+            inline_functions="none",
+            flatModel="
+fclass VariabilityPropagationTests.FunctionCallEquationPartial5
+ constant Real x1 = 3.0;
+ constant Real x2 = 6.0;
+ constant Real x3 = 12.0;
+ constant Real x4 = 3;
+ constant Real x5 = 6.0;
+ constant Real x6 = 12.0;
+end VariabilityPropagationTests.FunctionCallEquationPartial5;
+")})));
+end FunctionCallEquationPartial5;
+
+model FunctionCallEquationPartial6
+    Real x1,x2;
+    parameter Real x3;
+    Real x4;
+  equation
+    (x1,x2) = fp(x3,x4);
+    x4 = 7;
+    
+    annotation(__JModelica(UnitTesting(tests={
+        TransformCanonicalTestCase(
+            name="FunctionCallEquationPartial6",
+            description="Tests evaluation of matrix multiplication in function.",
+            inline_functions="none",
+            flatModel="
+fclass VariabilityPropagationTests.FunctionCallEquationPartial6
+ parameter Real x1;
+ constant Real x2 = 7.0;
+ parameter Real x3;
+ constant Real x4 = 7;
+parameter equation
+ (x1, ) = VariabilityPropagationTests.fp(x3, 7.0);
+
+public
+ function VariabilityPropagationTests.fp
+  input Real i1;
+  input Real i2;
+  output Real o1;
+  output Real o2;
+ algorithm
+  o1 := i1;
+  o2 := i2;
+  return;
+ end VariabilityPropagationTests.fp;
+
+end VariabilityPropagationTests.FunctionCallEquationPartial6;
+")})));
+end FunctionCallEquationPartial6;
+
+model FunctionCallEquationPartial7
+    function fp
+        input Real i1;
+        input Real i2;
+        output Real o1 = i1;
+        output Real o2 = i2;
+        output Real o3 = i1;
+    algorithm
+    end fp;
+    
+    Real x1,x2,x3,x4,x5,c;
+    parameter Real p;
+  equation
+    (x3,x4,x5) = fp(x1,x2);
+    (x1,x2) = fp(p,c);
+    c = 7;
+    
+    annotation(__JModelica(UnitTesting(tests={
+        TransformCanonicalTestCase(
+            name="FunctionCallEquationPartial7",
+            description="Tests evaluation of matrix multiplication in function.",
+            inline_functions="none",
+            flatModel="
+fclass VariabilityPropagationTests.FunctionCallEquationPartial7
+ parameter Real x1;
+ constant Real x2 = 7.0;
+ parameter Real x3;
+ constant Real x4 = 7.0;
+ parameter Real x5;
+ constant Real c = 7;
+ parameter Real p;
+parameter equation
+ (x1, ) = VariabilityPropagationTests.FunctionCallEquationPartial7.fp(p, 7.0);
+ (x3, , x5) = VariabilityPropagationTests.FunctionCallEquationPartial7.fp(x1, 7.0);
+
+public
+ function VariabilityPropagationTests.FunctionCallEquationPartial7.fp
+  input Real i1;
+  input Real i2;
+  output Real o1;
+  output Real o2;
+  output Real o3;
+ algorithm
+  o1 := i1;
+  o2 := i2;
+  o3 := i1;
+  return;
+ end VariabilityPropagationTests.FunctionCallEquationPartial7.fp;
+
+end VariabilityPropagationTests.FunctionCallEquationPartial7;
+")})));
+end FunctionCallEquationPartial7;
 
 
 model ConstantRecord1

trunk/Compiler/ModelicaFlatTree/src/jastadd/ConstantEvaluation.jrag
@@ -2776,6 +2776,47 @@ aspect ConstantEvaluation {
 	  }
 	  return res;
   }
+  
+    /**
+     * Perform constant evaluation of functions. Catches any ConstantEvaluationExceptions and
+     * provide CValue.UNKNOWN instead.
+     * 
+     * @return  constant values for the outputs
+     */
+    public CValue[] FAbstractFunctionCall.evaluateSafe(VariableEvaluator evaluator) {
+        Map<CommonVariableDecl, CValue> values = new HashMap<CommonVariableDecl, CValue>();
+        int i = 0;
+        CValue[] res = new CValue[myCallOutputs().size()];
+        try {
+            for (CommonVariableDecl var : myCallInputs()) {
+                var.prepareForFunctionEval();
+                FExp arg = argument(i++);
+                if (arg != null) {
+                    if (arg.variability().knownParameterOrLess()) {
+                        values.put(var, arg.ceval(evaluator));
+                    } else {
+                        values.put(var, CValue.UNKNOWN);
+                    }
+                } else {
+                    throw new ConstantEvaluationException();
+                }
+            }
+            
+            evaluate(values);
+            i = 0;
+            for (CommonVariableDecl var : myCallOutputs()) {
+                CValue val = values.get(var);
+                res[i] = (val == null) ? CValue.UNKNOWN : val;
+                i++;
+            }
+        } catch (ConstantEvaluationException e) {
+            for (int k = 0; k < res.length; k++) {
+                res[k] = CValue.UNKNOWN;
+            }
+        }
+        
+        return res;
+    }
     
     public void FAbstractFunctionCall.evaluate(Map<CommonVariableDecl, CValue> values) {
         evaluateCell(values);

commit 0fb339783347b0f6144b5f94ffc0610264d184b9

#3938 Fixes bug introduced in r6805 where initial equations were var. propagated.

trunk/Compiler/ModelicaMiddleEnd/src/jastadd/optimizations/VariabilityPropagation.jrag
@@ -44,7 +44,7 @@ aspect VariabilityPropagation {
 	 * Checks if equation is eligible for var. prop.
 	 */
     syn boolean FAbstractEquation.canPropagate() =
-            !inConditionalClause() && !containsActiveAnnotations();
+            canPropagateContext() && !containsActiveAnnotations();
     eq FEquation.canPropagate() = super.canPropagate() && variables().size() == 1;
     eq FFunctionCallEquation.canPropagate() {
         getCall().flushAllRecursiveClearFinal();
@@ -53,10 +53,10 @@ aspect VariabilityPropagation {
                         || potentialPartialVariability().constantVariability());
     }
     
-    inh boolean FAbstractEquation.inConditionalClause();
-    eq Root.getChild().inConditionalClause() = false;
-    eq FClass.getFAbstractEquation().inConditionalClause() = false;
-    eq FIfWhenElseEquation.getChild().inConditionalClause() = true;
+    inh boolean FAbstractEquation.canPropagateContext();
+    eq Root.getChild().canPropagateContext() = false;
+    eq FClass.getFAbstractEquation().canPropagateContext() = true;
+    eq FIfWhenElseEquation.getChild().canPropagateContext() = false;
     
     /**
      * If any arg is constant return constant. Otherwise returns combined variability.

trunk/Compiler/ModelicaMiddleEnd/src/test/VariabilityPropagationTests.mo
@@ -1183,6 +1183,39 @@ end VariabilityPropagationTests.InitialEquation2;
 ")})));
 end InitialEquation2;
 
+model InitialEquation3
+    Real x;
+    parameter Real p1 = 3;
+    Real p2 = p1;
+initial equation
+    x = p2;
+equation
+    when time > 1 then
+        x = time;
+    end when;
+
+    annotation(__JModelica(UnitTesting(tests={
+        TransformCanonicalTestCase(
+            name="InitialEquation3",
+            description="Test no propagation of initial equations",
+            flatModel="
+fclass VariabilityPropagationTests.InitialEquation3
+ discrete Real x;
+ parameter Real p1 = 3 /* 3 */;
+ parameter Real p2;
+ discrete Boolean temp_1;
+initial equation 
+ x = p2;
+ pre(temp_1) = false;
+parameter equation
+ p2 = p1;
+equation
+ temp_1 = time > 1;
+ x = if temp_1 and not pre(temp_1) then time else pre(x);
+end VariabilityPropagationTests.InitialEquation3;
+")})));
+end InitialEquation3;
+
 model AliasVariabilities1
 	Real a,b,c,d;
 	parameter Real p1,p2;