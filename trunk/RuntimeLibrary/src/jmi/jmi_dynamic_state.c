jnorthrup/jmodelica

commit 9022dc842a161a581458e4932b9fa8b2b01d7c5e

Cynamic state add calls to jmi now also contains the value references of the state vars and algebraic vars. Ticket #4231.
git-svn-id: https://svn.jmodelica.org@7308 7ae51c65-13d5-4921-9d80-6511ddd1419c

trunk/RuntimeLibrary/src/jmi/jmi_dynamic_state.c
@@ -25,7 +25,7 @@
 #include "jmi_util.h"
 #include "jmi_dynamic_state.h"
 
-int jmi_dynamic_state_add_set(jmi_t* jmi, int index, int n_variables, int n_states, int* value_references, jmi_dynamic_state_coefficents_func_t coefficents) {
+int jmi_dynamic_state_add_set(jmi_t* jmi, int index, int n_variables, int n_states, int* variable_value_references, int* state_value_references, int* algebraic_value_references, jmi_dynamic_state_coefficents_func_t coefficents);
     jmi_dynamic_state_set_t *set = &jmi->dynamic_state_sets[index];
     int i = 0;
     
@@ -36,11 +36,11 @@ int jmi_dynamic_state_add_set(jmi_t* jmi, int index, int n_variables, int n_stat
     set->coefficents = coefficents;
     
     for (i = 0; i < n_variables; i++) {
-        set->variables_value_references[i] = value_references[i];
+        set->variables_value_references[i] = variable_value_references[i];
     }
     /* As default, choose the first variables to the states */
     for (i = 0; i < n_states; i++) {
-        set->state_value_references[i] = value_references[i];
+        set->state_value_references[i] = variable_value_references[i];
     }
     
     return JMI_OK;

trunk/Compiler/ModelicaCBackEnd/src/jastadd/CCodeGen.jrag
@@ -5067,16 +5067,30 @@ aspect Serialize {
     public void DynamicStateSet.genDynamicStateAddCall_C(CodePrinter p, CodeStream str, String indent) {
         str.formatln("%s{", indent);
         String innerIndent = p.indent(indent);
-        str.formatln("%sint* ds_value_refs = calloc(%d, sizeof(int));", innerIndent, numVars());
+        str.formatln("%sint* ds_var_value_refs = calloc(%d, sizeof(int));", innerIndent, numVars());
+        str.formatln("%sint* ds_state_value_refs = calloc(%d, sizeof(int));", innerIndent, numStates());
+        str.formatln("%sint* ds_algebraic_value_refs = calloc(%d, sizeof(int));", innerIndent, numAlgebraics());
         int i = 0;
         for (FVariable var : fVars()) {
-            str.formatln("%sds_value_refs[%d] = %d;", innerIndent, i, var.valueReference());
+            str.formatln("%sds_var_value_refs[%d] = %d;", innerIndent, i, var.valueReference());
             i++;
         }
-        str.format("%sjmi_dynamic_state_add_set(*jmi, %d, %d, %d, ds_value_refs, ", innerIndent, id_C(), numVars(), numStates());
+        i = 0;
+        for (FVariable var : getStateVars()) {
+            str.formatln("%sds_state_value_refs[%d] = %d;", innerIndent, i, var.valueReference());
+            i++;
+        }
+        i = 0;
+        for (FVariable var : getAlgebraicVars()) {
+            str.formatln("%sds_algebraic_value_refs[%d] = %d;", innerIndent, i, var.valueReference());
+            i++;
+        }
+        str.format("%sjmi_dynamic_state_add_set(*jmi, %d, %d, %d, ds_var_value_refs, ds_state_value_refs, ds_algebraic_value_refs, ", innerIndent, id_C(), numVars(), numStates());
         genDSCoefficientsFuncName_C(p, str, indent);
         str.println(");");
-        str.formatln("%sfree(ds_value_refs);", innerIndent);
+        str.formatln("%sfree(ds_var_value_refs);", innerIndent);
+        str.formatln("%sfree(ds_state_value_refs);", innerIndent);
+        str.formatln("%sfree(ds_algebraic_value_refs);", innerIndent);
         str.formatln("%s}", indent);
     }
 

trunk/Compiler/ModelicaCBackEnd/src/test/CCodeGenTests.mo
@@ -19615,19 +19615,31 @@ static void ds_coefficients_1(jmi_t* jmi, jmi_real_t* res) {
 }
 
 
-    {
-        int* ds_value_refs = calloc(2, sizeof(int));
-        ds_value_refs[0] = 12;
-        ds_value_refs[1] = 13;
-        jmi_dynamic_state_add_set(*jmi, 0, 2, 1, ds_value_refs, ds_coefficients_0);
-        free(ds_value_refs);
+   {
+        int* ds_var_value_refs = calloc(2, sizeof(int));
+        int* ds_state_value_refs = calloc(1, sizeof(int));
+        int* ds_algebraic_value_refs = calloc(1, sizeof(int));
+        ds_var_value_refs[0] = 12;
+        ds_var_value_refs[1] = 13;
+        ds_state_value_refs[0] = 8;
+        ds_algebraic_value_refs[0] = 19;
+        jmi_dynamic_state_add_set(*jmi, 0, 2, 1, ds_var_value_refs, ds_state_value_refs, ds_algebraic_value_refs, ds_coefficients_0);
+        free(ds_var_value_refs);
+        free(ds_state_value_refs);
+        free(ds_algebraic_value_refs);
     }
     {
-        int* ds_value_refs = calloc(2, sizeof(int));
-        ds_value_refs[0] = 10;
-        ds_value_refs[1] = 11;
-        jmi_dynamic_state_add_set(*jmi, 1, 2, 1, ds_value_refs, ds_coefficients_1);
-        free(ds_value_refs);
+        int* ds_var_value_refs = calloc(2, sizeof(int));
+        int* ds_state_value_refs = calloc(1, sizeof(int));
+        int* ds_algebraic_value_refs = calloc(1, sizeof(int));
+        ds_var_value_refs[0] = 10;
+        ds_var_value_refs[1] = 11;
+        ds_state_value_refs[0] = 9;
+        ds_algebraic_value_refs[0] = 20;
+        jmi_dynamic_state_add_set(*jmi, 1, 2, 1, ds_var_value_refs, ds_state_value_refs, ds_algebraic_value_refs, ds_coefficients_1);
+        free(ds_var_value_refs);
+        free(ds_state_value_refs);
+        free(ds_algebraic_value_refs);
     }
 
     jmi_ad_var_t tmp_1;

trunk/RuntimeLibrary/src/jmi/jmi_dynamic_state.h
@@ -38,7 +38,7 @@ struct jmi_dynamic_state_set_t {
     jmi_dynamic_state_coefficents_func_t coefficents;
 };
 
-int jmi_dynamic_state_add_set(jmi_t* jmi, int index, int n_variables, int n_states, int* value_references, jmi_dynamic_state_coefficents_func_t coefficents);
+int jmi_dynamic_state_add_set(jmi_t* jmi, int index, int n_variables, int n_states, int* variable_value_references, int* state_value_references, int* algebraic_value_references, jmi_dynamic_state_coefficents_func_t coefficents);
 
 int jmi_dynamic_state_check_is_state(jmi_t* jmi, int index, int* value_references);
 

commit 475f9601b6f2f86fa17f6e2b8cb89886a151a070

Fixed typo that caused build problems, introduced in r7308. Ticket #4231.

trunk/RuntimeLibrary/src/jmi/jmi_dynamic_state.c
@@ -25,7 +25,7 @@
 #include "jmi_util.h"
 #include "jmi_dynamic_state.h"
 
-int jmi_dynamic_state_add_set(jmi_t* jmi, int index, int n_variables, int n_states, int* variable_value_references, int* state_value_references, int* algebraic_value_references, jmi_dynamic_state_coefficents_func_t coefficents);
+int jmi_dynamic_state_add_set(jmi_t* jmi, int index, int n_variables, int n_states, int* variable_value_references, int* state_value_references, int* algebraic_value_references, jmi_dynamic_state_coefficents_func_t coefficents) {
     jmi_dynamic_state_set_t *set = &jmi->dynamic_state_sets[index];
     int i = 0;
     