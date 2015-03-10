jnorthrup/jmodelica

commit 52f46b6c1fa04f5ceda1d63ff1edcae14da409c1

#4270 Moved fixed=false propagation to before function inlining to remove complexity issue. Upgraded variability propagation to propagate fixed=false.
git-svn-id: https://svn.jmodelica.org@7416 7ae51c65-13d5-4921-9d80-6511ddd1419c

trunk/Compiler/ModelicaMiddleEnd/src/jastadd/optimizations/VariabilityPropagation.jrag
@@ -165,6 +165,7 @@ aspect VariabilityPropagation {
 	public void FClass.variabilityPropagation() {
 
 		List<FAbstractEquation> parameterEquations = getFParameterEquations();
+        List<FAbstractEquation> initialEquations = getFInitialEquations();
 		LinkedHashSet<FAbstractEquation> worklist = new LinkedHashSet<FAbstractEquation>();
 		List<FAbstractEquation> equations = getFAbstractEquations();
 
@@ -178,7 +179,7 @@ aspect VariabilityPropagation {
 		// Work
 		while (!worklist.isEmpty()) {
 			FAbstractEquation equation = worklist.iterator().next();
-			equation.variabilityPropagation(worklist, parameterEquations);
+            equation.variabilityPropagation(worklist, parameterEquations, initialEquations);
 			worklist.remove(equation);
 		}
 
@@ -210,7 +211,7 @@ aspect VariabilityPropagation {
 		
 		// Clean initial equations
 		newEquations = new ArrayList<FAbstractEquation>();
-		for (FAbstractEquation equation: initialEquations()) {
+		for (FAbstractEquation equation: initialEquations) {
 			if (!equation.taggedByVProp) {
 				newEquations.add(equation);
 			}
@@ -220,13 +221,15 @@ aspect VariabilityPropagation {
 	
 	public void FAbstractEquation.variabilityPropagation(
 			LinkedHashSet<FAbstractEquation> worklist,
-			List<FAbstractEquation> parameterEquations) {
+            List<FAbstractEquation> parameterEquations,
+            List<FAbstractEquation> initialEquations) {
 		
 	}
 		
 	public void FEquation.variabilityPropagation(
 			LinkedHashSet<FAbstractEquation> worklist,
-			List<FAbstractEquation> parameterEquations) {
+            List<FAbstractEquation> parameterEquations,
+            List<FAbstractEquation> initialEquations) {
 		
 		FVariable fVariable;
 		FExp solution;
@@ -260,6 +263,8 @@ aspect VariabilityPropagation {
 			// discrete variability. IfExpr, sample() etc.
 			return;
 		}
+        
+        boolean solutionFixed = true;
 
 		if (solutionVariability.knownParameterOrLess()) {
 			try {
@@ -269,17 +274,22 @@ aspect VariabilityPropagation {
 				solutionVariability = fParameter();
 			}
 		} else {
-			parameterEquations.add(new FEquation(fVariable.createUseExp(), solution));
+            if (solution.containsNonFixedParameter()) {
+                solutionFixed = false;
+                parameterEquations = initialEquations;
+            }
+            parameterEquations.add(new FEquation(fVariable.createUseExp(), solution));
 		}
 
 		this.taggedByVProp = true;
-		fVariable.variabilityPropagation(worklist, solutionVariability, solution, parameterEquations);
+        fVariable.variabilityPropagation(worklist, solutionVariability, solution, solutionFixed, parameterEquations);
 
 	}
 	
 	public void FFunctionCallEquation.variabilityPropagation(
 			LinkedHashSet<FAbstractEquation> worklist,
-			List<FAbstractEquation> parameterEquations) {
+            List<FAbstractEquation> parameterEquations,
+            List<FAbstractEquation> initialEquations) {
         
         if (!canPropagate()) {
             return;
@@ -293,6 +303,8 @@ aspect VariabilityPropagation {
         }
         FTypePrefixVariability variability = potentialPartialVariability();
         boolean eval = variability.knownParameterOrLess();
+        boolean solutionFixed = !param || !getCall().containsNonFixedParameter();
+        parameterEquations = solutionFixed ? parameterEquations : initialEquations;
         
         CValue[] val = null;
         if (eval) {
@@ -316,7 +328,7 @@ aspect VariabilityPropagation {
             if (left.hasFExp()) {
                 if (left.getFExp().canPropagate()) {
                     left.taggedByVProp = left.getFExp().variabilityPropagation(worklist,
-                            eval ? val[i] : CValue.UNKNOWN, param ? parameterEquations : null);
+                            eval ? val[i] : CValue.UNKNOWN, solutionFixed, param ? parameterEquations : null);
                 }
                 allRemoved &= left.taggedByVProp;
             }
@@ -337,7 +349,8 @@ aspect VariabilityPropagation {
 	public boolean FExp.variabilityPropagation(
 			LinkedHashSet<FAbstractEquation> worklist,
 			CValue value,
-			List<FAbstractEquation> parameterEquations) {
+            boolean isFixed,
+            List<FAbstractEquation> parameterEquations) {
 		
 		throw new UnsupportedOperationException();
 	}
@@ -345,6 +358,7 @@ aspect VariabilityPropagation {
 	public boolean FRecordConstructor.variabilityPropagation(
 			LinkedHashSet<FAbstractEquation> worklist,
             CValue value,
+            boolean isFixed,
             List<FAbstractEquation> parameterEquations) {
 	    boolean res = true;
 		CValueRecord frc = null;
@@ -354,7 +368,8 @@ aspect VariabilityPropagation {
 
 		for (int i = 0; i < getNumArg(); i++) {
 			res &= getArg(i).variabilityPropagation(worklist, 
-					frc != null ? frc.getMember(i) : CValue.UNKNOWN, parameterEquations);
+                    frc != null ? frc.getMember(i) : CValue.UNKNOWN, 
+                    isFixed, parameterEquations);
 		}
 		taggedByVProp = res;
 		return res;
@@ -363,33 +378,37 @@ aspect VariabilityPropagation {
 	public boolean FArray.variabilityPropagation(
 			LinkedHashSet<FAbstractEquation> worklist,
             CValue value,
+            boolean isFixed,
             List<FAbstractEquation> parameterEquations) {
 		
 		CValueArray a = null;
 		if (value != CValue.UNKNOWN) {
 			a = (CValueArray) value;
 		}
-		taggedByVProp = this.variabilityPropagationArray(worklist, a, parameterEquations, new Enumerator());
+        taggedByVProp = this.variabilityPropagationArray(worklist, a, isFixed, parameterEquations, new Enumerator());
 		return taggedByVProp;
 	}
 	
     public boolean FExp.variabilityPropagationArray(
             LinkedHashSet<FAbstractEquation> worklist,
             CValueArray value,
+            boolean isFixed,
             List<FAbstractEquation> parameterEquations,
             Enumerator enumerator) {
         return this.variabilityPropagation(worklist, 
-                    value == null ? CValue.UNKNOWN : value.values[enumerator.next()], parameterEquations);
+                value == null ? CValue.UNKNOWN : value.values[enumerator.next()], 
+                isFixed, parameterEquations);
     }
     
     public boolean FArray.variabilityPropagationArray(
             LinkedHashSet<FAbstractEquation> worklist,
             CValueArray value,
+            boolean isFixed,
             List<FAbstractEquation> parameterEquations,
             Enumerator enumerator) {
         boolean res = true;
         for (FExp exp : getFExps()) {
-            res &= exp.variabilityPropagationArray(worklist, value, parameterEquations, enumerator);
+            res &= exp.variabilityPropagationArray(worklist, value, isFixed, parameterEquations, enumerator);
         }
         return res;
     }
@@ -398,6 +417,7 @@ aspect VariabilityPropagation {
     public boolean FIdUseExp.variabilityPropagation(
             LinkedHashSet<FAbstractEquation> worklist,
             CValue value,
+            boolean isFixed,
             List<FAbstractEquation> parameterEquations) {
         
         FExp bexp = null;
@@ -414,7 +434,7 @@ aspect VariabilityPropagation {
         }
         
         if (variability.knownParameterOrLess() || parameterEquations != null) {
-            myFV().variabilityPropagation(worklist, variability, bexp, parameterEquations);
+            myFV().variabilityPropagation(worklist, variability, bexp, isFixed, parameterEquations);
         }
         
         taggedByVProp = variability.knownParameterOrLess();
@@ -426,6 +446,7 @@ aspect VariabilityPropagation {
 			LinkedHashSet<FAbstractEquation> worklist,
 			FTypePrefixVariability variability,
             FExp bindingExp,
+            boolean isFixed,
             List<FAbstractEquation> parameterEquations) {
 		
 		throw new UnsupportedOperationException();
@@ -435,6 +456,7 @@ aspect VariabilityPropagation {
             LinkedHashSet<FAbstractEquation> worklist,
             FTypePrefixVariability variability,
             FExp bindingExp,
+            boolean isFixed,
             List<FAbstractEquation> parameterEquations) {
         
         FAbstractEquation equation;
@@ -449,10 +471,12 @@ aspect VariabilityPropagation {
         if (variability.knownParameterOrLess()) {
             setBindingExp(bindingExp.fullCopy());
         }
+        
+        // TODO: This would be much nicer if we had a fixedfalse parameter variability.
         if (variability.parameterVariability()) {
-            if (fixedAttributeSet()) {
-                setFixedAttribute(true);
-            }
+             if (!isFixed || fixedAttributeSet()) {
+                 setFixedAttribute(isFixed);
+             }
         }
         
         // Uses
@@ -494,14 +518,14 @@ aspect VariabilityPropagation {
         if (wasDiscrete) {
             FAbstractVariable pre = myPreVariable();
             pre.taggedByVProp = true;
-            pre.variabilityPropagation(worklist, variability, bindingExp, parameterEquations);
+            pre.variabilityPropagation(worklist, variability, bindingExp, isFixed, parameterEquations);
         }
         
         // Derivative
         if (isDifferentiatedVariable()) {
             FDerivativeVariable fDerivativeVariable = myDerivativeVariable();
             fDerivativeVariable.taggedByVProp = true;
-            fDerivativeVariable.variabilityPropagation(worklist, FTypePrefixVariability.fConstant(), new FIntegerLitExp(0), parameterEquations);
+            fDerivativeVariable.variabilityPropagation(worklist, FTypePrefixVariability.fConstant(), new FIntegerLitExp(0), true, parameterEquations);
         }
         
         // Alias variables

trunk/Compiler/ModelicaMiddleEnd/src/test/VariabilityPropagationTests.mo
@@ -1702,4 +1702,112 @@ end VariabilityPropagationTests.ZeroFactor3;
 ")})));
 end ZeroFactor3;
 
+model FixedFalse1
+    parameter Real p1(fixed=false);
+    Real p2 = p1 + 1;
+initial equation
+    p1 = 23;
+    annotation(__JModelica(UnitTesting(tests={
+        TransformCanonicalTestCase(
+            name="FixedFalse1",
+            description="Test propagation of fixed false parameters",
+            flatModel="
+fclass VariabilityPropagationTests.FixedFalse1
+ parameter Real p1(fixed = false);
+ parameter Real p2(fixed = false);
+initial equation 
+ p1 = 23;
+ p2 = p1 + 1;
+end VariabilityPropagationTests.FixedFalse1;
+")})));
+end FixedFalse1;
+
+model FixedFalse2
+
+    function f
+        input Real x;
+        output Real y2 = x;
+        output Real y3 = x;
+        algorithm
+    end f;
+
+    parameter Real p1(fixed=false);
+    Real p2;
+    Real p3;
+initial equation
+    p1 = 23;
+equation
+    (p2,p3) = f(p1);
+    annotation(__JModelica(UnitTesting(tests={
+        TransformCanonicalTestCase(
+            name="FixedFalse2",
+            inline_functions="none",
+            description="Test propagation of fixed false parameters, function call equation",
+            flatModel="
+fclass VariabilityPropagationTests.FixedFalse2
+ parameter Real p1(fixed = false);
+ parameter Real p2(fixed = false);
+ parameter Real p3(fixed = false);
+initial equation 
+ p1 = 23;
+ (p2, p3) = VariabilityPropagationTests.FixedFalse2.f(p1);
+
+public
+ function VariabilityPropagationTests.FixedFalse2.f
+  input Real x;
+  output Real y2;
+  output Real y3;
+ algorithm
+  y2 := x;
+  y3 := x;
+  return;
+ end VariabilityPropagationTests.FixedFalse2.f;
+
+end VariabilityPropagationTests.FixedFalse2;
+")})));
+end FixedFalse2;
+
+model FixedFalse3
+    parameter Real p1(fixed=false);
+    discrete Real p2 = p1 + 1;
+initial equation
+    p1 = 23;
+    annotation(__JModelica(UnitTesting(tests={
+        TransformCanonicalTestCase(
+            name="FixedFalse3",
+            description="Test propagation of fixed false parameters, originally discrete",
+            flatModel="
+fclass VariabilityPropagationTests.FixedFalse3
+ parameter Real p1(fixed = false);
+ parameter Real p2(fixed = false);
+initial equation 
+ p1 = 23;
+ p2 = p1 + 1;
+end VariabilityPropagationTests.FixedFalse3;
+")})));
+end FixedFalse3;
+
+model FixedFalse4
+    parameter Real p1(fixed=false);
+    Real p2 = p1 + 1;
+    Real p3 = p2 + 1;
+initial equation
+    p1 = 23;
+    annotation(__JModelica(UnitTesting(tests={
+        TransformCanonicalTestCase(
+            name="FixedFalse4",
+            description="Test propagation of fixed false parameters, chained",
+            flatModel="
+fclass VariabilityPropagationTests.FixedFalse4
+ parameter Real p1(fixed = false);
+ parameter Real p2(fixed = false);
+ parameter Real p3(fixed = false);
+initial equation 
+ p1 = 23;
+ p2 = p1 + 1;
+ p3 = p2 + 1;
+end VariabilityPropagationTests.FixedFalse4;
+")})));
+end FixedFalse4;
+
 end VariabilityPropagationTests;

trunk/Compiler/ModelicaMiddleEnd/src/jastadd/TransformCanonical.jrag
@@ -118,6 +118,7 @@ aspect TransformCanonical {
             transformations.add(new transformAlgorithms());
             transformations.add(new enableWhenClauseRewrite());
             transformations.add(new genInitArrayStatements());
+            transformations.add(new propagateNonFixedParameters());
             transformations.add(new FunctionInliningIfSet());
             transformations.add(new setFDerivativeVariables());
             transformations.add(new addFPreVariables());
@@ -131,7 +132,6 @@ aspect TransformCanonical {
             transformations.add(new genInitialEquations());
             transformations.add(new addStateInitialEquationsIfSet());
             transformations.add(new setFDerivativeVariablesPreBLT());
-            transformations.add(new propagateNonFixedParameters());
             transformations.add(new indexReduction());
             transformations.add(new LateFunctionInliningIfSet());
             transformations.add(new addFPreVariables());

commit 4643bebe261d1545f45e42b9445b43bf84fba5e5

#4270 Fixes bug introduced in r7416 where fixed=false wasn't propagated when breaking alias sets.

trunk/Compiler/ModelicaMiddleEnd/src/jastadd/optimizations/VariabilityPropagation.jrag
@@ -474,9 +474,7 @@ aspect VariabilityPropagation {
         
         // TODO: This would be much nicer if we had a fixedfalse parameter variability.
         if (variability.parameterVariability()) {
-             if (!isFixed || fixedAttributeSet()) {
-                 setFixedAttribute(isFixed);
-             }
+            propagateFixedFalse(isFixed);
         }
         
         // Uses
@@ -538,12 +536,20 @@ aspect VariabilityPropagation {
                     fv.setFTypePrefixVariability(variability);
                     if (variability.knownParameterOrLess())
                         fv.setBindingExp(bindingExp.negated(neg));
-                    if (variability.parameterVariability() && !fv.isTemporary())
+                    if (variability.parameterVariability() && !fv.isTemporary()) {
+                        fv.propagateFixedFalse(isFixed);
                         parameterEquations.add(new FEquation(fv.createUseExp(), createUseExp().negated(neg)));
+                    }
                 }
             }
             myFClass().getAliasManager().unAlias(this);
         }
+    }
+    
+    public void FVariable.propagateFixedFalse(boolean isFixed) {
+         if (!isFixed || fixedAttributeSet()) {
+             setFixedAttribute(isFixed);
+         }
     }
 	
     inh FExp FExp.myTopFactorFExp();

trunk/Compiler/ModelicaMiddleEnd/src/test/VariabilityPropagationTests.mo
@@ -1810,4 +1810,27 @@ end VariabilityPropagationTests.FixedFalse4;
 ")})));
 end FixedFalse4;
 
+model FixedFalse5
+    parameter Real p1(fixed=false);
+    Real p2 = p1 + 1;
+    Real p3 = p2;
+initial equation
+    p1 = p2 * 23;
+    annotation(__JModelica(UnitTesting(tests={
+        TransformCanonicalTestCase(
+            name="FixedFalse5",
+            description="Test propagation of fixed false parameters, alias",
+            flatModel="
+fclass VariabilityPropagationTests.FixedFalse5
+ parameter Real p1(fixed = false);
+ parameter Real p3(fixed = false);
+ parameter Real p2(fixed = false);
+initial equation 
+ p1 = p3 * 23;
+ p3 = p1 + 1;
+ p2 = p3;
+end VariabilityPropagationTests.FixedFalse5;
+")})));
+end FixedFalse5;
+
 end VariabilityPropagationTests;