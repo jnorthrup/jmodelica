jnorthrup/jmodelica

commit 9b666c48d0f4b078416ad7703bc9ed89e21ad0b1

First version of handling of meta equation during all symbolic steps. Needed for hybrid systems. Ticket #4302.
git-svn-id: https://svn.jmodelica.org@7482 7ae51c65-13d5-4921-9d80-6511ddd1419c

branches/PreVariablesUpdate/Compiler/ModelicaCBackEnd/templates/FMIBase/equ.c
@@ -16,6 +16,8 @@
 
 $C_enum_strings$
 
+$genReinitTempDecls_C$
+
 $C_dae_blocks_residual_functions$
 
 $CAD_dae_blocks_residual_functions$

branches/PreVariablesUpdate/Compiler/ModelicaCBackEnd/src/jastadd/CCodeGen.jrag
@@ -2034,67 +2034,145 @@ aspect CCodeGen {
         p.printPostSteps(getTest(), str, indent);
     }
     
-    public void FReinit.genVarDecls_C(CodePrinter p, CodeStream str, String indent) {
-        str.format("%sjmi_ad_var_t %s;\n", indent, tempName_C());
-    }
-    
     public void FReinit.prettyPrint_C(CodePrinter p, CodeStream str, String indent) {
         str.format("%s%s = ", indent, tempName_C());
         p.print(getFExp(), str, indent);
-        str.print(";\n");
+        str.println(";");
     }
     
-    public void FReinit.genReinitWritebacks_C(CodePrinter p, CodeStream str, String indent) {
-        str.print(indent);
+//    public void FReinit.genTempFree_C(CodePrinter p, CodeStream str, String indent) {
+//        str.formatln("%sjmi->reinit_triggered = 1;", indent);
+//    }
+
+    @Override
+    public void FReinit.genReinitTempDecls_C(CodePrinter p, CodeStream str, String indent) {
+        str.formatln("%sstatic jmi_ad_var_t %s;", indent, tempName_C());
+    }
+
+    public void FAbstractFunctionCall.genReinitTempDecls_C(CodePrinter p, CodeStream str, String indent) {}
+
+    public void FAbstractEquation.genReinitTempDecls_C(CodePrinter p, CodeStream str, String indent) {}
+
+    @Override
+    public void FFunctionCallEquation.genReinitTempDecls_C(CodePrinter p, CodeStream str, String indent) {
+        getCall().genReinitTempDecls_C(p, str, indent);
+    }
+
+    @Override
+    public void FIfWhenElseEquation.genReinitTempDecls_C(CodePrinter p, CodeStream str, String indent) {
+        for (FAbstractEquation e : getFAbstractEquations()) {
+            e.genReinitTempDecls_C(p, str, indent);
+        }
+    }
+
+    @Override
+    public void FIfWhenEquation.genReinitTempDecls_C(CodePrinter p, CodeStream str, String indent) {
+        super.genReinitTempDecls_C(p, str, indent);
+        if (hasElse()) {
+            getElse().genReinitTempDecls_C(p, str, indent);
+        }
+    }
+
+    /**
+     * Generates temporary variable declarations for storing reinit values.
+     */
+    public void AbstractEquationBlock.genReinitTempDecls_C(CodePrinter p, CodeStream str, String indent) {
+        for (FAbstractEquation eqn : allEquations()) {
+            eqn.genReinitTempDecls_C(p, str, indent);
+        }
+    }
+
+    @Override
+    public void FReinit.genReinitTempInits_C(CodePrinter p, CodeStream str, String indent) {
+        str.format("%s%s = ", indent, tempName_C());
         p.print(getVar(), str, indent);
-        str.format(" = %s;\n", tempName_C());
-        str.format("%sjmi->reinit_triggered = 1;\n", indent);
+        str.println(";");
     }
+
+    public void FAbstractFunctionCall.genReinitTempInits_C(CodePrinter p, CodeStream str, String indent) {}
+
+    public void FAbstractEquation.genReinitTempInits_C(CodePrinter p, CodeStream str, String indent) {}
+
+    @Override
+    public void FFunctionCallEquation.genReinitTempInits_C(CodePrinter p, CodeStream str, String indent) {
+        getCall().genReinitTempInits_C(p, str, indent);
+    }
+
+    @Override
+    public void FIfWhenElseEquation.genReinitTempInits_C(CodePrinter p, CodeStream str, String indent) {
+        for (FAbstractEquation e : getFAbstractEquations()) {
+            e.genReinitTempInits_C(p, str, indent);
+        }
+    }
+
+    @Override
+    public void FIfWhenEquation.genReinitTempInits_C(CodePrinter p, CodeStream str, String indent) {
+        super.genReinitTempInits_C(p, str, indent);
+        if (hasElse()) {
+            getElse().genReinitTempInits_C(p, str, indent);
+        }
+    }
+
+    /**
+     * Generates temporary variable initializaions for storing reinit values.
+     * This should be done in the begining of ode-derivatives and sets the
+     * value to current value.
+     */
+    public void AbstractEquationBlock.genReinitTempInits_C(CodePrinter p, CodeStream str, String indent) {
+        for (FAbstractEquation eqn : allEquations()) {
+            eqn.genReinitTempInits_C(p, str, indent);
+        }
+    }
+    
+    
+
+    @Override
+    public void FReinit.genReinitWritebacks_C(CodePrinter p, CodeStream str, String indent) {
+        //TODO: This is TEMPORARY, will need a better solution later!
+        str.format("%sif (%s != ", indent, tempName_C());
+        p.print(getVar(), str, indent);
+        str.println(") {");
+        str.print(p.indent(indent));
+        p.print(getVar(), str, indent);
+        str.formatln(" = %s;", tempName_C());
+        str.formatln("%sjmi->reinit_triggered = 1;", p.indent(indent));
+        str.formatln("%s}", indent);
+    }
+
     public void FAbstractFunctionCall.genReinitWritebacks_C(CodePrinter p, CodeStream str, String indent) {}
-    
+
     public void FAbstractEquation.genReinitWritebacks_C(CodePrinter p, CodeStream str, String indent) {}
-    
+
+    @Override
     public void FFunctionCallEquation.genReinitWritebacks_C(CodePrinter p, CodeStream str, String indent) {
         getCall().genReinitWritebacks_C(p, str, indent);
     }
-    
+
+    @Override
     public void FIfWhenElseEquation.genReinitWritebacks_C(CodePrinter p, CodeStream str, String indent) {
-        if (containsReinit()) {
-            genIfStart_C(p, str, indent);
-            String nextInd = p.indent(indent);
-            for (FAbstractEquation e : getFAbstractEquations())
-                e.genReinitWritebacks_C(p, str, nextInd);
+        for (FAbstractEquation e : getFAbstractEquations()) {
+            e.genReinitWritebacks_C(p, str, indent);
         }
     }
-    
+
+    @Override
     public void FIfWhenEquation.genReinitWritebacks_C(CodePrinter p, CodeStream str, String indent) {
-        if (containsReinit()) {
-            super.genReinitWritebacks_C(p, str, indent);
-            if (hasElse() && getElse().containsReinit())
-                getElse().genReinitWritebacks_C(p, str, indent);
-            genIfEnd_C(p, str, indent);
+        super.genReinitWritebacks_C(p, str, indent);
+        if (hasElse()) {
+            getElse().genReinitWritebacks_C(p, str, indent);
         }
     }
-    
-    public void AbstractEquationBlock.genReinitWritebacks_C(CodePrinter p, String indent, CodeStream str) {}
-    
-    public void MetaEquationBlock.genReinitWritebacks_C(CodePrinter p, String indent, CodeStream str) {
-        equation().genReinitWritebacks_C(p, str, indent);
+
+    /**
+     * Generates code that writes back the reinit values to the state. There is
+     * no if guard here, that is controlled from the actual reinit statement.
+     */
+    public void AbstractEquationBlock.genReinitWritebacks_C(CodePrinter p, String indent, CodeStream str) {
+        for (FAbstractEquation eqn : allEquations()) {
+            eqn.genReinitWritebacks_C(p, str, indent);
+        }
     }
-    
-    public void FunctionCallEquationBlock.genReinitWritebacks_C(CodePrinter p, CodeStream str, String indent) {
-        equation().genReinitWritebacks_C(p, str, indent);
-    }
-    
-    syn boolean ASTNode.containsReinit() {
-        for (ASTNode n : this)
-            if (n.containsReinit())
-                return true;
-        return false;
-    }
-    eq FExp.containsReinit()    = false;
-    eq FReinit.containsReinit() = true;
-	
+
 	syn String FBinExp.op_C() = op();
 	eq FDotAddExp.op_C() = " + ";
 	eq FDotSubExp.op_C() = " - ";
@@ -3389,7 +3467,13 @@ aspect CCodeGen {
             throw new UnsupportedOperationException("Unable to produce solving code for block of type " + getClass().getSimpleName() + " in residual function");
         }
     }
-    
+
+    @Override
+    public void MetaEquationBlock.genBlockResidualFunction_solvedEqn_C(CodePrinter p, CodeStream str,
+            String indent, boolean inInitial, Set<FAbstractEquation> visited) {
+        genBlock_C(p, str, indent, inInitial);
+    }
+
     public C_blockResidualAttributePrinter CodePrinter.C_blockResidualAttributePrinter = new C_blockResidualAttributePrinter();
     
     public abstract class CodePrinter {
@@ -4754,6 +4838,10 @@ aspect CCodeGen {
 	}
 
 	public void StructuredBLT.genOdeDerivativeBlocks(CodePrinter p, CodeStream str, String indent) {
+        str.formatln("%s/********* Initialize reinits *******/", indent);
+        for (AbstractEquationBlock eb : getAllBlocks()) {
+            eb.genReinitTempInits_C(p, str, indent);
+        }
 		str.formatln("%s/************* ODE section *********/", indent);
 		// Loop over all derivatives
 		for (AbstractEquationBlock eb : getOdeBlocks()) {

branches/PreVariablesUpdate/Compiler/ModelicaCBackEnd/src/jastadd/codegen/CGenerator.jrag
@@ -708,6 +708,25 @@ public class CGenerator extends GenericGenerator {
 		}
 	}
 	
+    /**
+     * Generates temporary variables for reinit writebacks
+     */
+    /**
+     * C: C temporary variables for reinit writebacks
+     */
+    public class DAETag_C_reinit_writeback_temps extends DAETag {
+        
+        public DAETag_C_reinit_writeback_temps(AbstractGenerator myGenerator, FClass fclass) {
+            super("C_reinit_temp_decls", myGenerator, fclass);
+            addOptions("generate_ode");
+        }
+
+        public void generate(CodeStream genPrinter) {
+            for (AbstractEquationBlock block : fclass.getDAEStructuredBLT().getAllBlocks())
+                block.genReinitTempDecls_C(ASTNode.printer_C, genPrinter, "");
+        }
+    }
+
 	/**
 	 * Generates headers for Modelica functions.
 	 */

branches/PreVariablesUpdate/Compiler/ModelicaCBackEnd/src/test/CCodeGenTests.mo
@@ -8251,6 +8251,8 @@ equation
             variability_propagation=false,
             relational_time_events=false,
             template="
+$C_reinit_temp_decls$
+-----
 $C_ode_derivatives$
 -----
 $C_ode_initialization$
@@ -8315,6 +8317,8 @@ equation
             description="",
             variability_propagation=false,
             template="
+$C_reinit_temp_decls$
+-----
 $C_ode_derivatives$
 -----
 $C_ode_initialization$
@@ -8398,6 +8402,8 @@ equation
             variability_propagation=false,
             relational_time_events=false,
             template="
+$C_reinit_temp_decls$
+-----
 $C_ode_derivatives$
 -----
 $C_ode_initialization$

branches/PreVariablesUpdate/Compiler/ModelicaMiddleEnd/src/jastadd/TransformCanonical.jrag
@@ -597,9 +597,9 @@ aspect TransformCanonical {
                 error(e.getMessage());
                 return;
             }
-            for (FAbstractEquation e : equations())
-                if (e.numScalarEquations() == 0)
-                    daeBLT.add(EquationBlockFactory.createMetaBlock(e));
+//            for (FAbstractEquation e : equations())
+//                if (e.numScalarEquations() == 0)
+//                    daeBLT.add(EquationBlockFactory.createMetaBlock(e));
             setDAEBLT(daeBLT);
             log.info(getDAEStructuredBLT());
             log.info(getDAEBLT());
@@ -723,17 +723,17 @@ aspect TransformCanonical {
                 error(e.getMessage());
                 return;
             }
-            for (FAbstractEquation e : equations()) {
-                if (e.numScalarEquations() == 0) {
-                    if (e.replacedAtInitialization()) {
-                        for (FAbstractEquation fae : e.getInitialEquations()) {
-                            daeInitBLT.add(EquationBlockFactory.createMetaBlock(e));
-                        }
-                    } else {
-                        daeInitBLT.add(EquationBlockFactory.createMetaBlock(e));
-                    }
-                }
-            }
+//            for (FAbstractEquation e : equations()) {
+//                if (e.numScalarEquations() == 0) {
+//                    if (e.replacedAtInitialization()) {
+//                        for (FAbstractEquation fae : e.getInitialEquations()) {
+//                            daeInitBLT.add(EquationBlockFactory.createMetaBlock(e));
+//                        }
+//                    } else {
+//                        daeInitBLT.add(EquationBlockFactory.createMetaBlock(e));
+//                    }
+//                }
+//            }
             setDAEInitBLT(daeInitBLT);
             log.info(daeInitBLT);
         } else {

branches/PreVariablesUpdate/Compiler/ModelicaMiddleEnd/src/jastadd/structural/BLT.jrag
@@ -77,13 +77,20 @@ aspect BLT{
             Collection<FVariable> fvs = new ArrayList<FVariable>();
             Collection<Integer> assignedIndices = new ArrayList<Integer>();
             FAbstractEquation e = component.iterator().next().getEquation(); // TODO remove this hack, set to null and introduce in first loop iteration
+            boolean isMeta = true;
             for (E eqn : component) {
                 if (eqn.getEquation() != e) 
                     return null;
-                fvs.add(eqn.getMatching().getVariable());
-                assignedIndices.add(eqn.groupNumber());
+                if (!eqn.isMeta()) {
+                    isMeta = false;
+                    fvs.add(eqn.getMatching().getVariable());
+                    assignedIndices.add(eqn.groupNumber());
+                }
             }
-            return createSimpleEquationBlock(e, fvs, assignedIndices, producer, component.computeBlockDependency(), solve);
+            if (isMeta)
+                return createMetaBlock(e);
+            else
+                return createSimpleEquationBlock(e, fvs, assignedIndices, producer, component.computeBlockDependency(), solve);
         }
 
         /**
@@ -105,9 +112,12 @@ aspect BLT{
             
             Collection<E> realEquations = new LinkedHashSet<E>();
             Collection<E> nonRealEquations = new ArrayList<E>();
+            Collection<E> metaEquations = new ArrayList<E>();
             Set<FVariable> realVariables = new HashSet<FVariable>();
             for (E e : component) {
-                if (e.getMatching().getVariable().isReal()) {
+                if (e.isMeta()) {
+                    metaEquations.add(e);
+                } else if (e.getMatching().getVariable().isReal()) {
                     realEquations.add(e);
                     realVariables.add(e.getMatching().getVariable());
                 } else {
@@ -126,12 +136,12 @@ aspect BLT{
             }
             
             AbstractEquationBlock block = null;
-            TornEquationBlock tornBlock = null;
+            EquationBlock eqnBlock = null;
             boolean torn = false;
             if (producer.useTearing() && realEquations.size() > 1) {
-                tornBlock = tear(component, producer, options, isInitial);
-                if (tornBlock != null && !tornBlock.solvedBlocks().isEmpty()) {
-                    block = tornBlock;
+                eqnBlock = tear(component, producer, options, isInitial);
+                if (eqnBlock != null && !eqnBlock.solvedBlocks().isEmpty()) {
+                    block = eqnBlock;
                     torn = true;
                 }
             }
@@ -140,7 +150,7 @@ aspect BLT{
                 // Check if an equation block is mixed, i.e., contains both continous
                 // and discrete variables, and if it is linear in the unknowns
                 Map<FVariable, AbstractEquationBlock> varToBlockMap = new HashMap<FVariable, AbstractEquationBlock>();
-                for (AbstractEquationBlock subBlock : tornBlock.solvedRealBlocks()) {
+                for (AbstractEquationBlock subBlock : eqnBlock.solvedRealBlocks()) {
                     for (FVariable var : subBlock.allVariables())
                         varToBlockMap.put(var, subBlock);
                 }
@@ -149,7 +159,7 @@ aspect BLT{
                     iterationVariables.add(var);
                 Set<AbstractEquationBlock> visitedBlocks = new HashSet<AbstractEquationBlock>();
                 Set<AbstractEquationBlock> activeBlocks = new HashSet<AbstractEquationBlock>();
-                for (SimpleEquationBlock subBlock : tornBlock.unsolvedBlocks()) {
+                for (SimpleEquationBlock subBlock : eqnBlock.unsolvedBlocks()) {
                     linearityUseCheck(subBlock, varToBlockMap, iterationVariables, visitedBlocks, activeBlocks);
                     activeBlocks.add(subBlock);
                 }
@@ -172,7 +182,8 @@ aspect BLT{
                 } else {
                     BiPGraph g = new BiPGraph(nonRealEquations);
                     Collection<SimpleEquationBlock> nonRealBlocks = g.computeNonRealBlocks(producer, options);
-                    block = new EquationBlock(producer, component.computeBlockDependency(), nonRealBlocks, createSimpleBlocks(groupComponent(realEquations), producer));
+                    eqnBlock = new EquationBlock(producer, component.computeBlockDependency(), nonRealBlocks, createSimpleBlocks(groupComponent(realEquations), producer));
+                    block = eqnBlock;
                 }
             }
             
@@ -181,6 +192,11 @@ aspect BLT{
                     block.computeJacobian(activeRealEquations);
 //                else
 //                   Give warning?
+            }
+            if (eqnBlock != null) {
+                for (E eqn : metaEquations) {
+                    eqnBlock.addSolvedBlock(createMetaBlock(eqn.getEquation()));
+                }
             }
             return block;
         }

branches/PreVariablesUpdate/Compiler/ModelicaMiddleEnd/src/jastadd/structural/DynamicStates.jrag
@@ -571,7 +571,7 @@ aspect DynamicStateSelect{
     
     public boolean SCCBlock.containsDynamicStates() {
         for (E eqn : members)
-            if (eqn.getMatching().getVariable().isFDynamicAlgebraicVariable())
+            if (!eqn.isMeta() && eqn.getMatching().getVariable().isFDynamicAlgebraicVariable())
                 return true;
         return false;
     }

commit 1c2ab9efd25e4e16770c378b7bb5e0625eaa5de4

Fixed typo that was introduced in r7482. Ticket #4302.

branches/PreVariablesUpdate/Compiler/ModelicaCBackEnd/templates/FMIBase/equ.c
@@ -16,7 +16,7 @@
 
 $C_enum_strings$
 
-$genReinitTempDecls_C$
+$C_reinit_temp_decls$
 
 $C_dae_blocks_residual_functions$
 