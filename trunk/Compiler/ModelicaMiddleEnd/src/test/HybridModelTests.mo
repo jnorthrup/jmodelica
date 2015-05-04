jnorthrup/jmodelica

commit 3a0a8119ed31c96897a11b6ef0a043814c2b43b5

Remerge of r7588 with a fix. Runtime can not handle "solved" blocks with kinsol solver. I.e. There must be at least one residual if kinsol is used. Ticket #4364.
git-svn-id: https://svn.jmodelica.org@7591 7ae51c65-13d5-4921-9d80-6511ddd1419c

trunk/Compiler/ModelicaMiddleEnd/src/jastadd/structural/BLT.jrag
@@ -108,7 +108,25 @@ aspect BLT{
             if (seb != null && seb.isSolvable())
                 return seb;
             
-            errorCheckWhenEquations(component);
+            BiPGraph g = new BiPGraph(component.getMembers());
+            Collection<SCCBlock<Eq, Var>> subBlocks = g.tarjan(options);
+            
+            errorCheckWhenEquations(subBlocks);
+            
+            Collection<SimpleEquationBlock> solvedBlocks = new ArrayList<SimpleEquationBlock>();
+            boolean allOk = true;
+            for (SCCBlock<Eq, Var> subBlock : subBlocks) {
+                SimpleEquationBlock subSEB = createSimpleEquationBlock(subBlock, producer, true);
+                if (subSEB != null && subSEB.isSolvable()) {
+                    solvedBlocks.add(subSEB);
+                } else {
+                    allOk = false;
+                    break;
+                }
+            }
+            if (allOk) {
+                return new PrePropagationEquationBlock(producer, component.computeBlockDependency(), solvedBlocks, Collections.<SimpleEquationBlock>emptyList());
+            }
             
             Collection<E> realEquations = new LinkedHashSet<E>();
             Collection<E> nonRealEquations = new ArrayList<E>();
@@ -180,8 +198,8 @@ aspect BLT{
                 if (seb != null && !mixed) {
                     block = seb;
                 } else {
-                    BiPGraph g = new BiPGraph(nonRealEquations);
-                    Collection<SimpleEquationBlock> nonRealBlocks = g.computeNonRealBlocks(producer, options);
+                    BiPGraph nonRealG = new BiPGraph(nonRealEquations);
+                    Collection<SimpleEquationBlock> nonRealBlocks = nonRealG.computeNonRealBlocks(producer, options);
                     eqnBlock = new EquationBlock(producer, component.computeBlockDependency(), nonRealBlocks, createSimpleBlocks(groupComponent(realEquations), producer));
                     block = eqnBlock;
                 }
@@ -228,28 +246,10 @@ aspect BLT{
             return blocks;
         }
 
-        private static <E extends AbstractEq<E, V>, V extends AbstractVar<E, V>> void errorCheckWhenEquations(SCCBlock<E, V> component) {
-            boolean containsWhen = false;
-            for (E eqn : component.getMembers()) {
-                if (eqn.getEquation().isWhen()) {
-                    containsWhen = true;
-                    break;
-                }
-            }
-            if (!containsWhen)
-                return;
-            BiPGraph g = new BiPGraph(component.getMembers());
-            Collection<SCCBlock<Eq, Var>> subBlocks = g.tarjan();
+        private static <E extends AbstractEq<E, V>, V extends AbstractVar<E, V>> void errorCheckWhenEquations(Collection<SCCBlock<Eq, Var>> subBlocks) {
             for (SCCBlock<Eq, Var> block : subBlocks) {
                 if (block.getMembers().size() == 1)
                     continue;
-                containsWhen = false;
-                for (Eq eqn : block.getMembers()) {
-                    if (eqn.getEquation().isWhen()) {
-                        containsWhen = true;
-                        break;
-                    }
-                }
                 Collection<FVariable> vars = new ArrayList<FVariable>();
                 for (Eq eqn : block.getMembers()) {
                     vars.add(eqn.getMatching().getVariable());
@@ -379,10 +379,14 @@ aspect BLT{
         }
     }
 
+    public BLT AbstractBiPGraph.computeBLT(OptionRegistry options) {
+        return computeBLT(options, Collections.<AbstractBiPGraph.SCCContributor>emptyList());
+    }
+
     public BLT AbstractBiPGraph.computeBLT(OptionRegistry options, Collection<SCCContributor> extraContributors) {
         return computeBLT(options, EquationBlockFactory.DEFAULT_BLOCK_PRODUCER, new BLT(), extraContributors);
     }
-
+    
     public <T extends BLT> T AbstractBiPGraph.computeBLT(OptionRegistry options, EquationBlockFactory.BlockProducer producer, T blt) {
         return computeBLT(options, EquationBlockFactory.DEFAULT_BLOCK_PRODUCER, blt, Collections.<AbstractBiPGraph.SCCContributor>emptyList());
     }
@@ -1485,6 +1489,18 @@ aspect BLT{
 
     }
 
+    /**
+     * PrePropagationEquationBlock represents an equation block resulting from
+     * algebraic loops that only exists when pre variables are considered.
+     */
+    public class PrePropagationEquationBlock extends EquationBlock {
+
+        public PrePropagationEquationBlock(EquationBlockFactory.BlockProducer producer, BlockDependency blockDependency, Collection<SimpleEquationBlock> solvedBlocks, Collection<SimpleEquationBlock> unsolvedBlocks) {
+            super(producer, blockDependency, solvedBlocks, unsolvedBlocks);
+        }
+
+    }
+    
     public class DynamicStateBlock extends AbstractEquationBlock {
         private Collection<DynamicStateBLT> subBLTs;
         private Collection<DynamicStateSet> sets;
@@ -2571,6 +2587,11 @@ aspect BLTPrettyPrinting {
         str.print("Torn");
     }
 
+    @Override
+    protected void PrePropagationEquationBlock.prettyPrintTypeStr(CodeStream str) {
+        str.print("Pre propagation");
+    }
+    
     protected void EquationBlock.prettyPrintNumVarStr(CodeStream str) {
         str.print(allVariables().size() + " variables");
     }
@@ -2591,6 +2612,19 @@ aspect BLTPrettyPrinting {
         str.println();
     }
 
+    @Override
+    protected void PrePropagationEquationBlock.prettyPrintBeforeVariablesStr(Printer p, CodeStream str, String indent) {
+        if (solvedRealBlocks().isEmpty())
+            return;
+        String innerIndent = p.indent(indent);
+        if (isMixed())
+            str.formatln("%sContinuous variables:", indent);
+        else
+            str.formatln("%sVariables:", indent);
+        for (AbstractEquationBlock aeb : solvedRealBlocks())
+            aeb.prettyPrintActiveVariables(p, str, innerIndent, false, true);
+    }
+    
     protected void EquationBlock.prettyPrintUnsolvedVariablesStr(Printer p, CodeStream str, String indent) {
         if (isMixed())
             str.formatln("%sUnknown continuous variables:", indent);
@@ -2603,6 +2637,10 @@ aspect BLTPrettyPrinting {
         str.formatln("%sIteration variables:", indent);
     }
 
+    @Override
+    protected void PrePropagationEquationBlock.prettyPrintUnsolvedVariablesStr(Printer p, CodeStream str, String indent) {
+    }
+    
     protected void EquationBlock.prettyPrintBeforeEquationsStr(Printer p, CodeStream str, String indent) {}
 
     @Override
@@ -2614,6 +2652,19 @@ aspect BLTPrettyPrinting {
         str.println();
     }
 
+    @Override
+    protected void PrePropagationEquationBlock.prettyPrintBeforeEquationsStr(Printer p, CodeStream str, String indent) {
+        if (solvedRealBlocks().isEmpty())
+            return;
+        String innerIndent = p.indent(indent);
+        if (isMixed())
+            str.formatln("%sContinuous equations:", indent);
+        else
+            str.formatln("%sEquations:", indent);
+        for (AbstractEquationBlock seb : solvedRealBlocks())
+            seb.prettyPrintSub(p, str, innerIndent);
+    }
+    
     protected void EquationBlock.prettyPrintUnsolvedEquationsStr(Printer p, CodeStream str, String indent) {
         if (isMixed())
             str.formatln("%sContinuous residual equations:", indent);
@@ -2629,6 +2680,10 @@ aspect BLTPrettyPrinting {
             str.formatln("%sResidual equations:", indent);
     }
 
+    @Override
+    protected void PrePropagationEquationBlock.prettyPrintUnsolvedEquationsStr(Printer p, CodeStream str, String indent) {
+    }
+    
     public abstract void AbstractEquationBlock.prettyPrintActiveVariables(Printer p, CodeStream str, String indent, boolean printAttributes, boolean printLabel);
 
     @Override

trunk/Compiler/ModelicaMiddleEnd/src/jastadd/structural/Graphs.jadd
@@ -901,8 +901,6 @@ public abstract class AbstractBiPGraph<E extends AbstractEq<E, V>, V extends Abs
         @Override
         public <E extends AbstractEq<E, V>, V extends AbstractVar<E, V>> Collection<E> members(E equation, AbstractBiPGraph<E, V, ?> graph) {
             Collection<E> depends = Collections.emptyList();
-            if (graph.isInitialSystem())
-                return depends;
             for (FVariable var : equation.getEquation().discretePreVariables()) {
                 FAbstractVariable nonPre = var.myNonPreVariable();
                 if (nonPre.isUnknown())
@@ -917,7 +915,7 @@ public abstract class AbstractBiPGraph<E extends AbstractEq<E, V>, V extends Abs
             if (!equation.isMeta()) {
                 V var = equation.getMatching();
                 FVariable fVar = var.getVariable();
-                if (fVar.isDiscrete() && !fVar.isPreVariable()) {
+                if (fVar.isDiscrete()) {
                     FAbstractVariable preVar = fVar.myPreVariable();
                     for (FIdUseExp exp : preVar.uses()) {
                         FAbstractEquation fae = exp.myFEquation().outermostEnclosingEquation();
@@ -925,18 +923,9 @@ public abstract class AbstractBiPGraph<E extends AbstractEq<E, V>, V extends Abs
                             continue;
                         Collection<E> eqns = graph.getEquations(fae);
                         if (eqns != null) {
-//                            boolean add = false;
-//                            for (E eqn : eqns) {
-//                                if (eqn.getVariable(var.getName()) != null) {
-//                                    add = true;
-//                                    break;
-//                                }
-//                            }
-//                            if (add) {
-                                if (depends.isEmpty())
-                                    depends = new ArrayList<E>();
-                                depends.addAll(eqns);
-//                            }
+                            if (depends.isEmpty())
+                                depends = new ArrayList<E>();
+                            depends.addAll(eqns);
                         }
                     }
                 }

trunk/Compiler/ModelicaMiddleEnd/src/test/HybridModelTests.mo
@@ -35,23 +35,18 @@ package HybridModelTests
 --- Solved equation ---
 b := sin(time) >= 0
 
---- Unsolved mixed linear system (Block 1) of 2 variables ---
-Coefficient variability: constant
-Unknown continuous variables:
+--- Pre propagation mixed system (Block 1) of 2 variables ---
+Continuous variables:
   der(x)
 
 Solved discrete variables:
   i
 
-Continuous residual equations:
-  der(x) = if pre(i) == 0 then 0 else 1
-    Iteration variables: der(x)
+Continuous equations:
+  der(x) := if pre(i) == 0 then 0 else 1
 
 Discrete equations:
   i := if b then 1 else 0
-
-Jacobian:
-  |1.0|
 
 --- Solved equation ---
 der(y) := if i == 0 then 2 else 3
@@ -76,23 +71,18 @@ der(y) := if i == 0 then 2 else 3
 --- Solved equation ---
 y := 3 * sin(time)
 
---- Unsolved mixed linear system (Block 1) of 2 variables ---
-Coefficient variability: constant
-Unknown continuous variables:
+--- Pre propagation mixed system (Block 1) of 2 variables ---
+Continuous variables:
   x
 
 Solved discrete variables:
   i
 
-Continuous residual equations:
-  x + pre(i) = y
-    Iteration variables: x
+Continuous equations:
+  x := - pre(i) + y
 
 Discrete equations:
   i := if x >= 0 then 1 else 2
-
-Jacobian:
-  |1.0|
 -------------------------------
 ")})));
     end PreTest2;
@@ -116,32 +106,22 @@ Jacobian:
 --- Solved equation ---
 y := 3 * sin(time)
 
---- Torn mixed linear system (Block 1) of 1 iteration variables and 1 solved variables ---
-Coefficient variability: constant
-Torn variables:
+--- Pre propagation mixed system (Block 1) of 4 variables ---
+Continuous variables:
   x
-
-Iteration variables:
   z
 
 Solved discrete variables:
   j
   i
 
-Torn equations:
+Continuous equations:
   x := - pre(i) + y
-
-Continuous residual equations:
-  z + pre(i) + pre(j) = y
-    Iteration variables: z
+  z := - pre(i) + (- pre(j)) + y
 
 Discrete equations:
   j := if x >= 0 and z >= 0 then 1 else 2
   i := if x >= 0 then 1 else 2
-
-Jacobian:
-  |1.0, 0.0|
-  |0.0, 1.0|
 -------------------------------
 ")})));
     end PreTest3;
@@ -163,23 +143,18 @@ Jacobian:
             description="Test interaction between continuous and discrete equations",
             methodName="printDAEBLT",
             methodResult="
---- Unsolved mixed linear system (Block 1) of 2 variables ---
-Coefficient variability: constant
-Unknown continuous variables:
+--- Pre propagation mixed system (Block 1) of 2 variables ---
+Continuous variables:
   x_d
 
 Solved discrete variables:
   temp_1
 
-Continuous residual equations:
-  x_d = if temp_1 and not pre(temp_1) then x_c + 1 else pre(x_d)
-    Iteration variables: x_d
+Continuous equations:
+  x_d := if temp_1 and not pre(temp_1) then x_c + 1 else pre(x_d)
 
 Discrete equations:
   temp_1 := sample(0, 1)
-
-Jacobian:
-  |1.0|
 
 --- Solved equation ---
 der(x_c) := - x_c + x_d
@@ -249,30 +224,20 @@ Jacobian:
             description="A case which gives bigger block with local pre handling, but avoid global iteration",
             methodName="printDAEBLT",
             methodResult="
---- Torn mixed linear system (Block 1) of 1 iteration variables and 1 solved variables ---
-Coefficient variability: constant
-Torn variables:
-  x_d
-
-Iteration variables:
+--- Pre propagation mixed system (Block 1) of 3 variables ---
+Continuous variables:
   x_c
+  x_d
 
 Solved discrete variables:
   temp_1
 
-Torn equations:
+Continuous equations:
+  x_c := pre(x_d)
   x_d := if temp_1 and not pre(temp_1) then x_c + 1 else pre(x_d)
-
-Continuous residual equations:
-  0 = - x_c + pre(x_d)
-    Iteration variables: x_c
 
 Discrete equations:
   temp_1 := sample(0, 1)
-
-Jacobian:
-  |1.0, 0.0|
-  |0.0, 1.0|
 -------------------------------
 ")})));
     end PreTest6;
@@ -294,23 +259,18 @@ Jacobian:
             description="A case which gives bigger block with local pre handling, but avoid global iteration",
             methodName="printDAEBLT",
             methodResult="
---- Unsolved mixed linear system (Block 1) of 2 variables ---
-Coefficient variability: constant
-Unknown continuous variables:
+--- Pre propagation mixed system (Block 1) of 2 variables ---
+Continuous variables:
   x_d
 
 Solved discrete variables:
   temp_1
 
-Continuous residual equations:
-  x_d = if temp_1 and not pre(temp_1) then pre(x_c) + 1 else pre(x_d)
-    Iteration variables: x_d
+Continuous equations:
+  x_d := if temp_1 and not pre(temp_1) then pre(x_c) + 1 else pre(x_d)
 
 Discrete equations:
   temp_1 := sample(0, 1)
-
-Jacobian:
-  |1.0|
 
 --- Solved equation ---
 x_c := x_d
@@ -335,32 +295,22 @@ x_c := x_d
             description="A case which gives bigger block with local pre handling, but avoid global iteration",
             methodName="printDAEBLT",
             methodResult="
---- Torn mixed linear system (Block 1) of 1 iteration variables and 1 solved variables ---
-Coefficient variability: constant
-Torn variables:
-  der(x)
-
-Iteration variables:
+--- Pre propagation mixed system (Block 1) of 4 variables ---
+Continuous variables:
   y
+  der(x)
 
 Solved discrete variables:
   temp_1
   i
 
-Torn equations:
+Continuous equations:
+  y := if temp_1 and not pre(temp_1) then pre(y) + 1 else pre(y)
   der(x) := (if pre(y) >= 3 then 1 else 2) + (if pre(i) == 4 then 5 else 6)
-
-Continuous residual equations:
-  y = if temp_1 and not pre(temp_1) then pre(y) + 1 else pre(y)
-    Iteration variables: y
 
 Discrete equations:
   temp_1 := sample(0, 1)
   i := if time >= 3 then 1 else 0
-
-Jacobian:
-  |1.0, 0.0|
-  |0.0, 1.0|
 -------------------------------
 ")})));
     end PreTest8;
@@ -436,37 +386,25 @@ Jacobian:
             description="Test complicated when and pre variable case",
             methodName="printDAEBLT",
             methodResult="
---- Torn mixed linear system (Block 1) of 1 iteration variables and 1 solved variables ---
-Coefficient variability: constant
-Torn variables:
-  x
-
-Iteration variables:
+--- Pre propagation mixed system (Block 1) of 3 variables ---
+Continuous variables:
   y
+  x
 
 Solved discrete variables:
   temp_1
 
-Torn equations:
+Continuous equations:
+  y := if temp_1 and not pre(temp_1) then pre(y) + 1.1 else pre(y)
   x := if temp_1 and not pre(temp_1) then pre(x) + 1.1 else pre(x)
-
-Continuous residual equations:
-  y = if temp_1 and not pre(temp_1) then pre(y) + 1.1 else pre(y)
-    Iteration variables: y
 
 Discrete equations:
   temp_1 := sample(0, 1)
 
-Jacobian:
-  |1.0, 0.0|
-  |0.0, 1.0|
-
 --- Solved equation ---
 der(xx) := - x
 
---- Unsolved mixed linear system (Block 2) of 4 variables ---
-Coefficient variability: constant
-Unknown continuous variables:
+--- Pre propagation mixed system (Block 2) of 4 variables ---
 
 Solved discrete variables:
   temp_2
@@ -474,7 +412,6 @@ Solved discrete variables:
   temp_3
   z
 
-Continuous residual equations:
 
 Discrete equations:
   temp_2 := y > 2 and pre(z)
@@ -482,25 +419,62 @@ Discrete equations:
   temp_3 := x > 2
   z := if temp_3 and not pre(temp_3) then false else pre(z)
 
-Jacobian:
-
---- Unsolved mixed linear system (Block 3) of 2 variables ---
-Coefficient variability: constant
-Unknown continuous variables:
+--- Pre propagation mixed system (Block 3) of 2 variables ---
 
 Solved discrete variables:
   temp_4
   v
 
-Continuous residual equations:
 
 Discrete equations:
   temp_4 := y > 2 and z
   v := if temp_4 and not pre(temp_4) then false else pre(v)
-
-Jacobian:
 -------------------------------
 ")})));
     end WhenAndPreTest1;
+    
+    model NoResTest1
+        function F
+            input Real i1;
+            input Real i2;
+            output Real o;
+        algorithm
+            assert(i1 == 0, "Oh, no!");
+            assert(i2 == 0, "Oh, no!");
+            o := 3.14 / i1 + 42 / i2;
+            annotation(Inline=false);
+        end F;
+        Real next, x;
+    initial equation
+        pre(next) = 0;
+    equation
+        when time >= pre(next) then
+            next = pre(next) + 1;
+        end when;
+        x = F(next, pre(next));
+        
+    annotation(__JModelica(UnitTesting(tests={
+        FClassMethodTestCase(
+            name="HybridModelTests.NoResTest1",
+            description="Verify that no residuals are added to the block even though it contains continuous equations",
+            methodName="printDAEBLT",
+            methodResult="
+--- Pre propagation mixed system (Block 1) of 3 variables ---
+Continuous variables:
+  next
+  x
+
+Solved discrete variables:
+  temp_1
+
+Continuous equations:
+  next := if temp_1 and not pre(temp_1) then pre(next) + 1 else pre(next)
+  x := HybridModelTests.NoResTest1.F(next, pre(next))
+
+Discrete equations:
+  temp_1 := time >= pre(next)
+-------------------------------
+")})));
+    end NoResTest1;
 
 end HybridModelTests;

trunk/Compiler/ModelicaCBackEnd/src/jastadd/CCodeGen.jrag
@@ -3766,7 +3766,7 @@ aspect CCodeGen {
 	eq FCompositeVariability.genJMIVariabilityConstant_C() = null;
 	
     public String AbstractEquationBlock.genJMISolverConstant_C(String nleSolver) {
-        if (isLinear())
+        if (isLinear() || localUnsolvedEquations().isEmpty())
             return "JMI_LINEAR_SOLVER";
         else if (nleSolver.equals("kinsol"))
             return "JMI_KINSOL_SOLVER";

trunk/Compiler/ModelicaCBackEnd/src/test/CADCodeGenTests.mo
@@ -5463,74 +5463,4 @@ equation
 ")})));
 end IfEqu4;
 
-model IfEqu5
-    Real x;
-    parameter Real y(fixed=false,start=3);
-initial equation
-    if time >= 1 then
-        (x,) = dummyFunc(1);
-    else
-        (x,) = dummyFunc(2);
-    end if;
-equation
-    when time > 1 then
-        x = 1;
-    end when;
-
-    annotation(__JModelica(UnitTesting(tests={
-        CADCodeGenTestCase(
-            name="IfEqu5",
-            description="Code generation for if equation, initial equation",
-            variability_propagation=false,
-            inline_functions="none",
-            generate_dae_jacobian=true,
-            generate_ode_jacobian=true,
-            template="
-$CAD_ode_derivatives$
-$CAD_dae_blocks_residual_functions$
-",
-            generatedCode="
-/******** Declarations *******/
-
-jmi_real_t** dz = jmi->dz;
-    /*********** ODE section ***********/
-    /*********** Real outputs **********/
-    /*** Integer and boolean outputs ***/
-    /********* Other variables *********/
-    jmi_ode_unsolved_block_dir_der(jmi, jmi->dae_block_residuals[0]);
-
-static int dae_block_dir_der_0(jmi_t* jmi, jmi_real_t* x, jmi_real_t* dx,jmi_real_t* residual, jmi_real_t* dRes, int evaluation_mode) {
-    /***** Block: 1 *****/
-    jmi_ad_var_t v_0;
-    jmi_ad_var_t v_1;
-    jmi_real_t** res = &residual;
-    int ef = 0;
-    jmi_real_t** dF = &dRes;
-    jmi_real_t** dz;
-    if (evaluation_mode == JMI_BLOCK_INITIALIZE) {
-        x[0] = _x_0;
-        return 0;
-    } else if (evaluation_mode == JMI_BLOCK_EVALUATE) {
-        dz = jmi->dz_active_variables;
-        (*dz)[ jmi_get_index_from_value_ref(2)-jmi->offs_real_dx] = dx[0];
-        _x_0 = x[0];
-    } else if (evaluation_mode == JMI_BLOCK_EVALUATE_INACTIVE) {
-        dz = jmi->dz;
-    } else if (evaluation_mode == JMI_BLOCK_WRITE_BACK) {
-        dz = jmi->dz;
-        (*dz)[jmi_get_index_from_value_ref(2)-jmi->offs_real_dx] = -(*dF)[0];
-    } else {
-        return -1;
-    }
-    if (evaluation_mode == JMI_BLOCK_EVALUATE_INACTIVE || evaluation_mode == JMI_BLOCK_EVALUATE) {
-        (*res)[0] = COND_EXP_EQ(LOG_EXP_AND(_temp_1_2, LOG_EXP_NOT(pre_temp_1_2)), JMI_TRUE, AD_WRAP_LITERAL(1), pre_x_0) - (_x_0);
-        (*dz)[jmi_get_index_from_value_ref(2)-jmi->offs_real_dx] = 0;
-    }
-    return ef;
-}
-
-
-")})));
-end IfEqu5;
-
 end CADCodeGenTests;

commit 283548318be7134a0b9abc5363904796a1a8edae

Fixed crash bug that was introduced in r7591. It should now be possible to match noncontinuous variables to continuous equations in the initial system. Ticket #4364.

trunk/Compiler/ModelicaMiddleEnd/src/jastadd/structural/BLT.jrag
@@ -108,7 +108,7 @@ aspect BLT{
             if (seb != null && seb.isSolvable())
                 return seb;
             
-            BiPGraph g = new BiPGraph(component.getMembers());
+            BiPGraph g = new BiPGraph(component.getMembers(), isInitial);
             Collection<SCCBlock<Eq, Var>> subBlocks = g.tarjan(options);
             
             errorCheckWhenEquations(subBlocks);
@@ -198,7 +198,7 @@ aspect BLT{
                 if (seb != null && !mixed) {
                     block = seb;
                 } else {
-                    BiPGraph nonRealG = new BiPGraph(nonRealEquations);
+                    BiPGraph nonRealG = new BiPGraph(nonRealEquations, isInitial);
                     Collection<SimpleEquationBlock> nonRealBlocks = nonRealG.computeNonRealBlocks(producer, options);
                     eqnBlock = new EquationBlock(producer, component.computeBlockDependency(), nonRealBlocks, createSimpleBlocks(groupComponent(realEquations), producer));
                     block = eqnBlock;
@@ -2850,7 +2850,7 @@ aspect AutomaticTearing {
             }
             
             private <E extends AbstractEq<E, V>, V extends AbstractVar<E, V>> void tear(SCCBlock<E, V> block, TornEquationBlock eb, boolean isInitial) {
-                SolvingBiPGraph g = new SolvingBiPGraph(block.getMembers());
+                SolvingBiPGraph g = new SolvingBiPGraph(block.getMembers(), isInitial);
                 if (isInitial)
                     g.setAsInitialSystem();
                 
@@ -2925,7 +2925,7 @@ aspect AutomaticTearing {
                             }
                         } else {
                             // Build Inner Graph and add info about iteration variables
-                            SolvingBiPGraph gInner = new SolvingBiPGraph(bInner.getMembers());
+                            SolvingBiPGraph gInner = new SolvingBiPGraph(bInner.getMembers(), isInitial);
                             // Build block-graph and look for interchangeable variables in SUMV
                             java.util.List<SolvingEq> eqs = new ArrayList<SolvingEq>();
                             boolean graphHasTear = false;
@@ -2978,11 +2978,11 @@ aspect AutomaticTearing {
                                         if (tearBlock != null && tearBlock.isSolvable()) {
                                             eb.addSolvedBlock(tearBlock);
                                         } else {
-                                            tearInner(new SolvingBiPGraph(bInnerInner.getMembers()), eb);
+                                            tearInner(new SolvingBiPGraph(bInnerInner.getMembers(), isInitial), eb);
                                         }
                                     }
                                 } else {
-                                    tearInner(new SolvingBiPGraph(bInner.getMembers()), eb);
+                                    tearInner(new SolvingBiPGraph(bInner.getMembers(), isInitial), eb);
                                 }
                             } else {
                                 // Reuse the already constructed graph
@@ -3020,7 +3020,7 @@ aspect AutomaticTearing {
                     if (tearBlock != null && tearBlock.isSolvable()) {
                         eb.addSolvedBlock(tearBlock);
                     } else {
-                        tearInner(new SolvingBiPGraph(block.getMembers()), eb);
+                        tearInner(new SolvingBiPGraph(block.getMembers(), g.isInitialSystem()), eb);
                     }
                 }
             }

trunk/Compiler/ModelicaMiddleEnd/src/jastadd/structural/Graphs.jadd
@@ -60,8 +60,9 @@ public abstract class AbstractBiPGraph<E extends AbstractEq<E, V>, V extends Abs
         }
     }
 
-    public <E_OTHER extends AbstractEq<E_OTHER, V_OTHER>, V_OTHER extends AbstractVar<E_OTHER, V_OTHER>> AbstractBiPGraph(Collection<E_OTHER> block) {
+    public <E_OTHER extends AbstractEq<E_OTHER, V_OTHER>, V_OTHER extends AbstractVar<E_OTHER, V_OTHER>> AbstractBiPGraph(Collection<E_OTHER> block, boolean isInitial) {
         this(block, collectMatchingVariables(block));
+        initialSystem = isInitial;
         for (E_OTHER e : block) {
             if (!e.isMeta()) {
                 match(getEquation(e.getName()), getVariable(e.getMatching().getName()));
@@ -1228,8 +1229,8 @@ public class BiPGraph extends AbstractBiPGraph<Eq, Var, SCCBlock<Eq, Var>> {
         super();
     }
 
-    public <E extends AbstractEq<E, V>, V extends AbstractVar<E, V>> BiPGraph(Collection<E> block) {
-        super(block);
+    public <E extends AbstractEq<E, V>, V extends AbstractVar<E, V>> BiPGraph(Collection<E> block, boolean isInitial) {
+        super(block, isInitial);
     }
 
     public <E extends AbstractEq<E, V>, V extends AbstractVar<E, V>> BiPGraph(Collection<E> eqns, Collection<V> vars) {
@@ -1275,8 +1276,8 @@ public class SolvingBiPGraph extends AbstractBiPGraph<SolvingEq, SolvingVar, SCC
         super(eqns, vars);
     }
 
-    public <E extends AbstractEq<E, V>, V extends AbstractVar<E, V>> SolvingBiPGraph(Collection<E> block) {
-        super(block);
+    public <E extends AbstractEq<E, V>, V extends AbstractVar<E, V>> SolvingBiPGraph(Collection<E> block, boolean isInitial) {
+        super(block, isInitial);
     }
 
     @Override

trunk/Compiler/ModelicaMiddleEnd/src/test/HybridModelTests.mo
@@ -476,5 +476,29 @@ Discrete equations:
 -------------------------------
 ")})));
     end NoResTest1;
+    
+    model MixedVariabilityMatch1
+        parameter Real a(fixed=false);
+        Real b;
+    initial equation
+        b = 1;
+    equation
+        b = sin(a + time);
+        
+    annotation(__JModelica(UnitTesting(tests={
+        FClassMethodTestCase(
+            name="HybridModelTests.MixedVariabilityMatch1",
+            description="Verify that a noncontinuous variable can be matched to and continuous equation in the initial system",
+            methodName="printDAEInitBLT",
+            methodResult="
+--- Solved equation ---
+b := 1
+
+--- Unsolved equation (Block 1) ---
+b = sin(a + time)
+  Computed variables: a
+-------------------------------
+")})));
+    end MixedVariabilityMatch1;
 
 end HybridModelTests;