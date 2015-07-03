jnorthrup/jmodelica

commit ed7f3309f7605e13b9a116c8c9b75ea1ce674c56

#4443 Fixes scalarization of nested fiterexps. Fixes parameter array index bugs.
git-svn-id: https://svn.jmodelica.org@7779 7ae51c65-13d5-4921-9d80-6511ddd1419c

trunk/Compiler/ModelicaFlatTree/src/jastadd/Variability.jrag
@@ -30,6 +30,8 @@ aspect Variability {
     eq FFinalParameter.finalParameterVariability() = true;
     syn boolean FTypePrefixVariability.knownParameterVariability() = false;
     eq FKnownParameter.knownParameterVariability() = true;
+    syn boolean FTypePrefixVariability.indexParameterVariability() = false;
+    eq FIndexParameter.indexParameterVariability() = true;
     syn boolean FTypePrefixVariability.parameterVariability() = false;
     eq FParameter.parameterVariability() = true;	
     syn boolean FTypePrefixVariability.discreteVariability() = false;
@@ -53,12 +55,13 @@ aspect Variability {
     syn boolean FTypePrefixVariability.lessOrEqual(FTypePrefixVariability other) = 
             compareTo(other) <= 0;
 	
-    syn boolean FTypePrefixVariability.evalOrLess()       = lessOrEqual(fEvalParameter());
+    syn boolean FTypePrefixVariability.evalOrLess()           = lessOrEqual(fEvalParameter());
     syn boolean FTypePrefixVariability.knownParameterOrLess() = lessOrEqual(fFinalParameter());
-    syn boolean FTypePrefixVariability.parameterOrLess()  = lessOrEqual(fNonFixedParameter());
+    syn boolean FTypePrefixVariability.parameterOrLess()      = lessOrEqual(fNonFixedParameter());
+    syn boolean FTypePrefixVariability.indexParameterOrLess() = lessOrEqual(fIndexParameter());
     syn boolean FTypePrefixVariability.fixedParameterOrLess() = lessOrEqual(fFixedParameter());
-    syn boolean FTypePrefixVariability.discreteOrLess()   = lessOrEqual(fDiscrete());
-    syn boolean FTypePrefixVariability.continuousOrLess() = lessOrEqual(fContinuous());
+    syn boolean FTypePrefixVariability.discreteOrLess()       = lessOrEqual(fDiscrete());
+    syn boolean FTypePrefixVariability.continuousOrLess()     = lessOrEqual(fContinuous());
 
     /**
      * An ordering of the variability types.
@@ -74,19 +77,21 @@ aspect Variability {
     protected int FStructParameter.variabilityLevel() { return VARIABILITY_LEVEL; }
     protected int FEvalParameter.variabilityLevel()   { return VARIABILITY_LEVEL; }
     protected int FFinalParameter.variabilityLevel()  { return VARIABILITY_LEVEL; }
+    protected int FIndexParameter.variabilityLevel()  { return VARIABILITY_LEVEL; }
     protected int FFixedParameter.variabilityLevel()  { return VARIABILITY_LEVEL; }
     protected int FNonFixedParameter.variabilityLevel() { return VARIABILITY_LEVEL; }
     protected int FDiscrete.variabilityLevel()        { return VARIABILITY_LEVEL; }
     protected int FContinuous.variabilityLevel()      { return VARIABILITY_LEVEL; }
     protected int FCompositeVariability.variabilityLevel() { return combine().variabilityLevel(); }
-    protected static final int FConstant.VARIABILITY_LEVEL        = 0;
-    protected static final int FEvalParameter.VARIABILITY_LEVEL   = 3;
-    protected static final int FStructParameter.VARIABILITY_LEVEL = 4;
-    protected static final int FFinalParameter.VARIABILITY_LEVEL  = 5;
-    protected static final int FFixedParameter.VARIABILITY_LEVEL  = 10;
+    protected static final int FConstant.VARIABILITY_LEVEL          = 0;
+    protected static final int FEvalParameter.VARIABILITY_LEVEL     = 3;
+    protected static final int FStructParameter.VARIABILITY_LEVEL   = 4;
+    protected static final int FFinalParameter.VARIABILITY_LEVEL    = 5;
+    protected static final int FIndexParameter.VARIABILITY_LEVEL    = 8;
+    protected static final int FFixedParameter.VARIABILITY_LEVEL    = 10;
     protected static final int FNonFixedParameter.VARIABILITY_LEVEL = 11;
-    protected static final int FDiscrete.VARIABILITY_LEVEL        = 20;
-    protected static final int FContinuous.VARIABILITY_LEVEL      = 30;
+    protected static final int FDiscrete.VARIABILITY_LEVEL          = 20;
+    protected static final int FContinuous.VARIABILITY_LEVEL        = 30;
     
     /**
      * Combines (up) component variabilities to a single primitive variability.
@@ -656,6 +661,7 @@ aspect VariabilitySingletons {
     public static final FDiscrete        FDiscrete.instance        = new FDiscrete();
     public static final FNonFixedParameter FNonFixedParameter.instance = new FNonFixedParameter();
     public static final FFixedParameter  FFixedParameter.instance  = new FFixedParameter();
+    public static final FIndexParameter  FIndexParameter.instance  = new FIndexParameter();
     public static final FFinalParameter  FFinalParameter.instance  = new FFinalParameter();
     public static final FEvalParameter   FEvalParameter.instance   = new FEvalParameter();
     public static final FStructParameter FStructParameter.instance = new FStructParameter();
@@ -682,6 +688,10 @@ aspect VariabilitySingletons {
     
     public static FParameter ASTNode.fFixedParameter() {
         return FFixedParameter.instance;
+    }
+    
+    public static FParameter ASTNode.fIndexParameter() {
+        return FIndexParameter.instance;
     }
     
     public static FFinalParameter ASTNode.fFinalParameter() {

trunk/Compiler/ModelicaFrontEnd/src/jastadd/flattening/Flattening.jrag
@@ -1698,7 +1698,7 @@ aspect FlatExpressions {
             int i = parent.myCallInputs().indexOf(var);
             int d = findDepthOfNestledFunctionCalls();
             return new FArgumentExp(i, d);
-        } else if (!inFunction() && variability().extractNonParameterIndices(getInstAccess().indexVariability())) {
+        } else if (!inFunction() && variability().flattenExtractIndices(getInstAccess().indexVariability())) {
             FInstAccessExp copy = (FInstAccessExp) dynamicFExp(fullCopy());
             List<FSubscript> sub = new List<FSubscript>();
             copy.getInstAccess().extractNonParameterIndices(sub, variability());
@@ -1792,7 +1792,7 @@ aspect FlatExpressions {
         FArraySubscripts fas = getFArraySubscripts();
         for (int i = 0, n = fas.getNumFSubscript(); i < n; i++) {
             FSubscript fs = fas.getFSubscript(i);
-            if (v.extractNonParameterIndices(fs.variability())) {
+            if (v.flattenExtractIndices(fs.variability())) {
                 sub.add(fs);
                 fas.setFSubscript(new FColonSubscript(), i);
             } else if (fs.ndims() == 1) {
@@ -1815,9 +1815,9 @@ aspect FlatExpressions {
         }
     }
     
-    syn boolean FTypePrefixVariability.extractNonParameterIndices(FTypePrefixVariability other) = !other.fixedParameterOrLess();
-    eq FConstant.extractNonParameterIndices(FTypePrefixVariability other) = !other.knownParameterOrLess();
-    eq FKnownParameter.extractNonParameterIndices(FTypePrefixVariability other) = !other.knownParameterOrLess();
+    syn boolean FTypePrefixVariability.flattenExtractIndices(FTypePrefixVariability other) = !other.indexParameterOrLess();
+    eq FConstant.flattenExtractIndices(FTypePrefixVariability other) = !other.knownParameterOrLess();
+    eq FKnownParameter.flattenExtractIndices(FTypePrefixVariability other) = !other.knownParameterOrLess();
 
 	/**
 	 * Check if this access is in the binding expression of an input to a function 
@@ -2516,18 +2516,18 @@ aspect FlatExpressions {
         else 
             return prefix.copyAndAppend(name());
     }
-
-	/**
-	 * Flatten variability specifier.
+    
+    /**
+     * Flatten variability specifier.
      *
-	 * This is done during variability calculation to keep compatibility with FExp. 
-	 */
-	abstract public FTypePrefixVariability TypePrefixVariability.flatten();
-	public FTypePrefixVariability Continuous.flatten() { return new FContinuous(); }
-	public FTypePrefixVariability Discrete.flatten()   { return new FDiscrete();   }
-	public FTypePrefixVariability Parameter.flatten()  { return new FFixedParameter();  }
-	public FTypePrefixVariability Constant.flatten()   { return new FConstant();   }
-
+     * This is done during variability calculation to keep compatibility with FExp. 
+     */
+    abstract public FTypePrefixVariability TypePrefixVariability.flatten();
+    public FTypePrefixVariability Continuous.flatten()     { return new FContinuous();     }
+    public FTypePrefixVariability Discrete.flatten()       { return new FDiscrete();       }
+    public FTypePrefixVariability Parameter.flatten()      { return new FFixedParameter(); }
+    public FTypePrefixVariability IndexParameter.flatten() { return fIndexParameter();     }
+    public FTypePrefixVariability Constant.flatten()       { return new FConstant();       }
 }
 
 aspect InstantiatedExpressions {

trunk/Compiler/ModelicaFrontEnd/src/test/ArrayTests.mo
@@ -1216,9 +1216,20 @@ fclass ArrayTests.General.ArrayTest45
  Real b.x[2];
  parameter Integer b.j = 1 /* 1 */;
 equation
- b.x[1] = 1.0 + time;
- b.x[2] = 2.0 + 2 * time;
-end ArrayTests.General.ArrayTest45;
+ b.x[1] = temp_1(b.j, 1, {{1.0, 2.0}, {3.0, 4.0}}) + time;
+ b.x[2] = temp_1(b.j, 2, {{1.0, 2.0}, {3.0, 4.0}}) + 2 * time;
+
+public
+ function temp_1
+  input Integer i_0;
+  input Integer i_1;
+  input Real[2, 2] x;
+  output Real y;
+ algorithm
+  y := x[i_0,i_1];
+  return;
+ annotation(Inline = false);
+ end temp_1;end ArrayTests.General.ArrayTest45;
 ")})));
 end ArrayTest45;
 
@@ -6063,7 +6074,7 @@ equation
  r.t[1] = time;
  r.t[2] = time;
  r.x[1,1] = r.t[1] + r.t[1];
- r.x[2,1] = r.t[1] + r.t[1];
+ r.x[2,1] = r.t[1] + r.t[2];
 end ArrayTests.Constructors.Iterators.ArrayIterTest12;
 ")})));
 end ArrayIterTest12;
@@ -6325,7 +6336,7 @@ fclass ArrayTests.For.ForStructural1
  Real x[2];
 equation
  for i in 1:2 loop
-if ({true, false})[i] then
+  if ({true, false})[i] then
    x[i] = time;
   else
    x[i] = 1;
@@ -7482,7 +7493,20 @@ model ArrayConst4
 			flatModel="
 fclass ArrayTests.Other.ArrayConst4
  parameter Integer i = 1 /* 1 */;
- constant Real x = 1.0;
+ parameter Real x;
+parameter equation
+ x = temp_1(i, {1.0, 2.0});
+
+public
+ function temp_1
+  input Integer i_0;
+  input Real[2] x;
+  output Real y;
+ algorithm
+  y := x[i_0];
+  return;
+ annotation(Inline = false);
+ end temp_1;
 end ArrayTests.Other.ArrayConst4;
 ")})));
 end ArrayConst4;

trunk/Compiler/ModelicaFlatTree/src/jastadd/ast/FlatModelica.ast
@@ -161,6 +161,8 @@ FNonFixedParameter : FParameter;
 
 FFixedParameter : FParameter;
 
+FIndexParameter : FParameter;
+
 /**
  * Parameter with known value variability
  */

trunk/Compiler/ModelicaFrontEnd/src/jastadd/ast/Modelica.ast
@@ -257,6 +257,7 @@ abstract TypePrefixVariability : TypePrefix;
 Continuous : TypePrefixVariability;
 Discrete   : TypePrefixVariability;
 Parameter  : TypePrefixVariability;
+IndexParameter : Parameter;
 Constant   : TypePrefixVariability;
 abstract TypePrefixInputOutput : TypePrefix;
 Input      : TypePrefixInputOutput;

trunk/Compiler/ModelicaFrontEnd/src/parser/Modelica.parser
@@ -880,7 +880,7 @@ ForIndex for_index =
                                                               new Opt(),
                                                               new Opt(),
                                                               new Opt(),
-                                                              new Opt(new Parameter()),
+                                                              new Opt(new IndexParameter()),
                                                               new Opt(),
                                                               new ParseAccess("Integer",new Opt()),
                                                               new Opt(),

trunk/Compiler/ModelicaFrontEnd/src/test/InnerOuterTests.mo
@@ -466,7 +466,7 @@ model InnerOuterTest16
 fclass InnerOuterTests.InnerOuterTest16
  Real x[3] = {1, 2, 3} * time;
  parameter Integer a.y = 2 /* 2 */;
- Real a.z = x[a.y];
+ Real a.z = (x[1:3])[a.y];
 end InnerOuterTests.InnerOuterTest16;
 ")})));
 end InnerOuterTest16;

trunk/Compiler/ModelicaFrontEnd/src/test/NameTests.mo
@@ -2840,8 +2840,8 @@ model IndexLookup1
 			flatModel="
 fclass NameTests.IndexLookup1
  parameter Integer i = 2 /* 2 */;
- Real y.z[2] = {1,2};
- Real x = y.z[i];
+ Real y.z[2] = {1, 2};
+ Real x = (y.z[1:2])[i];
 
 end NameTests.IndexLookup1;
 ")})));
@@ -2866,8 +2866,8 @@ model IndexLookup2
 fclass NameTests.IndexLookup2
  parameter Integer i = 2 /* 2 */;
  parameter Integer y.i = 1 /* 1 */;
- Real y.z[2] = {1,2};
- Real x = y.z[i];
+ Real y.z[2] = {1, 2};
+ Real x = (y.z[1:2])[i];
 
 end NameTests.IndexLookup2;
 ")})));

commit 04dd2df44de0d741195cc290f75d542dc6ff6245

#4454 Fixed problems introduced in r7779 exposed by r7791. Introdeues symbolic simplification of if expressions in flattening. Fixes variability calculation bug for FSubscriptedExp.

trunk/Compiler/ModelicaFlatTree/src/jastadd/Variability.jrag
@@ -414,6 +414,9 @@ aspect Variability {
     eq FInstAccessExp.variability() = getInstAccess().myInstComponentDecl().variability();
 
     eq FUnsupportedExp.expVariability() = fContinuous(); // Only here to avoid null pointer 
+    
+    eq FSubscriptedExp.expVariability() = super.expVariability().combine(indexVariability());
+    
 
     public static FTypePrefixVariability FExp.combineFExpListVariability(Iterable<? extends FExp> exps) {
         Iterator<? extends FExp> it = exps.iterator();

trunk/Compiler/ModelicaFrontEnd/src/jastadd/flattening/Flattening.jrag
@@ -2178,6 +2178,15 @@ aspect FlatExpressions {
 	}
 
     public FExp FIfExp.flatten(FQName prefix) {
+        if (getIfExpNoTransform().variability().knownParameterOrLess()) {
+            try {
+                return getIfExpNoTransform().ceval().booleanValue() ? 
+                        getThenExp().flatten(prefix) : 
+                        getElseExp().flatten(prefix);
+            } catch (ConstantEvaluationException e) {
+                
+            }
+        }
 		return new FIfExp(getIfExp().flatten(prefix),
 		                  getThenExp().flatten(prefix),
 		                  getElseExp().flatten(prefix));

trunk/Compiler/ModelicaFlatTree/src/test/EvaluationTests.mo
@@ -3907,7 +3907,7 @@ model ParameterMinMax1
             flatModel="
 fclass EvaluationTests.ParameterMinMax1
  structural parameter Integer n(min = 1) = 1 /* 1 */;
- Real x[1] = if 1 < 2 then {2} else 1:1;
+ Real x[1] = {2};
 end EvaluationTests.ParameterMinMax1;
 ")})));
 end ParameterMinMax1;
@@ -3924,7 +3924,7 @@ model ParameterMinMax2
             flatModel="
 fclass EvaluationTests.ParameterMinMax2
  structural parameter Integer n(min = 1,start = 2) = 2 /* 2 */;
- Real x[2] = if 2 < 2 then {2} else 1:2;
+ Real x[2] = 1:2;
 end EvaluationTests.ParameterMinMax2;
 ")})));
 end ParameterMinMax2;
@@ -3941,7 +3941,7 @@ model ParameterMinMax3
             flatModel="
 fclass EvaluationTests.ParameterMinMax3
  structural parameter Integer n(max = -1) = -1 /* -1 */;
- Real x[1] = if -1 > -2 then {2} else 1:1;
+ Real x[1] = {2};
 end EvaluationTests.ParameterMinMax3;
 ")})));
 end ParameterMinMax3;
@@ -3958,7 +3958,7 @@ model ParameterMinMax4
             flatModel="
 fclass EvaluationTests.ParameterMinMax4
  structural parameter Integer n(max = -1,start = -2) = -2 /* -2 */;
- Real x[2] = if -2 > -2 then {2} else 1:2;
+ Real x[2] = 1:2;
 end EvaluationTests.ParameterMinMax4;
 ")})));
 end ParameterMinMax4;

trunk/Compiler/ModelicaFrontEnd/src/test/ArrayTests.mo
@@ -6079,6 +6079,45 @@ end ArrayTests.Constructors.Iterators.ArrayIterTest12;
 ")})));
 end ArrayIterTest12;
 
+model ArrayIterTest13
+    Integer[:] x0 = 1:8;
+    constant Integer[:,:,:] x1 = {{{1,2},{3,4}},{{5,6},{7,8}}};
+    Integer[:,:,:] x2 = {{{x0[x1[i,j,k]] for i in 1:2} for j in 1:2} for k in 1:2};
+    
+    annotation(__JModelica(UnitTesting(tests={
+        TransformCanonicalTestCase(
+            name="Constructors_Iterators_ArrayIterTest13",
+            description="Nested iteration expressions",
+            flatModel="
+fclass ArrayTests.Constructors.Iterators.ArrayIterTest13
+ constant Integer x1[1,1,1] = 1;
+ constant Integer x1[1,1,2] = 2;
+ constant Integer x1[1,2,1] = 3;
+ constant Integer x1[1,2,2] = 4;
+ constant Integer x1[2,1,1] = 5;
+ constant Integer x1[2,1,2] = 6;
+ constant Integer x1[2,2,1] = 7;
+ constant Integer x1[2,2,2] = 8;
+ constant Integer x2[1,1,1] = 1;
+ constant Integer x2[1,1,2] = 5;
+ constant Integer x2[1,2,1] = 3;
+ constant Integer x2[1,2,2] = 7;
+ constant Integer x2[2,1,1] = 2;
+ constant Integer x2[2,1,2] = 6;
+ constant Integer x2[2,2,1] = 4;
+ constant Integer x2[2,2,2] = 8;
+ constant Integer x0[1] = 1;
+ constant Integer x0[2] = 2;
+ constant Integer x0[3] = 3;
+ constant Integer x0[4] = 4;
+ constant Integer x0[5] = 5;
+ constant Integer x0[6] = 6;
+ constant Integer x0[7] = 7;
+ constant Integer x0[8] = 8;
+end ArrayTests.Constructors.Iterators.ArrayIterTest13;
+")})));
+end ArrayIterTest13;
+
 model ArrayIterTestUnknown1
     function f
 		input Integer a;

trunk/Compiler/ModelicaFrontEnd/src/test/TypeTests.mo
@@ -2481,8 +2481,8 @@ model CircularIfExp1
             flatModel="
 fclass TypeTests.CircularIfExp1
  eval parameter Boolean a = true /* true */;
- parameter Real b = if true then 1 else c;
- parameter Real c = if true then b else 2;
+ parameter Real b = 1 /* 1 */;
+ parameter Real c = b;
 end TypeTests.CircularIfExp1;
 ")})));
 end CircularIfExp1;
@@ -2502,8 +2502,8 @@ model CircularIfExp2
 fclass TypeTests.CircularIfExp2
  eval parameter Boolean a = true /* true */;
  eval parameter Boolean b = true /* true */;
- parameter Real c = if true and true then 1 else d;
- parameter Real d = if true and true then c else 2;
+ parameter Real c = 1 /* 1 */;
+ parameter Real d = c;
 end TypeTests.CircularIfExp2;
 ")})));
 end CircularIfExp2;
@@ -2523,8 +2523,8 @@ model CircularIfExp3
 fclass TypeTests.CircularIfExp3
  eval parameter Boolean a = true /* true */;
  eval parameter Boolean b = true /* true */;
- parameter Real c = if true then 1 else d;
- parameter Real d = if true then c else 2;
+ parameter Real c = 1 /* 1 */;
+ parameter Real d = c;
 end TypeTests.CircularIfExp3;
 ")})));
 end CircularIfExp3;