jnorthrup/jmodelica

commit c909c78a0bab5e4e390bc67379fcb3485f5fb5f1

#4552 Split generation of c-functions model_ode_derivatives and model_ode_initialize
git-svn-id: https://svn.jmodelica.org@7967 7ae51c65-13d5-4921-9d80-6511ddd1419c

trunk/Compiler/GenericCodeGen/src/jastadd/DAETag.jrag
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 /*
     Copyright (C) 2009 Modelon AB
 
@@ -115,7 +117,7 @@ public abstract class DAETag extends AbstractTag {
 		instantiateTags(getClass(), map, this, myGenerator, fclass);
 	}
 
-    public abstract class CodeSplitter<T extends ASTNode> {
+    public abstract class CodeSplitter<T> {
         private int itemIndex = 0;
         private int itemLimit = 1000;
         private int splitCount = 0;
@@ -149,27 +151,44 @@ public abstract class DAETag extends AbstractTag {
         
         private int numSplits() {
             int s = elements.size();
-            if (s > 0) {
+            if (s < itemLimit) {
+                s = 0;
+            } else if (s > 0) {
                 s = 1 + ((s-1) / itemLimit);
             }
             return s;
         }
         
+        public void printStatusDecl() {
+            str.print(indent + "int ef = 0;\n");
+        }
+        
+        public void printStatusReturn() {
+            str.print(indent + "return ef;\n");
+        }
+        
         public void genFuncs() {
             for (int split = 0; split < numSplits(); split++) {
-                str.print("void ");
+                str.print("int ");
                 str.print(funcName);
                 str.print(split);
                 str.print("(jmi_t* jmi) {\n");
+                printStatusDecl();
                 int n = Math.min(itemLimit, elements.size()-split*itemLimit);
                 gen(split*itemLimit, n);
+                printStatusReturn();
                 str.print("}\n\n");
             }
         }
         
         public void genCalls() {
-            for (int split = 0; split < numSplits(); split++) {
+            int n = numSplits();
+            if (n == 0) {
+                gen(0, elements.size());
+            }
+            for (int split = 0; split < n; split++) {
                 str.print(indent);
+                str.print("ef |= ");
                 str.print(funcName);
                 str.print(split);
                 str.print("(jmi);\n");
@@ -178,13 +197,14 @@ public abstract class DAETag extends AbstractTag {
         
         protected void gen(int start, int n) {
             for (int i = start; i < start + n; i++) {
-                p.printVarDecls(elements.get(i), str, indent);
+                genDecl(elements.get(i));
             }
             for (int i = start; i < start + n; i++) {
                 gen(elements.get(i));
             }
         }
         
+        public abstract void genDecl(T element);
         public abstract void gen(T element);
     }
 

trunk/Compiler/ModelicaCBackEnd/src/jastadd/codegen/CGenerator.jrag
@@ -316,6 +316,10 @@ public class CGenerator extends GenericGenerator {
             CodeSplitter<FAbstractEquation> cs1 = new CodeSplitter<FAbstractEquation>(p, str, next, 
                     "model_init_eval_parameters_", fclass.getFParameterEquations().toArrayList()) {
                 @Override
+                public void genDecl(FAbstractEquation element) {
+                    p.printVarDecls(element, str, indent);
+                }
+                @Override
                 public void gen(FAbstractEquation element) {
                     element.genAssignment_C(p, str, indent);
                 }
@@ -323,8 +327,10 @@ public class CGenerator extends GenericGenerator {
             
             cs1.genFuncs();
             
-            str.print("void model_init_eval_parameters_base(jmi_t* jmi) {\n");
+            str.print("int model_init_eval_parameters_base(jmi_t* jmi) {\n");
+            cs1.printStatusDecl();
             cs1.genCalls();
+            cs1.printStatusReturn();
             str.print("}\n");
 		}
 	
@@ -656,16 +662,51 @@ public class CGenerator extends GenericGenerator {
 		
 		public DAETag_C_ode_derivatives(AbstractGenerator myGenerator, FClass fclass) {
 			super("C_ode_derivatives", myGenerator, fclass);
-            addOptions("generate_ode");
 		}
 
-		public void generate(CodeStream genPrinter) {
+        public void generate(CodeStream str) {
 			if (fclass.onlyInitBLT()) {
-				genPrinter.print(INDENT + "ef = model_ode_initialize(jmi);\n");
+				str.print(INDENT + "ef = model_ode_initialize(jmi);\n");
 			} else {
-				fclass.getDAEStructuredBLT().genVarDecls_C(ASTNode.printer_C, genPrinter, INDENT);
-				genPrinter.print(INDENT + "model_ode_guards(jmi);\n");
-				fclass.getDAEStructuredBLT().genOdeDerivativeBlocks(ASTNode.printer_C, genPrinter, INDENT);
+                CodePrinter p = ASTNode.printer_C.initialSystemPrinter();
+                String indent = "";
+                String next = p.indent(indent);
+                
+                if (!fclass.myOptions().getBooleanOption("generate_ode")) {
+                    str.print("int model_ode_derivatives_base(jmi_t* jmi) {\n");
+                    str.print(next);
+                    str.print("return 0;");
+                    str.print("}\n");
+                    return;
+                }
+                
+                CodeSplitter<AbstractEquationBlock> cs = new CodeSplitter<AbstractEquationBlock>(p, str, next, "model_ode_derivatives_") {
+                    @Override
+                    public void genDecl(AbstractEquationBlock element) {
+                        element.genVarDecls_C(p, str, indent);
+                    }
+                    @Override
+                    public void gen(AbstractEquationBlock element) {
+                        element.genBlock_C(p, str, indent, false);
+                    }
+                };
+                
+                java.util.List<AbstractEquationBlock> blocks = fclass.getDAEStructuredBLT().getAllBlocks();
+                cs.add(blocks);
+                cs.genFuncs();
+                
+                str.print("int model_ode_derivatives_base(jmi_t* jmi) {\n");
+                cs.printStatusDecl();
+                for (AbstractEquationBlock eb : blocks) {
+                    eb.genReinitTempInits_C(p, str, next);
+                }
+                cs.genCalls();
+                for (AbstractEquationBlock eb : blocks) {
+                    eb.genReinitWritebacks_C(p, next, str);
+                }
+                cs.printStatusReturn();
+                str.print("}\n");
+                
 			}
 		}
 	}
@@ -680,17 +721,43 @@ public class CGenerator extends GenericGenerator {
 		
 		public DAETag_C_ode_initialization(AbstractGenerator myGenerator, FClass fclass) {
 			super("C_ode_initialization", myGenerator, fclass);
-            addOptions("generate_ode");
 		}
 
-		public void generate(CodeStream genPrinter) {
-            CodePrinter printer = ASTNode.printer_C.initialSystemPrinter();
-			fclass.getDAEInitBLT().genVarDecls_C(printer, genPrinter, INDENT);
-			genPrinter.print(INDENT + "model_ode_guards(jmi);\n");
-			for (AbstractEquationBlock block : fclass.getDAEInitBLT())
-				block.genBlock_C(printer, genPrinter, printer.indent(""), true);
-		}
-	}
+        public void generate(CodeStream str) {
+            
+            CodePrinter p = ASTNode.printer_C.initialSystemPrinter();
+            String indent = "";
+            String next = p.indent(indent);
+            
+            if (!fclass.myOptions().getBooleanOption("generate_ode")) {
+                str.print("int model_ode_initialize_base(jmi_t* jmi) {\n");
+                str.print(next);
+                str.print("return 0;");
+                str.print("}\n");
+                return;
+            }
+            
+            CodeSplitter<AbstractEquationBlock> cs = new CodeSplitter<AbstractEquationBlock>(p, str, next, "model_ode_initialize_") {
+                @Override
+                public void genDecl(AbstractEquationBlock element) {
+                    element.genVarDecls_C(p, str, indent);
+                }
+                @Override
+                public void gen(AbstractEquationBlock element) {
+                    element.genBlock_C(p, str, indent, true);
+                }
+            };
+            
+            cs.add(fclass.getDAEInitBLT());
+            cs.genFuncs();
+            
+            str.print("int model_ode_initialize_base(jmi_t* jmi) {\n");
+            cs.printStatusDecl();
+            cs.genCalls();
+            cs.printStatusReturn();
+            str.print("}\n");
+        }
+    }
 
 	/**
 	 * Generates code for solving the BLT blocks for computing the outputs
@@ -994,6 +1061,10 @@ public class CGenerator extends GenericGenerator {
             
             CodeSplitter<FVariable> cs1 = new CodeSplitter<FVariable>(p, str, next, "jmi_set_start_values_0_") {
                 @Override
+                public void genDecl(FVariable element) {
+                    p.printVarDecls(element, str, indent);
+                }
+                @Override
                 public void gen(FVariable element) {
                     element.genStartValue_C(p, str, indent);
                 }
@@ -1005,7 +1076,12 @@ public class CGenerator extends GenericGenerator {
             cs1.add(fclass.independentParameters());
             cs1.genFuncs();
             
+            
             CodeSplitter<FVariable> cs2 = new CodeSplitter<FVariable>(p, str, next, "jmi_set_start_values_1_") {
+                @Override
+                public void genDecl(FVariable element) {
+                    p.printVarDecls(element, str, indent);
+                }
                 @Override
                 public void gen(FVariable element) {
                     element.genStartValue_C(p, str, indent);
@@ -1020,11 +1096,13 @@ public class CGenerator extends GenericGenerator {
             cs2.add(fclass.discretePreVariables());
             cs2.genFuncs();
             
-            str.print("void jmi_set_start_values_base(jmi_t* jmi) {\n");
+            str.print("int jmi_set_start_values_base(jmi_t* jmi) {\n");
+            cs1.printStatusDecl();
             cs1.genCalls();
             str.print(next);
             str.print("model_init_eval_parameters(jmi);\n");
             cs2.genCalls();
+            cs1.printStatusReturn();
             str.print("}\n");
         }
 	}

trunk/Compiler/ModelicaCBackEnd/src/jastadd/CCodeGen.jrag
@@ -4985,16 +4985,20 @@ aspect CCodeGen {
 	
 	public void BLT.genVarDecls_C(CodePrinter p, CodeStream str, String indent) {
 		for (AbstractEquationBlock eb : getAllBlocks()) {
-			if (eb.isSolvable()) {
-				eb.genVarDecls_C(p, str, indent, null);
-			} else {
-                for (FRelExp relExp : eb.inactiveSwitches_C())
-                    p.printVarDecls(relExp, str, indent);
-                for (FRelExp relExp : eb.inactiveInitialSwitches_C())
-                    p.printVarDecls(relExp, str, indent);
-			}
+            eb.genVarDecls_C(p, str, indent);
 		}
 	}
+	
+    public void AbstractEquationBlock.genVarDecls_C(CodePrinter p, CodeStream str, String indent) {
+        if (isSolvable()) {
+            genVarDecls_C(p, str, indent, null);
+        } else {
+            for (FRelExp relExp : inactiveSwitches_C())
+                p.printVarDecls(relExp, str, indent);
+            for (FRelExp relExp : inactiveInitialSwitches_C())
+                p.printVarDecls(relExp, str, indent);
+        }
+    }
 
 	public void StructuredBLT.genOdeDerivativeBlocks(CodePrinter p, CodeStream str, String indent) {
         str.formatln("%s/********* Initialize reinits *******/", indent);

trunk/Compiler/ModelicaCBackEnd/templates/FMIBase/equ.c
@@ -28,10 +28,10 @@ $C_dae_add_blocks_residual_functions$
 $CAD_dae_add_blocks_residual_functions$
 }
 
-int model_ode_derivatives(jmi_t* jmi) {
-    int ef = 0;
 $C_ode_derivatives$
-    return ef;
+
+int model_ode_derivatives(jmi_t* jmi) {
+    return model_ode_derivatives_base(jmi);
 }
 
 int model_dae_R(jmi_t* jmi, jmi_real_t** res) {

trunk/Compiler/ModelicaCBackEnd/templates/FMIBase/equ_init.c
@@ -26,10 +26,10 @@ $C_dae_init_add_blocks_residual_functions$
 $CAD_dae_init_add_blocks_residual_functions$
 }
 
-int model_ode_initialize(jmi_t* jmi) {
-    int ef = 0;
 $C_ode_initialization$
-    return ef;
+
+int model_ode_initialize(jmi_t* jmi) {
+    return model_ode_initialize_base(jmi);
 }
 
 int model_init_R0(jmi_t* jmi, jmi_real_t** res) {

trunk/Compiler/ModelicaCBackEnd/templates/FMIBase/param.c
@@ -19,6 +19,5 @@ $C_enum_strings$
 $C_DAE_initial_dependent_parameter_assignments$
 
 int model_init_eval_parameters(jmi_t* jmi) {
-    model_init_eval_parameters_base(jmi);
-    return 0;
+    return model_init_eval_parameters_base(jmi);
 }

trunk/Compiler/ModelicaCBackEnd/templates/FMIBase/start.c
@@ -19,6 +19,5 @@ $C_enum_strings$
 $C_set_start_values$
 
 int jmi_set_start_values(jmi_t* jmi) {
-    jmi_set_start_values_base(jmi);
-    return 0;
+    return jmi_set_start_values_base(jmi);
 }

commit 775aad734e3cddf99e30d50fb5914b4e56208220

#4552 Fixes bug introduced in r7967

trunk/Compiler/GenericCodeGen/src/jastadd/DAETag.jrag
@@ -125,20 +125,22 @@ public abstract class DAETag extends AbstractTag {
         protected CodeStream str;
         protected CodePrinter p;
         protected String indent;
+        protected boolean allowDirect;
         
         private ArrayList<T> elements = new ArrayList<T>();
         
-        public CodeSplitter(CodePrinter p, CodeStream str, String indent, String funcName) {
-            this(p, str, indent, funcName, new ArrayList<T>());
+        public CodeSplitter(CodePrinter p, CodeStream str, String indent, boolean allowDirect, String funcName) {
+            this(p, str, indent, allowDirect, funcName, new ArrayList<T>());
         }
         
         public CodeSplitter(CodePrinter p, CodeStream str, String indent,
-                String funcName, ArrayList<T> elements) {
+                boolean allowDirect, String funcName, ArrayList<T> elements) {
             this.funcName = funcName;
             this.str = str;
             this.p = p;
             this.indent = indent;
             this.elements = elements;
+            this.allowDirect = allowDirect;
         }
         
         public Collection<T> getElements() {
@@ -155,7 +157,7 @@ public abstract class DAETag extends AbstractTag {
         
         private int numSplits() {
             int s = elements.size();
-            if (s < itemLimit) {
+            if (allowDirect && s < itemLimit) {
                 s = 0;
             } else if (s > 0) {
                 s = 1 + ((s-1) / itemLimit);

trunk/Compiler/ModelicaCBackEnd/src/jastadd/codegen/CGenerator.jrag
@@ -314,7 +314,7 @@ public class CGenerator extends GenericGenerator {
             String next = p.indent(indent);
             
             CodeSplitter<FAbstractEquation> cs1 = new CodeSplitter<FAbstractEquation>(p, str, next, 
-                    "model_init_eval_parameters_", fclass.getFParameterEquations().toArrayList()) {
+                    true, "model_init_eval_parameters_", fclass.getFParameterEquations().toArrayList()) {
                 @Override
                 public void genDecl(FAbstractEquation element) {
                     p.printVarDecls(element, str, indent);
@@ -669,7 +669,8 @@ public class CGenerator extends GenericGenerator {
             String indent = "";
             String next = p.indent(indent);
             
-            CodeSplitter<AbstractEquationBlock> cs = new CodeSplitter<AbstractEquationBlock>(p, str, next, "model_ode_derivatives_") {
+            CodeSplitter<AbstractEquationBlock> cs = new CodeSplitter<AbstractEquationBlock>(p, str, next, true,
+                    "model_ode_derivatives_") {
                 @Override
                 public void genDecl(AbstractEquationBlock element) {
                     element.genVarDecls_C(p, str, indent);
@@ -722,7 +723,8 @@ public class CGenerator extends GenericGenerator {
             String indent = "";
             String next = p.indent(indent);
             
-            CodeSplitter<AbstractEquationBlock> cs = new CodeSplitter<AbstractEquationBlock>(p, str, next, "model_ode_initialize_") {
+            CodeSplitter<AbstractEquationBlock> cs = new CodeSplitter<AbstractEquationBlock>(p, str, next, true,
+                    "model_ode_initialize_") {
                 @Override
                 public void genDecl(AbstractEquationBlock element) {
                     element.genVarDecls_C(p, str, indent);
@@ -1047,7 +1049,8 @@ public class CGenerator extends GenericGenerator {
             String indent = "";
             String next = p.indent(indent);
             
-            CodeSplitter<FVariable> cs1 = new CodeSplitter<FVariable>(p, str, next, "jmi_set_start_values_0_") {
+            CodeSplitter<FVariable> cs1 = new CodeSplitter<FVariable>(p, str, next, false,
+                    "jmi_set_start_values_0_") {
                 @Override
                 public void genDecl(FVariable element) {
                     p.printVarDecls(element, str, indent);
@@ -1058,14 +1061,13 @@ public class CGenerator extends GenericGenerator {
                 }
             };
             
-            
             cs1.add(fclass.independentConstants());
             cs1.add(fclass.dependentConstants());
             cs1.add(fclass.independentParameters());
             cs1.genFuncs();
             
-            
-            CodeSplitter<FVariable> cs2 = new CodeSplitter<FVariable>(p, str, next, "jmi_set_start_values_1_") {
+            CodeSplitter<FVariable> cs2 = new CodeSplitter<FVariable>(p, str, next, false,
+                    "jmi_set_start_values_1_") {
                 @Override
                 public void genDecl(FVariable element) {
                     p.printVarDecls(element, str, indent);

trunk/Compiler/ModelicaCBackEnd/src/test/CCodeGenTests.mo
@@ -1079,11 +1079,23 @@ int model_init_eval_parameters_base(jmi_t* jmi) {
     return ef;
 }
 
-int jmi_set_start_values_base(jmi_t* jmi) {
+int jmi_set_start_values_0_0(jmi_t* jmi) {
     int ef = 0;
     _x_0 = (1)/sf(0);
-    model_init_eval_parameters(jmi);
+    return ef;
+}
+
+int jmi_set_start_values_1_0(jmi_t* jmi) {
+    int ef = 0;
     _dummy_3 = (0.0)/sf(3);
+    return ef;
+}
+
+int jmi_set_start_values_base(jmi_t* jmi) {
+    int ef = 0;
+    ef |= jmi_set_start_values_0_0(jmi);
+    model_init_eval_parameters(jmi);
+    ef |= jmi_set_start_values_1_0(jmi);
     return ef;
 }
 ")})));
@@ -1205,7 +1217,7 @@ int model_init_eval_parameters_base(jmi_t* jmi) {
 }
 
 ---
-int jmi_set_start_values_base(jmi_t* jmi) {
+int jmi_set_start_values_0_0(jmi_t* jmi) {
     int ef = 0;
     _reg1_0 = (1);
     _reg2_1 = (1);
@@ -1226,6 +1238,12 @@ int jmi_set_start_values_base(jmi_t* jmi) {
     _eval3_17 = (1);
     _eval4_18 = (JMI_TRUE);
     _n_22 = (2);
+    return ef;
+}
+
+int jmi_set_start_values_base(jmi_t* jmi) {
+    int ef = 0;
+    ef |= jmi_set_start_values_0_0(jmi);
     model_init_eval_parameters(jmi);
     return ef;
 }
@@ -2394,11 +2412,15 @@ $C_DAE_initial_dependent_parameter_assignments$
 $C_DAE_initial_guess_equation_residuals$
 ",
             generatedCode="
-int jmi_set_start_values_base(jmi_t* jmi) {
+int jmi_set_start_values_0_0(jmi_t* jmi) {
     int ef = 0;
     _p1_1_0 = (1);
     _p1_2_1 = (2);
-    model_init_eval_parameters(jmi);
+    return ef;
+}
+
+int jmi_set_start_values_1_0(jmi_t* jmi) {
+    int ef = 0;
     JMI_ARR(STATREAL, jmi_ad_var_t, jmi_array_t, tmp_1, 2, 1)
     JMI_ARR(STATREAL, jmi_ad_var_t, jmi_array_t, tmp_2, 2, 1)
     JMI_ARRAY_INIT_1(STATREAL, jmi_ad_var_t, jmi_array_t, tmp_1, 2, 1, 2)
@@ -2411,6 +2433,14 @@ int jmi_set_start_values_base(jmi_t* jmi) {
     _w_5 = (func_CCodeGenTests_CFunctionTest15_f_exp0(tmp_2));
     _der_z_6 = (0.0);
     _der_w_7 = (0.0);
+    return ef;
+}
+
+int jmi_set_start_values_base(jmi_t* jmi) {
+    int ef = 0;
+    ef |= jmi_set_start_values_0_0(jmi);
+    model_init_eval_parameters(jmi);
+    ef |= jmi_set_start_values_1_0(jmi);
     return ef;
 }
 
@@ -13220,14 +13250,26 @@ equation
             variability_propagation=false,
             template="$C_set_start_values$",
             generatedCode="
-int jmi_set_start_values_base(jmi_t* jmi) {
+int jmi_set_start_values_0_0(jmi_t* jmi) {
     int ef = 0;
     _y_1 = (2);
     _z_2 = (3);
-    model_init_eval_parameters(jmi);
+    return ef;
+}
+
+int jmi_set_start_values_1_0(jmi_t* jmi) {
+    int ef = 0;
     _x_0 = (1);
     _q_3 = (0.0);
     _der_x_4 = (0.0);
+    return ef;
+}
+
+int jmi_set_start_values_base(jmi_t* jmi) {
+    int ef = 0;
+    ef |= jmi_set_start_values_0_0(jmi);
+    model_init_eval_parameters(jmi);
+    ef |= jmi_set_start_values_1_0(jmi);
     return ef;
 }
 ")})));
@@ -13255,12 +13297,16 @@ equation
             variability_propagation=false,
             template="$C_set_start_values$",
             generatedCode="
-int jmi_set_start_values_base(jmi_t* jmi) {
+int jmi_set_start_values_0_0(jmi_t* jmi) {
     int ef = 0;
     _pr_0 = (1.5);
     _pi_1 = (2);
     _pb_2 = (JMI_TRUE);
-    model_init_eval_parameters(jmi);
+    return ef;
+}
+
+int jmi_set_start_values_1_0(jmi_t* jmi) {
+    int ef = 0;
     _r_3 = (5.5);
     _i_4 = (10);
     _b_5 = (JMI_FALSE);
@@ -13269,6 +13315,14 @@ int jmi_set_start_values_base(jmi_t* jmi) {
     pre_i_4 = (10);
     pre_b_5 = (JMI_FALSE);
     pre_temp_1_6 = (0);
+    return ef;
+}
+
+int jmi_set_start_values_base(jmi_t* jmi) {
+    int ef = 0;
+    ef |= jmi_set_start_values_0_0(jmi);
+    model_init_eval_parameters(jmi);
+    ef |= jmi_set_start_values_1_0(jmi);
     return ef;
 }
 ")})));
@@ -15868,16 +15922,28 @@ $C_DAE_initial_dependent_parameter_assignments$
 $C_destruct_external_object$
 ",
             generatedCode="
-int jmi_set_start_values_base(jmi_t* jmi) {
+int jmi_set_start_values_0_0(jmi_t* jmi) {
     int ef = 0;
     _myEO1_0 = (func_CCodeGenTests_ExtObject_constructor_exp1());
     _myEO2_1 = (func_CCodeGenTests_ExtObject_constructor_exp1());
     _z1_8 = (5);
-    model_init_eval_parameters(jmi);
+    return ef;
+}
+
+int jmi_set_start_values_1_0(jmi_t* jmi) {
+    int ef = 0;
     _y1_4 = (0.0);
     _y2_5 = (0.0);
     _y3_6 = (0.0);
     _y4_7 = (0.0);
+    return ef;
+}
+
+int jmi_set_start_values_base(jmi_t* jmi) {
+    int ef = 0;
+    ef |= jmi_set_start_values_0_0(jmi);
+    model_init_eval_parameters(jmi);
+    ef |= jmi_set_start_values_1_0(jmi);
     return ef;
 }
 
@@ -16011,11 +16077,17 @@ $C_set_start_values$
 $C_destruct_external_object$
 ",
             generatedCode="
-int jmi_set_start_values_base(jmi_t* jmi) {
+int jmi_set_start_values_0_0(jmi_t* jmi) {
     int ef = 0;
     _r_eo_0 = (func_CCodeGenTests_ExtObject_constructor_exp1());
     _r_eos_1_1 = (func_CCodeGenTests_ExtObject_constructor_exp1());
     _r_eos_2_2 = (func_CCodeGenTests_ExtObject_constructor_exp1());
+    return ef;
+}
+
+int jmi_set_start_values_base(jmi_t* jmi) {
+    int ef = 0;
+    ef |= jmi_set_start_values_0_0(jmi);
     model_init_eval_parameters(jmi);
     return ef;
 }
@@ -16217,7 +16289,7 @@ const int fmi_runtime_options_map_length = 28;
 #define _time ((*(jmi->z))[jmi->offs_t])
 #define pre_x_0 ((*(jmi->z))[jmi->offs_pre_real_w+0])
 
-int jmi_set_start_values_base(jmi_t* jmi) {
+int jmi_set_start_values_0_0(jmi_t* jmi) {
     int ef = 0;
     __block_jacobian_check_1 = (JMI_FALSE);
     __block_jacobian_check_tol_2 = (1.0E-6);
@@ -16247,8 +16319,20 @@ int jmi_set_start_values_base(jmi_t* jmi) {
     __use_Brent_in_1d_26 = (JMI_TRUE);
     __use_jacobian_equilibration_27 = (JMI_FALSE);
     __use_newton_for_brent_28 = (JMI_TRUE);
-    model_init_eval_parameters(jmi);
+    return ef;
+}
+
+int jmi_set_start_values_1_0(jmi_t* jmi) {
+    int ef = 0;
     _x_0 = (0.0);
+    return ef;
+}
+
+int jmi_set_start_values_base(jmi_t* jmi) {
+    int ef = 0;
+    ef |= jmi_set_start_values_0_0(jmi);
+    model_init_eval_parameters(jmi);
+    ef |= jmi_set_start_values_1_0(jmi);
     return ef;
 }
 ")})));
@@ -18271,12 +18355,24 @@ $C_ode_initialization$
 ",
             generatedCode="
 set_start
-int jmi_set_start_values_base(jmi_t* jmi) {
+int jmi_set_start_values_0_0(jmi_t* jmi) {
     int ef = 0;
     _y_3 = (4);
-    model_init_eval_parameters(jmi);
+    return ef;
+}
+
+int jmi_set_start_values_1_0(jmi_t* jmi) {
+    int ef = 0;
     _x2_1 = (_z_4);
     _x3_2 = (1);
+    return ef;
+}
+
+int jmi_set_start_values_base(jmi_t* jmi) {
+    int ef = 0;
+    ef |= jmi_set_start_values_0_0(jmi);
+    model_init_eval_parameters(jmi);
+    ef |= jmi_set_start_values_1_0(jmi);
     return ef;
 }
 
@@ -18313,15 +18409,21 @@ model CFixedFalseParam3
             description="Test of C code generation of parameters with fixed = false. Check that start value is generated.",
             template="$C_set_start_values$",
             generatedCode="
-int jmi_set_start_values_base(jmi_t* jmi) {
+int jmi_set_start_values_1_0(jmi_t* jmi) {
     int ef = 0;
-    model_init_eval_parameters(jmi);
     JMI_ARR(STAT, jmi_ad_var_t, jmi_array_t, tmp_1, 3, 1)
     JMI_ARRAY_INIT_1(STAT, jmi_ad_var_t, jmi_array_t, tmp_1, 3, 1, 3)
     jmi_array_ref_1(tmp_1, 1) = AD_WRAP_LITERAL(1);
     jmi_array_ref_1(tmp_1, 2) = AD_WRAP_LITERAL(2);
     jmi_array_ref_1(tmp_1, 3) = AD_WRAP_LITERAL(3);
     _x1_0 = (func_CCodeGenTests_CFixedFalseParam3_f_exp0(tmp_1));
+    return ef;
+}
+
+int jmi_set_start_values_base(jmi_t* jmi) {
+    int ef = 0;
+    model_init_eval_parameters(jmi);
+    ef |= jmi_set_start_values_1_0(jmi);
     return ef;
 }
 ")})));