jnorthrup/jmodelica

commit 6d11ee3f85c12cc55e79de95208bfb2469aab0dd

#4565 Added another transformation step which eliminates identical function calls in the equation system.
git-svn-id: https://svn.jmodelica.org@8008 7ae51c65-13d5-4921-9d80-6511ddd1419c

trunk/Compiler/ModelicaMiddleEnd/src/jastadd/optimizations/CommonSubexpressionElimination.jrag
@@ -1,0 +1,203 @@
+/*
+    Copyright (C) 20015 Modelon AB
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, version 3 of the License.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+
+aspect CommonSubexpressionElimination {
+
+    public class FClass {
+        public class commonSubexpressionEliminationIfSet extends Transformation {
+            
+            Map<String, CSESet> m;
+            
+            public commonSubexpressionEliminationIfSet() {
+                super("common_subexp_elim");
+            }
+            
+            public void perform() {
+                m = new LinkedHashMap<String, CSESet>();
+                
+                getFAbstractEquations().collectFunctionCalls(this);
+                
+                boolean changed = false;
+                for (CSESet s : m.values()) {
+                    boolean res = s.eliminate();
+                    changed = changed || res;
+                }
+                
+                m = null;
+                
+                if (changed) {
+                    List<FAbstractEquation> l = new List<FAbstractEquation>();
+                    for (FAbstractEquation fae : getFAbstractEquations()) {
+                        if (!fae.markedForRemoval) {
+                            l.add(fae);
+                        }
+                    }
+                    
+                    setFAbstractEquationList(l);
+                    
+                    change();
+                }
+            }
+            
+            public void add(FFunctionCall fc) {
+                String s = fc.prettyPrint("");
+                CSESet l = m.get(s);
+                if (l == null) {
+                    l = new CSESet();
+                    m.put(s,l);
+                }
+                l.add(fc);
+            }
+            
+            public class CSESet extends ArrayList<FFunctionCall> {
+                private FExp repl = null;
+                private List<FFunctionCallLeft> res = null;
+                
+                public boolean eliminate() {
+                    if (size() > 1) {
+                        FFunctionCall fc = get(0);
+                        FFunctionCall fcCopy = (FFunctionCall) fc.copySymbolic();
+                        res = new List<FFunctionCallLeft>();
+                        res.setParent(fc);
+                        
+                        merge();
+                        
+                        FAbstractEquation fae;
+                        if (res.getNumChild() > 1 || fc.isComposite()) {
+                            add(new FFunctionCallEquation(res, fcCopy));
+                        } else {
+                            add(new FEquation(res.getChild(0).getFExp(), fcCopy));
+                        }
+                        return true;
+                    }
+                    return false;
+                }
+                
+                public void merge() {
+                    for (FFunctionCall ffc : this) {
+                        if (ffc.isFunctionCallClause()) {
+                            FFunctionCallEquation fce = (FFunctionCallEquation) ffc.myFEquation();
+                            for (int i = 0; i < fce.getNumLeft(); i++) {
+                                if (fce.getLeft(i).hasFExp()) {
+                                    merge(i, fce.getLeft(i).getFExp());
+                                }
+                            }
+                            ffc.myFEquation().markForRemoval();
+                        } else {
+                            ffc.replaceMe(getRepl().fullCopy());
+                        }
+                    }
+                }
+                
+                public void merge(int i, FExp other) {
+                    if (i >= res.getNumChild()) {
+                        res.add(new FFunctionCallLeft());
+                    }
+                    res.getChild(i).merge(this, other);
+                }
+                
+                public void add(FAbstractEquation fae) {
+                    if (get(0).variability().parameterOrLess()) {
+                        addFParameterEquationNoTransform(fae);
+                    } else {
+                        addFAbstractEquationNoTransform(fae);
+                    }
+                }
+                
+                public FExp getRepl() {
+                    if (repl == null) {
+                        FFunctionCall fc = get(0);
+                        final String name = fc.tempVarName();
+                        final FTypePrefixVariability v = fc.variability().combine(ASTNode.fFixedParameter());
+                        ExpFromTypeBuilder b = new ExpFromTypeBuilder() {
+                            public FExp build(String suffix, FType type) {
+                                String n = name + suffix;
+                                addFVariable(type.createTempFVariable(new FQNameString(n), v.combineDown(type.funcOutputVariability())));
+                                return new FIdUseExp(name + suffix);
+                            }
+                        };
+                        repl = fc.type().buildFExp(b, "");
+                        merge(0, repl);
+                    }
+                    return repl;
+                }
+                
+            }
+        }
+    }
+    
+    /**
+     * Add function calls in this sub tree to <code>m</code>
+     */
+    public void ASTNode.collectFunctionCalls(FClass.commonSubexpressionEliminationIfSet m) {
+        for (ASTNode n : this) {
+            n.collectFunctionCalls(m);
+        }
+    }
+    
+    public void FAlgorithm.collectFunctionCalls(FClass.commonSubexpressionEliminationIfSet m) {
+        
+    }
+    
+    public void FIfWhenElseEquation.collectFunctionCalls(FClass.commonSubexpressionEliminationIfSet m) {
+        
+    }
+    
+    public void FFunctionCall.collectFunctionCalls(FClass.commonSubexpressionEliminationIfSet m) {
+        /* We dont try to eliminate nested function call expressions to avoid some complicated cases */
+        m.add(this);
+    }
+    
+    /**
+     * Merge copies of FExps found in <code>other</code> into their respective position in <code>this</code>.
+     * Add equation to <code>m</code> in case of collision.
+     */
+    public void FFunctionCallLeft.merge(FClass.commonSubexpressionEliminationIfSet.CSESet m, FExp other) {
+        if (hasFExp()) {
+            setFExp(getFExp().merge(m, other));
+        } else {
+            setFExp(other.fullCopy());
+        }
+    }
+    
+    public FExp FExp.merge(FClass.commonSubexpressionEliminationIfSet.CSESet m, FExp exp) {
+        if (!exp.isNoExp()) {
+            m.add(new FEquation(fullCopy(), exp.fullCopy()));
+        }
+        return this;
+    }
+    
+    public FExp FNoExp.merge(FClass.commonSubexpressionEliminationIfSet.CSESet m, FExp exp) {
+        return exp.fullCopy();
+    }
+    
+    public FExp FRecordConstructor.merge(FClass.commonSubexpressionEliminationIfSet.CSESet m, FExp exp) {
+        FRecordConstructor other = (FRecordConstructor) exp;
+        for (int i = 0; i < getNumArg(); i++) {
+            setArg(getArg(i).merge(m, other.getArg(i)), i);
+        }
+        return this;
+    }
+    
+    public FExp FArray.merge(FClass.commonSubexpressionEliminationIfSet.CSESet m, FExp exp) {
+        FArray other = (FArray) exp;
+        for (int i = 0; i < getNumFExp(); i++) {
+            setFExp(getFExp(i).merge(m, other.getFExp(i)), i);
+        }
+        return this;
+    }
+}

trunk/Compiler/ModelicaMiddleEnd/src/test/CommonSubexpressionEliminationTests.mo
@@ -1,0 +1,251 @@
+/*
+	Copyright (C) 2015 Modelon AB
+
+	This program is free software: you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation, version 3 of the License.
+
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+within ;
+package CommonSubexpressionEliminationTests
+
+    function f1
+        input Real x;
+        output Real y1 = x;
+        output Real y2 = x;
+    algorithm
+        annotation(Inline=false);
+    end f1;
+
+model FunctionCall1
+    Real y1 = f1(time);
+    Real y2 = f1(time);
+    
+    annotation(__JModelica(UnitTesting(tests={
+        TransformCanonicalTestCase(
+            name="FunctionCall1",
+            description="Elimination of duplicate function call",
+            flatModel="
+fclass CommonSubexpressionEliminationTests.FunctionCall1
+ Real y1;
+equation
+ y1 = CommonSubexpressionEliminationTests.f1(time);
+
+public
+ function CommonSubexpressionEliminationTests.f1
+  input Real x;
+  output Real y1;
+  output Real y2;
+ algorithm
+  y1 := x;
+  y2 := x;
+  return;
+ annotation(Inline = false);
+ end CommonSubexpressionEliminationTests.f1;
+
+end CommonSubexpressionEliminationTests.FunctionCall1;
+")})));
+end FunctionCall1;
+
+model FunctionCall2
+    Real y1;
+    Real y2,y3;
+equation
+    y1 = f1(time);
+    (y2,y3) = f1(time);
+    
+    annotation(__JModelica(UnitTesting(tests={
+        TransformCanonicalTestCase(
+            name="FunctionCall2",
+            description="Elimination of duplicate function call",
+            flatModel="
+fclass CommonSubexpressionEliminationTests.FunctionCall2
+ Real y1;
+ Real y3;
+equation
+ (y1, y3) = CommonSubexpressionEliminationTests.f1(time);
+
+public
+ function CommonSubexpressionEliminationTests.f1
+  input Real x;
+  output Real y1;
+  output Real y2;
+ algorithm
+  y1 := x;
+  y2 := x;
+  return;
+ annotation(Inline = false);
+ end CommonSubexpressionEliminationTests.f1;
+
+end CommonSubexpressionEliminationTests.FunctionCall2;
+")})));
+end FunctionCall2;
+
+model FunctionCall3
+    Real y1;
+    Real y2,y3;
+equation
+    (y2,y3) = f1(time);
+    y1 = f1(time);
+    
+    annotation(__JModelica(UnitTesting(tests={
+        TransformCanonicalTestCase(
+            name="FunctionCall3",
+            description="Elimination of duplicate function call",
+            flatModel="
+fclass CommonSubexpressionEliminationTests.FunctionCall3
+ Real y1;
+ Real y3;
+equation
+ (y1, y3) = CommonSubexpressionEliminationTests.f1(time);
+
+public
+ function CommonSubexpressionEliminationTests.f1
+  input Real x;
+  output Real y1;
+  output Real y2;
+ algorithm
+  y1 := x;
+  y2 := x;
+  return;
+ annotation(Inline = false);
+ end CommonSubexpressionEliminationTests.f1;
+
+end CommonSubexpressionEliminationTests.FunctionCall3;
+")})));
+end FunctionCall3;
+
+model FunctionCall4
+    Real y1,y4;
+    Real y2,y3;
+equation
+    (y2,y3) = f1(time);
+    (y1,y4) = f1(time);
+    
+    annotation(__JModelica(UnitTesting(tests={
+        TransformCanonicalTestCase(
+            name="FunctionCall4",
+            description="Elimination of duplicate function call",
+            flatModel="
+fclass CommonSubexpressionEliminationTests.FunctionCall4
+ Real y2;
+ Real y3;
+equation
+ (y2, y3) = CommonSubexpressionEliminationTests.f1(time);
+
+public
+ function CommonSubexpressionEliminationTests.f1
+  input Real x;
+  output Real y1;
+  output Real y2;
+ algorithm
+  y1 := x;
+  y2 := x;
+  return;
+ annotation(Inline = false);
+ end CommonSubexpressionEliminationTests.f1;
+
+end CommonSubexpressionEliminationTests.FunctionCall4;
+")})));
+end FunctionCall4;
+
+model FunctionCall5
+    Real y1,y2;
+equation
+    (y1, ) = f1(time);
+    ( ,y2) = f1(time);
+    
+    annotation(__JModelica(UnitTesting(tests={
+        TransformCanonicalTestCase(
+            name="FunctionCall5",
+            description="Elimination of duplicate function call",
+            flatModel="
+fclass CommonSubexpressionEliminationTests.FunctionCall5
+ Real y1;
+ Real y2;
+equation
+ (y1, y2) = CommonSubexpressionEliminationTests.f1(time);
+
+public
+ function CommonSubexpressionEliminationTests.f1
+  input Real x;
+  output Real y1;
+  output Real y2;
+ algorithm
+  y1 := x;
+  y2 := x;
+  return;
+ annotation(Inline = false);
+ end CommonSubexpressionEliminationTests.f1;
+
+end CommonSubexpressionEliminationTests.FunctionCall5;
+")})));
+end FunctionCall5;
+
+model FunctionCall6
+
+    record R
+        Real x;
+        Real[2,1] y;
+    end R;
+    
+    function f2
+        input Real x;
+        output Real y = x;
+        output R r = R(x,{{x},{x}});
+    algorithm
+        annotation(Inline=false);
+    end f2;
+
+    Real y1,y2;
+    R y3,y4;
+equation
+    (y1, ) = f2(time);
+    ( ,y3) = f2(time);
+    (y2,y4) = f2(time);
+    
+    annotation(__JModelica(UnitTesting(tests={
+        TransformCanonicalTestCase(
+            name="FunctionCall6",
+            description="Elimination of duplicate function call",
+            flatModel="
+fclass CommonSubexpressionEliminationTests.FunctionCall6
+ Real y1;
+ Real y3.x;
+ Real y3.y[1,1];
+ Real y3.y[2,1];
+equation
+ (y1, CommonSubexpressionEliminationTests.FunctionCall6.R(y3.x, {{y3.y[1,1]}, {y3.y[2,1]}})) = CommonSubexpressionEliminationTests.FunctionCall6.f2(time);
+
+public
+ function CommonSubexpressionEliminationTests.FunctionCall6.f2
+  input Real x;
+  output Real y;
+  output CommonSubexpressionEliminationTests.FunctionCall6.R r;
+ algorithm
+  y := x;
+  r.x := x;
+  r.y[1,1] := x;
+  r.y[2,1] := x;
+  return;
+ annotation(Inline = false);
+ end CommonSubexpressionEliminationTests.FunctionCall6.f2;
+
+ record CommonSubexpressionEliminationTests.FunctionCall6.R
+  Real x;
+  Real y[2,1];
+ end CommonSubexpressionEliminationTests.FunctionCall6.R;
+
+end CommonSubexpressionEliminationTests.FunctionCall6;
+")})));
+end FunctionCall6;
+
+end CommonSubexpressionEliminationTests;

trunk/Compiler/ModelicaCBackEnd/src/test/CADCodeGenTests.mo
@@ -1644,50 +1644,37 @@ jmi_ad_var_t tmp_var_2;
 jmi_ad_var_t tmp_der_2;
 jmi_ad_var_t tmp_var_3;
 jmi_ad_var_t tmp_der_3;
-jmi_ad_var_t tmp_var_4;
-jmi_ad_var_t tmp_der_4;
-jmi_ad_var_t tmp_var_5;
-jmi_ad_var_t tmp_der_5;
-jmi_ad_var_t tmp_var_6;
-jmi_ad_var_t tmp_der_6;
-    (*res)[0] = _x1_1 * _U_12 - (_der_x_14);
-    (*dF)[0] = (*dz)[jmi_get_index_from_value_ref(6)-jmi->offs_real_dx] * _U_12 + _x1_1 * (*dz)[jmi_get_index_from_value_ref(10)-jmi->offs_real_dx] - ((*dz)[jmi_get_index_from_value_ref(0)-jmi->offs_real_dx]);
-    func_CADCodeGenTests_CADFunction6_F_der_AD0(_x_0, _x1_1, _x2_2, _x3_3, _x4_4, (*dz)[jmi_get_index_from_value_ref(5)-jmi->offs_real_dx], (*dz)[jmi_get_index_from_value_ref(6)-jmi->offs_real_dx], (*dz)[jmi_get_index_from_value_ref(7)-jmi->offs_real_dx], (*dz)[jmi_get_index_from_value_ref(8)-jmi->offs_real_dx], (*dz)[jmi_get_index_from_value_ref(9)-jmi->offs_real_dx], &tmp_var_0, &tmp_var_1, &tmp_var_2, &tmp_var_3, NULL, NULL, NULL, &tmp_der_0, &tmp_der_1, &tmp_der_2, &tmp_der_3, NULL, NULL, NULL);
-    (*res)[1] = tmp_var_0 - (_a_5);
-    (*dF)[1] = tmp_der_0 - ((*dz)[jmi_get_index_from_value_ref(11)-jmi->offs_real_dx]);
-    (*res)[2] = tmp_var_1 - (_b_6);
-    (*dF)[2] = tmp_der_1 - ((*dz)[jmi_get_index_from_value_ref(12)-jmi->offs_real_dx]);
-    (*res)[3] = tmp_var_2 - (_c_7);
-    (*dF)[3] = tmp_der_2 - ((*dz)[jmi_get_index_from_value_ref(13)-jmi->offs_real_dx]);
-    (*res)[4] = tmp_var_3 - (_d_8);
-    (*dF)[4] = tmp_der_3 - ((*dz)[jmi_get_index_from_value_ref(14)-jmi->offs_real_dx]);
-    (*res)[5] = _b_6 - (_der_x1_15);
-    (*dF)[5] = (*dz)[jmi_get_index_from_value_ref(12)-jmi->offs_real_dx] - ((*dz)[jmi_get_index_from_value_ref(1)-jmi->offs_real_dx]);
-    (*res)[6] = _c_7 - (_der_x2_16);
-    (*dF)[6] = (*dz)[jmi_get_index_from_value_ref(13)-jmi->offs_real_dx] - ((*dz)[jmi_get_index_from_value_ref(2)-jmi->offs_real_dx]);
-    (*res)[7] = _d_8 + _a_5 - (_der_x3_17);
-    (*dF)[7] = (*dz)[jmi_get_index_from_value_ref(14)-jmi->offs_real_dx] + (*dz)[jmi_get_index_from_value_ref(11)-jmi->offs_real_dx] - ((*dz)[jmi_get_index_from_value_ref(3)-jmi->offs_real_dx]);
-    v_1 = _e_9 * _f_10;
-    d_1 = (*dz)[jmi_get_index_from_value_ref(15)-jmi->offs_real_dx] * _f_10 + _e_9 * (*dz)[jmi_get_index_from_value_ref(16)-jmi->offs_real_dx];
-    v_0 = v_1 + _g_11;
-    d_0 = d_1 + (*dz)[jmi_get_index_from_value_ref(17)-jmi->offs_real_dx];
-    (*res)[8] = v_0 + _x1_1 - (_der_x4_18);
-    (*dF)[8] = d_0 + (*dz)[jmi_get_index_from_value_ref(6)-jmi->offs_real_dx] - ((*dz)[jmi_get_index_from_value_ref(4)-jmi->offs_real_dx]);
-    func_CADCodeGenTests_CADFunction6_F_der_AD0(_x_0, _x1_1, _x2_2, _x3_3, _x4_4, (*dz)[jmi_get_index_from_value_ref(5)-jmi->offs_real_dx], (*dz)[jmi_get_index_from_value_ref(6)-jmi->offs_real_dx], (*dz)[jmi_get_index_from_value_ref(7)-jmi->offs_real_dx], (*dz)[jmi_get_index_from_value_ref(8)-jmi->offs_real_dx], (*dz)[jmi_get_index_from_value_ref(9)-jmi->offs_real_dx], &tmp_var_4, &tmp_var_5, &tmp_var_6, NULL, NULL, NULL, NULL, &tmp_der_4, &tmp_der_5, &tmp_der_6, NULL, NULL, NULL, NULL);
-    (*res)[9] = tmp_var_4 - (_e_9);
-    (*dF)[9] = tmp_der_4 - ((*dz)[jmi_get_index_from_value_ref(15)-jmi->offs_real_dx]);
-    (*res)[10] = tmp_var_5 - (_f_10);
-    (*dF)[10] = tmp_der_5 - ((*dz)[jmi_get_index_from_value_ref(16)-jmi->offs_real_dx]);
-    (*res)[11] = tmp_var_6 - (_g_11);
-    (*dF)[11] = tmp_der_6 - ((*dz)[jmi_get_index_from_value_ref(17)-jmi->offs_real_dx]);
+    (*res)[0] = _x1_1 * _U_9 - (_der_x_11);
+    (*dF)[0] = (*dz)[jmi_get_index_from_value_ref(6)-jmi->offs_real_dx] * _U_9 + _x1_1 * (*dz)[jmi_get_index_from_value_ref(10)-jmi->offs_real_dx] - ((*dz)[jmi_get_index_from_value_ref(0)-jmi->offs_real_dx]);
+    (*res)[1] = _b_6 - (_der_x1_12);
+    (*dF)[1] = (*dz)[jmi_get_index_from_value_ref(12)-jmi->offs_real_dx] - ((*dz)[jmi_get_index_from_value_ref(1)-jmi->offs_real_dx]);
+    (*res)[2] = _c_7 - (_der_x2_13);
+    (*dF)[2] = (*dz)[jmi_get_index_from_value_ref(13)-jmi->offs_real_dx] - ((*dz)[jmi_get_index_from_value_ref(2)-jmi->offs_real_dx]);
+    (*res)[3] = _d_8 + _a_5 - (_der_x3_14);
+    (*dF)[3] = (*dz)[jmi_get_index_from_value_ref(14)-jmi->offs_real_dx] + (*dz)[jmi_get_index_from_value_ref(11)-jmi->offs_real_dx] - ((*dz)[jmi_get_index_from_value_ref(3)-jmi->offs_real_dx]);
+    v_1 = _a_5 * _der_x1_12;
+    d_1 = (*dz)[jmi_get_index_from_value_ref(11)-jmi->offs_real_dx] * _der_x1_12 + _a_5 * (*dz)[jmi_get_index_from_value_ref(1)-jmi->offs_real_dx];
+    v_0 = v_1 + _der_x2_13;
+    d_0 = d_1 + (*dz)[jmi_get_index_from_value_ref(2)-jmi->offs_real_dx];
+    (*res)[4] = v_0 + _x1_1 - (_der_x4_15);
+    (*dF)[4] = d_0 + (*dz)[jmi_get_index_from_value_ref(6)-jmi->offs_real_dx] - ((*dz)[jmi_get_index_from_value_ref(4)-jmi->offs_real_dx]);
     v_4 = _x_0 + _x1_1;
     d_4 = (*dz)[jmi_get_index_from_value_ref(5)-jmi->offs_real_dx] + (*dz)[jmi_get_index_from_value_ref(6)-jmi->offs_real_dx];
     v_3 = v_4 + _x2_2;
     d_3 = d_4 + (*dz)[jmi_get_index_from_value_ref(7)-jmi->offs_real_dx];
     v_2 = v_3 + _x3_3;
     d_2 = d_3 + (*dz)[jmi_get_index_from_value_ref(8)-jmi->offs_real_dx];
-    (*res)[12] = v_2 + _x4_4 - (_Y_13);
-    (*dF)[12] = d_2 + (*dz)[jmi_get_index_from_value_ref(9)-jmi->offs_real_dx] - ((*dz)[jmi_get_index_from_value_ref(18)-jmi->offs_real_dx]);
+    (*res)[5] = v_2 + _x4_4 - (_Y_10);
+    (*dF)[5] = d_2 + (*dz)[jmi_get_index_from_value_ref(9)-jmi->offs_real_dx] - ((*dz)[jmi_get_index_from_value_ref(15)-jmi->offs_real_dx]);
+    func_CADCodeGenTests_CADFunction6_F_der_AD0(_x_0, _x1_1, _x2_2, _x3_3, _x4_4, (*dz)[jmi_get_index_from_value_ref(5)-jmi->offs_real_dx], (*dz)[jmi_get_index_from_value_ref(6)-jmi->offs_real_dx], (*dz)[jmi_get_index_from_value_ref(7)-jmi->offs_real_dx], (*dz)[jmi_get_index_from_value_ref(8)-jmi->offs_real_dx], (*dz)[jmi_get_index_from_value_ref(9)-jmi->offs_real_dx], &tmp_var_0, &tmp_var_1, &tmp_var_2, &tmp_var_3, NULL, NULL, NULL, &tmp_der_0, &tmp_der_1, &tmp_der_2, &tmp_der_3, NULL, NULL, NULL);
+    (*res)[6] = tmp_var_0 - (_a_5);
+    (*dF)[6] = tmp_der_0 - ((*dz)[jmi_get_index_from_value_ref(11)-jmi->offs_real_dx]);
+    (*res)[7] = tmp_var_1 - (_b_6);
+    (*dF)[7] = tmp_der_1 - ((*dz)[jmi_get_index_from_value_ref(12)-jmi->offs_real_dx]);
+    (*res)[8] = tmp_var_2 - (_c_7);
+    (*dF)[8] = tmp_der_2 - ((*dz)[jmi_get_index_from_value_ref(13)-jmi->offs_real_dx]);
+    (*res)[9] = tmp_var_3 - (_d_8);
+    (*dF)[9] = tmp_der_3 - ((*dz)[jmi_get_index_from_value_ref(14)-jmi->offs_real_dx]);
 ")})));
 end CADFunction6;
 

trunk/Compiler/ModelicaCBackEnd/src/test/CCodeGenTests.mo
@@ -17420,6 +17420,7 @@ model TestRelationalOp9
             description="Test correct event generation. Temp in time event calculation.",
             variability_propagation=false,
             inline_functions="none",
+            common_subexp_elim=false,
             template="
 $C_DAE_relations$
 static const int N_sw = $n_state_switches$ + $n_time_switches$;
@@ -19611,6 +19612,7 @@ model LoadResource1
             name="LoadResource1",
             description="",
             variability_propagation=false,
+            common_subexp_elim=false,
             template="
 $C_ode_derivatives$
 $C_DAE_initial_dependent_parameter_assignments$
@@ -21005,6 +21007,7 @@ model Delay4
             description="Delay operator code gen: Array temp generation",
             generate_ode=true,
             equation_sorting=true,
+            common_subexp_elim=false,
             template="
 N_delays = $n_delays$;
 $C_DAE_relations$

trunk/Compiler/ModelicaFrontEnd/src/java/org/jmodelica/util/OptionRegistry.java
@@ -238,6 +238,13 @@ abstract public class OptionRegistry {
              true,
              "If this option is set to true (default), then variabilities are " +
              "propagated through the model."),
+        COMMON_SUBEXP_ELIM
+            ("common_subexp_elim", 
+             OptionType.compiler, 
+             Category.uncommon,
+             true,
+             "If this option is set to true (default), then identical function calls are "
+             + " extracted into a common equation."),
         EXT_CEVAL
             ("external_constant_evaluation", 
              OptionType.compiler, 

trunk/Compiler/ModelicaFrontEnd/src/test/FunctionTests.mo
@@ -12641,6 +12641,7 @@ package FuncCallInSize
             description="Scalarization of functions: Function call from other package in declaration size",
             variability_propagation=false,
             inline_functions="none",
+            common_subexp_elim=false,
             flatModel="
 fclass FunctionTests.UnknownSize.FuncCallInSize.FromOtherPackage
  Real y1[1];

trunk/Compiler/ModelicaMiddleEnd/src/jastadd/TransformCanonical.jrag
@@ -138,6 +138,7 @@ aspect TransformCanonical {
             transformations.add(new propagateNonFixedParameters());
             transformations.add(new indexReduction());
             transformations.add(new LateFunctionInliningIfSet());
+            transformations.add(new commonSubexpressionEliminationIfSet());
             transformations.add(new addFPreVariables());
             transformations.add(new aliasEliminationIfSet());
             transformations.add(new sortDependentParameters());

trunk/Compiler/ModelicaMiddleEnd/src/test/TransformCanonicalTests.mo
@@ -4492,9 +4492,12 @@ equation
 fclass TransformCanonicalTests.IfEqu25
  Real x;
  Real y;
+ parameter Real temp_1;
+parameter equation
+ temp_1 = TransformCanonicalTests.IfEqu25.f(2);
 equation
- x = if TransformCanonicalTests.IfEqu25.f(2) > 0 then time else 2;
- y = if TransformCanonicalTests.IfEqu25.f(2) > 0 then x else x * x;
+ x = if temp_1 > 0 then time else 2;
+ y = if temp_1 > 0 then x else x * x;
 
 public
  function TransformCanonicalTests.IfEqu25.f

commit 090fbbf608935525691038f1bccc35bc4e38d2b5

#4565 Fixes null pointer exception introduced in r8008 triggered by non-builtins without return values.

trunk/Compiler/ModelicaMiddleEnd/src/jastadd/optimizations/CommonSubexpressionElimination.jrag
@@ -77,7 +77,7 @@ aspect CommonSubexpressionElimination {
                         merge();
                         
                         FAbstractEquation fae;
-                        if (res.getNumChild() > 1 || fc.isComposite()) {
+                        if (res.getNumChild() != 1 || fc.isComposite()) {
                             add(new FFunctionCallEquation(res, fcCopy));
                         } else {
                             add(new FEquation(res.getChild(0).getFExp(), fcCopy));

trunk/Compiler/ModelicaMiddleEnd/src/test/CommonSubexpressionEliminationTests.mo
@@ -248,4 +248,39 @@ end CommonSubexpressionEliminationTests.FunctionCall6;
 ")})));
 end FunctionCall6;
 
+model FunctionCall7
+
+    function f2
+        input Real x;
+    algorithm
+        assert(x > 0, "msg");
+        annotation(Inline=false);
+    end f2;
+
+equation
+    f2(time);
+    f2(time);
+    
+    annotation(__JModelica(UnitTesting(tests={
+        TransformCanonicalTestCase(
+            name="FunctionCall7",
+            description="Elimination of duplicate function call",
+            flatModel="
+fclass CommonSubexpressionEliminationTests.FunctionCall7
+equation
+ CommonSubexpressionEliminationTests.FunctionCall7.f2(time);
+
+public
+ function CommonSubexpressionEliminationTests.FunctionCall7.f2
+  input Real x;
+ algorithm
+  assert(x > 0, \"msg\");
+  return;
+ annotation(Inline = false);
+ end CommonSubexpressionEliminationTests.FunctionCall7.f2;
+
+end CommonSubexpressionEliminationTests.FunctionCall7;
+")})));
+end FunctionCall7;
+
 end CommonSubexpressionEliminationTests;