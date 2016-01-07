jnorthrup/jmodelica

commit 970175573f361c2382d0402baa31553a4136f183

Added support for identifiers for problems (errors and warnings). Introduced the concept of problem producers which each type of problem message should have an static instance of. This instance is the used when producing the actual problem. This way we can filter and categorize problems which will be helpful for the user when debugging models and libraries. This commit is backwards compatible with the previous methods for reporting problems, the identifier is left empty for problems reported with the old methods. We should now migrate all places where we report problems. This will be a tedious task since there are more than 300 different problems all over the compiler. However most of them will be straight forward to migrate since they only use String.format() to produce the error, which there are convenient classes for in the new framework. Ticket #4735.
git-svn-id: https://svn.jmodelica.org@8368 7ae51c65-13d5-4921-9d80-6511ddd1419c

trunk/Compiler/ModelicaFrontEnd/src/java/org/jmodelica/util/problemHandling/Problem.java
@@ -13,7 +13,7 @@
     You should have received a copy of the GNU General Public License
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
-package org.jmodelica.util;
+package org.jmodelica.util.problemHandling;
 
 import java.util.Collection;
 import java.util.Set;
@@ -27,6 +27,9 @@ public class Problem implements Comparable<Problem>, LoggingUnit {
     private static final long serialVersionUID = 1;
     
     public int compareTo(Problem other) {
+        if (identifier != null && other.identifier != null && !identifier.equals(other.identifier)) {
+            return identifier.compareTo(other.identifier);
+        }
         if (kind.order != other.kind.order)
             return kind.order - other.kind.order;
         if (fileName == null || other.fileName == null) {
@@ -45,36 +48,11 @@ public class Problem implements Comparable<Problem>, LoggingUnit {
         return message.compareTo(other.message);
     }
 
-    public enum Severity { ERROR, WARNING }
-
-    public enum Kind { 
-        OTHER(2), LEXICAL("Syntax", 1), SYNTACTIC("Syntax", 1), SEMANTIC(2), COMPLIANCE("Compliance", 2);
-
-        private String desc = null;
-        public final int order;
-
-        private Kind(int order) {
-            this.order = order;
-        }
-
-        private Kind(String desc, int order) {
-            this.desc = desc;
-            this.order = order;
-        }
-
-        public void writeKindAndSeverity(StringBuilder sb, Severity sev) {
-            String s = sev.toString().toLowerCase();
-            if (desc != null) {
-                sb.append(desc);
-                sb.append(' ');
-                sb.append(s);
-            } else {
-                sb.append(Character.toUpperCase(s.charAt(0)));
-                sb.append(s.substring(1));
-            }
-        }
+    protected final String identifier;
+    public String identifier() {
+        return identifier;
     }
-
+    
     protected final int beginLine;
     protected final int beginColumn;
     public int beginLine() { return beginLine; }
@@ -89,11 +67,11 @@ public class Problem implements Comparable<Problem>, LoggingUnit {
     protected final String message;
     public String message() { return message; }
 
-    protected final Severity severity;
-    public Severity severity() { return severity; }
+    protected final ProblemSeverity severity;
+    public ProblemSeverity severity() { return severity; }
 
-    protected final Kind kind;
-    public Kind kind() { return kind; }
+    protected final ProblemKind kind;
+    public ProblemKind kind() { return kind; }
 
     protected Set<String> components = new TreeSet<String>(); // TreeSet is used so that we get a sorted set
 
@@ -104,19 +82,28 @@ public class Problem implements Comparable<Problem>, LoggingUnit {
 
     public Collection<String> components() { return components; }
 
+    @Deprecated
     public Problem(String fileName, String message) {
-        this(fileName, message, Severity.ERROR);
+        this(fileName, message, ProblemSeverity.ERROR);
     }
 
-    public Problem(String fileName, String message, Severity severity) {
-        this(fileName, message, severity, Kind.OTHER);
+    @Deprecated
+    public Problem(String fileName, String message, ProblemSeverity severity) {
+        this(fileName, message, severity, ProblemKind.OTHER);
     }
 
-    public Problem(String fileName, String message, Severity severity, Kind kind) {
-        this(fileName, message, severity, kind, 0, 0);
+    @Deprecated
+    public Problem(String fileName, String message, ProblemSeverity severity, ProblemKind kind) {
+        this(null, fileName, message, severity, kind, 0, 0);
     }
 
-    public Problem(String fileName, String message, Severity severity, Kind kind, int beginLine, int beginColumn) {
+    @Deprecated
+    public Problem(String fileName, String message, ProblemSeverity severity, ProblemKind kind, int beginLine, int beginColumn) {
+        this(null, fileName, message, severity, kind, beginLine, beginColumn);
+    }
+    @Deprecated
+    public Problem(String identifier, String fileName, String message, ProblemSeverity severity, ProblemKind kind, int beginLine, int beginColumn) {
+        this.identifier = identifier;
         this.fileName = fileName;
         this.message = message;
         this.severity = severity;
@@ -127,9 +114,16 @@ public class Problem implements Comparable<Problem>, LoggingUnit {
         if (message == null)
             throw new NullPointerException();
     }
+    
+    public Problem(String identifier, ReporterNode src, ProblemSeverity severity, ProblemKind kind, String message) {
+        this(identifier, src.fileName(), message, severity, kind, src.beginLine(), src.beginColumn());
+        if (src.myOptions().getBooleanOption("component_names_in_errors")) {
+            addComponent(src.errorComponentName());
+        }
+    }
 
     public boolean isTestError(boolean checkAll) {
-        return checkAll || (severity == Severity.ERROR && kind != Kind.COMPLIANCE);
+        return checkAll || (severity == ProblemSeverity.ERROR && kind != ProblemKind.COMPLIANCE);
     }
 
     public boolean equals(Object o) {
@@ -145,7 +139,12 @@ public class Problem implements Comparable<Problem>, LoggingUnit {
         components.addAll(p.components);
     }
 
+    @Override
     public String toString() {
+        return toString(false);
+    }
+
+    public String toString(boolean printIdentifier) {
         StringBuilder sb = new StringBuilder();
         kind.writeKindAndSeverity(sb, severity);
         if (fileName == null) {
@@ -158,6 +157,9 @@ public class Problem implements Comparable<Problem>, LoggingUnit {
             sb.append(", in file '");
             sb.append(fileName);
             sb.append("'");
+        }
+        if (printIdentifier && identifier != null) {
+            sb.append(", " + identifier);
         }
         if (!components.isEmpty()) {
             sb.append(",\n");
@@ -194,17 +196,18 @@ public class Problem implements Comparable<Problem>, LoggingUnit {
 
     @Override
     public String print(Level level) {
-        return toString();
-}
+        return toString(level.shouldLog(Level.INFO));
+    }
 
     @Override
     public String printXML(Level level) {
         return XMLLogger.write_node(Problem.capitalize(severity()), 
-                "kind",    kind().toString().toLowerCase(),
-                "file",    fileName(),
-                "line",    beginLine(),
-                "column",  beginColumn(),
-                "message", message());
+                "identifier",   identifier() == null ? "" : identifier(),
+                "kind",         kind().toString().toLowerCase(),
+                "file",         fileName(),
+                "line",         beginLine(),
+                "column",       beginColumn(),
+                "message",      message());
     }
 
     @Override

trunk/Compiler/ModelicaCompiler/src/jastadd/DiagnosticsGenerator.jrag
@@ -374,15 +374,15 @@ public class DiagnosticsGenerator {
             numComplianceErrors = 0;
             numWarnings = 0;
             for (Problem p : problems) {
-                if (p.severity() == Problem.Severity.ERROR &&
-                    p.kind() != Problem.Kind.COMPLIANCE) {
+                if (p.severity() == ProblemSeverity.ERROR &&
+                    p.kind() != ProblemKind.COMPLIANCE) {
                     errs.append(p.toString() + "\n");
                     numErrors++;
-                } else if (p.severity() == Problem.Severity.ERROR &&
-                           p.kind() == Problem.Kind.COMPLIANCE) {
+                } else if (p.severity() == ProblemSeverity.ERROR &&
+                           p.kind() == ProblemKind.COMPLIANCE) {
                     compErrs.append(p.toString() + "\n");
                     numComplianceErrors++;
-                } else if (p.severity() == Problem.Severity.WARNING) {
+                } else if (p.severity() == ProblemSeverity.WARNING) {
                     warns.append(p.toString() + "\n");
                     numWarnings++;
                 }

trunk/Compiler/ModelicaCompiler/src/jastadd/ModelicaCompiler.jrag
@@ -939,9 +939,7 @@ public class ModelicaCompiler {
 						log.info(file + " parsed OK.");
 					}
 				} catch (CompilerException e) {
-					StringBuilder str = new StringBuilder();
-					for (Problem p : e.getErrors()) 
-						log.error(p.toString());
+                    log.logCompilerException(e);
 				} catch (FileNotFoundException e) {
 					log.error("Could not find file: " + e.getMessage());
 				} catch (IOException e) {
@@ -2085,11 +2083,11 @@ public class ModelicaCompiler {
     }
     
     protected void ModelicaCompiler.warning(String message) {
-        errorHandler.problem(new Problem("", message, Problem.Severity.WARNING));
+        errorHandler.problem(new Problem("", message, ProblemSeverity.WARNING));
     }
     
     protected void ModelicaCompiler.error(String message) {
-        errorHandler.problem(new Problem("", message, Problem.Severity.ERROR));
+        errorHandler.problem(new Problem("", message, ProblemSeverity.ERROR));
     }
 }
 

trunk/Compiler/ModelicaFrontEnd/src/jastadd/errorcheck/ComplianceCheck.jadd
@@ -15,6 +15,9 @@
 */
 
 import org.jmodelica.util.ErrorCheckType;
+import org.jmodelica.util.problemHandling.SimpleProblemProducer;
+import org.jmodelica.util.problemHandling.SimpleWarningProducer;
+import org.jmodelica.util.problemHandling.SimpleErrorProducer;
 
 aspect ComplianceCheck {
 
@@ -84,6 +87,7 @@ aspect ComplianceCheck {
 		complianceOnlyFMU("When equations are");
 	}
 	
+	@Deprecated
 	public void ASTNode.complianceOnlyFMU(String message) {
 		if (!myOptions().getBooleanOption("generate_ode"))
 			compliance(message + " currently only supported when compiling FMUs");
@@ -239,18 +243,25 @@ aspect ComplianceCheck {
                getSignificantDigits().compliance("significantDigits with higher than parameter variability is not supported");
        }
 	}
-		
+	
+    public static final SimpleProblemProducer ASTNode.STRING_ARGUMENTS_PARTIALLY_SUPPORTED =
+            new SimpleWarningProducer("STRING_ARGUMENTS_PARTIALLY_SUPPORTED", ProblemKind.COMPLIANCE, "String arguments in functions are only partially supported");
+    public static final SimpleProblemProducer ASTNode.STRING_PARAMETERS_PARTIALLY_SUPPORTED =
+            new SimpleWarningProducer("STRING_PARAMETERS_PARTIALLY_SUPPORTED", ProblemKind.COMPLIANCE, "String parameters are only partially supported");
+    public static final SimpleProblemProducer ASTNode.STRING_VARIABLES_NOT_SUPPORTED =
+            new SimpleErrorProducer("STRING_VARIABLES_NOT_SUPPORTED", ProblemKind.COMPLIANCE, "String variables are not supported");
+	
 	public void InstPrimitive.complianceCheck(ErrorCheckType checkType) {
 		super.complianceCheck(checkType);
 		if (isString()) {
 			if (inFunction()) {
-				warning("String arguments in functions are only partially supported");
+			    STRING_ARGUMENTS_PARTIALLY_SUPPORTED.invoke(this);
 			} else if (variability().parameterOrLess()) {
-				warning("String parameters are only partially supported");
+			    STRING_PARAMETERS_PARTIALLY_SUPPORTED.invoke(this);
 				if (isParameter())
 					forceVariability(checkType, fStructParameter());
 			} else {
-				compliance("String variables are not supported");
+			    STRING_VARIABLES_NOT_SUPPORTED.invoke(this);
 			}
 		}
 		if (!myOptions().getBooleanOption("generate_ode")) {

trunk/Compiler/ModelicaFrontEnd/src/jastadd/errorcheck/ErrorCheck.jrag
@@ -22,7 +22,10 @@ import java.util.Map;
 
 import org.jmodelica.util.collections.ListMap;
 import org.jmodelica.util.collections.LinkedHashListMap;
-import org.jmodelica.util.Problem;
+import org.jmodelica.util.problemHandling.Problem;
+import org.jmodelica.util.problemHandling.ProblemSeverity;
+import org.jmodelica.util.problemHandling.ProblemKind;
+import org.jmodelica.util.problemHandling.ReporterNode;
 import org.jmodelica.util.ErrorCheckType;
 
 
@@ -96,7 +99,7 @@ import org.jmodelica.util.ErrorCheckType;
 	   * @param n	the node the error originated from.
 	   */
 	  public void error(String s, ASTNode n) {
-		  problem(s, n, errors, Problem.Severity.ERROR, Problem.Kind.SEMANTIC);
+		  problem(s, n, errors, ProblemSeverity.ERROR, ProblemKind.SEMANTIC);
 	  }
 
 	  /**
@@ -107,7 +110,7 @@ import org.jmodelica.util.ErrorCheckType;
 	   * @param n	the node the error originated from.
 	   */
 	  public void compliance(String s, ASTNode n) {
-		  problem(s, n, errors, Problem.Severity.ERROR, Problem.Kind.COMPLIANCE);
+		  problem(s, n, errors, ProblemSeverity.ERROR, ProblemKind.COMPLIANCE);
 	  }
 
 	  /**
@@ -117,7 +120,7 @@ import org.jmodelica.util.ErrorCheckType;
 	   * @param n	the node the warning originated from.
 	   */
 	  public void warning(String s, ASTNode n) {
-		  problem(s, n, warnings, Problem.Severity.WARNING, Problem.Kind.OTHER);
+		  problem(s, n, warnings, ProblemSeverity.WARNING, ProblemKind.OTHER);
 	  }
 
       /**
@@ -128,7 +131,7 @@ import org.jmodelica.util.ErrorCheckType;
        * @see ASTNode#problem(Problem)
        */
       public void problem(Problem p) {
-          ArrayList<Problem> list = (p.severity() == Problem.Severity.WARNING) ? warnings : errors;
+          ArrayList<Problem> list = (p.severity() == ProblemSeverity.WARNING) ? warnings : errors;
           int pos = list.indexOf(p);
           if (pos == -1)
               list.add(p);
@@ -136,7 +139,7 @@ import org.jmodelica.util.ErrorCheckType;
               list.get(pos).merge(p);
       }
 
-      protected void problem(String s, ASTNode n, ArrayList<Problem> list, Problem.Severity sev, Problem.Kind kind) {
+      protected void problem(String s, ASTNode n, ArrayList<Problem> list, ProblemSeverity sev, ProblemKind kind) {
           Problem p = new Problem(n.fileName(), s, sev, kind, n.lineNumber(), n.columnNumber());
           if (n.myOptions().getBooleanOption("component_names_in_errors"))
               p.addComponent(n.errorComponentName());
@@ -267,10 +270,20 @@ aspect ErrorCheck {
   syn int ASTNode.lineNumber() = (start != 0 || getParent()==null)? beginLine() : getParent().lineNumber();
   syn int ASTNode.columnNumber() = (start != 0 || getParent()==null)? beginColumn() : getParent().columnNumber();
   
+  
+  ASTNode implements ReporterNode;
+  
+  public void ASTNode.reportProblem(Problem problem) {
+      root().getErrorHandler().problem(problem);
+  }
+
+  
+  
   /**
    * Register an error. Delegates to an {@link IErrorHandler}.
    * @param s	the error message.
    */
+  @Deprecated
   public void ASTNode.error(String s) {
 	  root().getErrorHandler().error(s, this);
   }
@@ -280,6 +293,7 @@ aspect ErrorCheck {
    * 
    * Builds error message using <code>format</code> as format string.
    */
+  @Deprecated
   public void ASTNode.error(String format, Object... args) {
 	  error(String.format(format, args));
   }
@@ -288,6 +302,7 @@ aspect ErrorCheck {
    * Register a compliance error. Delegates to an {@link IErrorHandler}.
    * @param s	the error message.
    */
+  @Deprecated
   public void ASTNode.compliance(String s) {
 	  root().getErrorHandler().compliance(s, this);
   }
@@ -297,6 +312,7 @@ aspect ErrorCheck {
    * 
    * Builds error message using <code>format</code> as format string.
    */
+  @Deprecated
   public void ASTNode.compliance(String format, Object... args) {
 	  compliance(String.format(format, args));
   }
@@ -305,6 +321,7 @@ aspect ErrorCheck {
    * Register a warning. Delegates to an {@link IErrorHandler}.
    * @param s	the warning message.
    */
+  @Deprecated
   public void ASTNode.warning(String s) {
 	  root().getErrorHandler().warning(s, this);
   }
@@ -314,6 +331,7 @@ aspect ErrorCheck {
    * 
    * Builds warning message using <code>format</code> as format string.
    */
+  @Deprecated
   public void ASTNode.warning(String format, Object... args) {
 	  warning(String.format(format, args));
   }
@@ -322,6 +340,7 @@ aspect ErrorCheck {
    * Register a problem. Delegates to an {@link IErrorHandler}.
    * @param p   the problem.
    */
+  @Deprecated
   public void ASTNode.problem(Problem p) {
       root().getErrorHandler().problem(p);
   }
@@ -329,6 +348,7 @@ aspect ErrorCheck {
     /**
      * If this node is in a disabled conditional component, generate a warning, otherwise generate an error.
      */
+    @Deprecated
     public void ASTNode.errorUnlessDisabled(String s) {
         if (inDisabledComponent()) {
             warning("Found error in disabled conditional:\n  " + s);
@@ -340,6 +360,7 @@ aspect ErrorCheck {
     /**
      * If this node is in a disabled conditional component, generate a warning, otherwise generate an error.
      */
+    @Deprecated
     public void ASTNode.errorUnlessDisabled(String format, Object... args) {
         if (inDisabledComponent()) {
             warning("Found error in disabled conditional:\n  " + format, args);
@@ -352,6 +373,7 @@ aspect ErrorCheck {
      * If this node is in a disabled conditional component and <code>condition</code> is <code>true</code>, 
      * generate a warning, otherwise generate an error.
      */
+    @Deprecated
     public void ASTNode.errorUnlessDisabledAnd(boolean condition, String s) {
         if (condition) {
             errorUnlessDisabled(s);
@@ -364,6 +386,7 @@ aspect ErrorCheck {
      * If this node is in a disabled conditional component and <code>condition</code> is <code>true</code>, 
      * generate a warning, otherwise generate an error.
      */
+    @Deprecated
     public void ASTNode.errorUnlessDisabledAnd(boolean condition, String format, Object... args) {
         if (condition) {
             errorUnlessDisabled(format, args);

trunk/Compiler/ModelicaFrontEnd/src/java/org/jmodelica/separateProcess/Compilation.java
@@ -27,7 +27,8 @@ import java.util.concurrent.ConcurrentLinkedQueue;
 
 import org.jmodelica.util.CompiledUnit;
 import org.jmodelica.util.Criteria;
-import org.jmodelica.util.Problem;
+import org.jmodelica.util.problemHandling.Problem;
+import org.jmodelica.util.problemHandling.ProblemSeverity;
 import org.jmodelica.util.streams.StreamGobbler;
 import org.jmodelica.util.collections.FilteredIterator;
 import org.jmodelica.util.logging.ObjectStreamLogger;
@@ -87,7 +88,7 @@ public final class Compilation {
         return new FilteredIterator<Problem>(getProblems(), new Criteria<Problem>() {
             @Override
             public boolean test(Problem elem) {
-                return elem.severity() == Problem.Severity.ERROR;
+                return elem.severity() == ProblemSeverity.ERROR;
             }});
     }
     
@@ -95,7 +96,7 @@ public final class Compilation {
         return new FilteredIterator<Problem>(getProblems(), new Criteria<Problem>() {
             @Override
             public boolean test(Problem elem) {
-                return elem.severity() == Problem.Severity.WARNING;
+                return elem.severity() == ProblemSeverity.WARNING;
             }});
     }
     

trunk/Compiler/ModelicaFrontEnd/src/java/org/jmodelica/util/exceptions/CompilerException.java
@@ -18,7 +18,8 @@ package org.jmodelica.util.exceptions;
 import java.util.ArrayList;
 import java.util.Collection;
 
-import org.jmodelica.util.Problem;
+import org.jmodelica.util.problemHandling.Problem;
+import org.jmodelica.util.problemHandling.ProblemSeverity;
 
 /**
  * Exception containing a list of compiler errors/warnings.
@@ -51,7 +52,7 @@ public class CompilerException extends ModelicaException {
      * Add a new problem.
      */
     public void addProblem(Problem p) {
-        if (p.severity() == Problem.Severity.ERROR)
+        if (p.severity() == ProblemSeverity.ERROR)
             errors.add(p);
         else
             warnings.add(p);
@@ -94,6 +95,9 @@ public class CompilerException extends ModelicaException {
      * Convert to error message.
      */
     public String getMessage() {
+        return getMessage(false);
+    }
+    public String getMessage(boolean printIdentifier) {
         StringBuilder str = new StringBuilder();
         if (!errors.isEmpty()) {
             str.append(errors.size());
@@ -105,11 +109,11 @@ public class CompilerException extends ModelicaException {
             str.append(" warnings found:\n\n");
         }
         for (Problem p : errors) {
-            str.append(p);
+            str.append(p.toString(printIdentifier));
             str.append("\n\n");
         }
         for (Problem p : warnings) {
-            str.append(p);
+            str.append(p.toString(printIdentifier));
             str.append("\n\n");
         }
         str.deleteCharAt(str.length() - 1);

trunk/Compiler/ModelicaFrontEnd/src/java/org/jmodelica/util/logging/Level.java
@@ -15,7 +15,7 @@
 */
 package org.jmodelica.util.logging;
 
-import org.jmodelica.util.Problem.Severity;
+import org.jmodelica.util.problemHandling.ProblemSeverity;
 
 
 public enum Level {
@@ -40,7 +40,7 @@ public enum Level {
         return this.compareTo(other) >= 0 && (alreadySentLevel == null || this.compareTo(alreadySentLevel) < 0);
     }
     
-    public static Level fromKind(Severity severity) {
+    public static Level fromKind(ProblemSeverity severity) {
         switch (severity) {
         case ERROR:
             return ERROR;

commit 518563a37077f16453f96f78d46500d95a059a98

Fixed ordering problem introduced in r8368. Ticket #4735.

trunk/Compiler/ModelicaFrontEnd/src/java/org/jmodelica/util/problemHandling/Problem.java
@@ -27,9 +27,6 @@ public class Problem implements Comparable<Problem>, LoggingUnit {
     private static final long serialVersionUID = 1;
     
     public int compareTo(Problem other) {
-        if (identifier != null && other.identifier != null && !identifier.equals(other.identifier)) {
-            return identifier.compareTo(other.identifier);
-        }
         if (kind.order != other.kind.order)
             return kind.order - other.kind.order;
         if (fileName == null || other.fileName == null) {
@@ -45,6 +42,9 @@ public class Problem implements Comparable<Problem>, LoggingUnit {
             return beginLine - other.beginLine;
         if (beginColumn != other.beginColumn)
             return beginColumn - other.beginColumn;
+        if (identifier != null && other.identifier != null && !identifier.equals(other.identifier)) {
+            return identifier.compareTo(other.identifier);
+        }
         return message.compareTo(other.message);
     }
 