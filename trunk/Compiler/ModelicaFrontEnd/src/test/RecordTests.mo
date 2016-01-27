jnorthrup/jmodelica

commit 793e603ea5d8fdcf4e043252cb8dbdf85509d1bb

Fixed handling of record constructors where array sizes in the record depends on an input to the constructor. (#4777)
git-svn-id: https://svn.jmodelica.org@8438 7ae51c65-13d5-4921-9d80-6511ddd1419c

trunk/Compiler/ModelicaFrontEnd/src/jastadd/flattening/Flattening.jrag
@@ -1335,30 +1335,34 @@ aspect Flattening {
 	eq FIntegerSubscript.specify(int index) = this;
 	eq FColonSubscript.specify(int index)   = new FIntegerSubscript(index);
 
-	// TODO: The names here needs to be clearer. (Down to and including getInstValueMod().)
-	/**
-	 * Check if this InstAssignable node has an InstModification setting 
-	 *        its binding expression.
-	 */
-	syn boolean InstAssignable.hasInstValueMod() = (myInstValueMod() != null);
-	
-	/**
+    // TODO: The names here needs to be clearer. (Down to and including getInstValueMod().)
+    /**
+     * Check if this InstComponentDecl is an assignable has an InstModification setting 
+     * its binding expression.
+     */
+    syn boolean InstComponentDecl.hasInstValueMod() = (myInstValueMod() != null);
+
+    /**
      * Check if this InstComponentDecl is an assignable with a value modifier
      */
     syn boolean InstComponentDecl.hasBindingExp() = false;
     eq InstAssignable.hasBindingExp()             = hasInstValueMod();
-	
-	/**
-	 * Retrieve the InstModification setting the binding expression of 
-	 *        this InstAssignable node.
-	 */
-	syn lazy InstValueModification InstAssignable.myInstValueMod() {
-		for (InstModification im : totalMergedEnvironment()) 
-			if (im.hasInstValueMod()) 
-				return im.getInstValueMod();
-		return null;
-	}
-    
+
+    /**
+     * Retrieve the InstModification setting the binding expression of 
+     * this component. 
+     * 
+     * If the component is not an assignable, or is an assignable that does 
+     * not have a binding expression, return <code>null</code>.
+     */
+    syn InstValueModification InstComponentDecl.myInstValueMod() = null;
+    syn lazy InstValueModification InstAssignable.myInstValueMod() {
+        for (InstModification im : totalMergedEnvironment()) 
+            if (im.hasInstValueMod()) 
+                return im.getInstValueMod();
+        return null;
+    }
+
     syn boolean InstAssignable.hasInstValueModTop() = myInstValueModTop() != null;
     
     syn InstValueModification InstAssignable.myInstValueModTop() {
@@ -1826,8 +1830,13 @@ aspect FlatExpressions {
     public FExp FIdUseInstAccess.flatten(FQName prefix, FIdUseExp use) {
         InstComponentDecl var = getInstAccess().myInstComponentDecl();
         if (use.inDefaultArgument()) {
-            InstClassDecl parent = (InstClassDecl) var.containingEntity();
-            int i = parent.myCallInputs().indexOf(var);
+            int i;
+            if (var.inRecordDecl()) {
+                InstClassDecl parent = (InstClassDecl) var.containingEntity();
+                i = parent.myCallInputs().indexOf(var);
+            } else { // In record constructor
+                i = var.getParent().getIndexOfChild(var);
+            }
             int d = use.findDepthOfNestledFunctionCalls();
             return new FArgumentExp(i, d);
         } else if (!inFunction() && variability().flattenExtractIndices(getInstAccess().indexVariability())) {
@@ -1953,19 +1962,25 @@ aspect FlatExpressions {
     eq FConstant.flattenExtractIndices(FTypePrefixVariability other) = !other.knownParameterOrLess();
     eq FKnownParameter.flattenExtractIndices(FTypePrefixVariability other) = !other.knownParameterOrLess();
 
-	/**
-	 * Check if this access is in the binding expression of an input to a function 
-	 * or component in a record class, and refers to an input in the same function 
-	 * or component in the same record class.
-	 */
+    /**
+     * Check if this access is in the binding expression of an input to a function 
+     * or component in a record class, and refers to an input in the same function 
+     * or component in the same record class.
+     */
     syn boolean FIdUseExp.inDefaultArgument() {
-		InstComponentDecl target = asInstAccess().myInstComponentDecl();
-		InstComponentDecl holder = findComponentWithThisBindingExp();
-		return holder != null && target.containingEntity() == holder.containingEntity() && 
-		       ((inFunction() && holder.isInput() && target.isInput()) || 
-		        (inRecordDecl() && holder.isModifiable() && target.isModifiable()));
-	}
-	
+        InstComponentDecl target = asInstAccess().myInstComponentDecl();
+        InstComponentDecl holder = findComponentWithThisBindingExp();
+        return holder != null && target.containingEntity() == holder.containingEntity() && 
+               ((inFunction() && holder.isInput() && target.isInput()) || 
+                (holder.inNonComponentRecord() && holder.isModifiable() && target.isModifiable()));
+    }
+
+    /**
+     * Check if this component is in a record, but not in a record component - 
+     * i.e. a record declaration or a record constructor.
+     */
+    syn boolean InstComponentDecl.inNonComponentRecord() = inRecordDecl() || inRecordConstructor();
+
 	/**
 	 * Find the component that has this expression as a part of its binding expression, if any.
 	 */
@@ -2532,9 +2547,88 @@ aspect FlatExpressions {
     eq InstArrayAccess.getChild().flatPrefix() = myInstComponentDecl().getFQName().copyPrefix();
     eq InstNode.getChild().flatPrefix() = new FQNameEmpty();
     eq FlatRoot.getChild().flatPrefix() = new FQNameEmpty();
-    
-	public FRecordConstructor InstRecordConstructor.flatten(FQName prefix) {
-		List args = new List();
+
+    // TODO: move stuff to InstanceTree.jrag & InstFunctionBindin.jrag as appropriate
+    private boolean InstRecordConstructor.constructorArgsRebound = false;
+    rewrite InstRecordConstructor {
+        when (!constructorArgsRebound) to InstRecordConstructor {
+            for (int i = 0; i < getNumArg(); i++) {
+                getArg(i).updateBoundInput(getInstComponentDecl(i));
+            }
+            constructorArgsRebound = true;
+            return this;
+        }
+    }
+
+    public void InstFunctionArgument.updateBoundInput(InstComponentDecl icd) {
+        setBoundInput(icd);
+    }
+
+    public void InstDefaultArgument.updateBoundInput(InstComponentDecl icd) {
+        setBoundInput(icd);
+        setFExp(icd.myInstValueMod().getFExp());
+    }
+
+    syn nta List<InstComponentDecl> InstRecordConstructor.getInstComponentDeclList() {
+        InstClassDecl icd = getRecord().myInstClassDecl().actualInstClass();
+        List<InstComponentDecl> l = new List<InstComponentDecl>();
+        icd.populateRecordConstructorComponentList(l, new HashSet<String>());
+        return l;
+    }
+
+    protected void InstNode.populateRecordConstructorComponentList(
+            List<InstComponentDecl> list, Set<String> names) {
+        for (InstExtends ie : getInstExtendss()) {
+            ie.populateRecordConstructorComponentList(list, names);
+        }
+        for (ComponentDecl cd : components()) {
+            if (names.add(cd.name())) {
+                list.add(createInstComponentDecl(cd));
+            }
+        }
+    }
+
+    syn nta List<InstModification> InstRecordConstructor.getInstModificationList() {
+        List<InstModification> res = new List<InstModification>();
+        for (InstFunctionArgument ifa : getArgs()) {
+            ifa.populateInstModifications(res);
+        }
+        return res;
+    }
+
+    public void InstFunctionArgument.populateInstModifications(List<InstModification> res) {}
+
+    public void InstPositionalArgument.populateInstModifications(List<InstModification> res) {
+        res.add(newInstModification());
+    }
+
+    public void InstNamedArgument.populateInstModifications(List<InstModification> res) {
+        res.add(newInstModification());
+    }
+
+    syn InstModification InstFunctionArgument.newInstModification() = 
+            new InstComponentModification(getModification(), false, false,
+                boundInput.createInstAccess(),
+                new Opt<InstModification>(getModification().newInstModification()));
+
+    //syn InstAccess InstFunctionArgument.createComponentAccess() = boundInput.createAccess();
+    //eq InstNamedArgument.createComponentAccess() = getName().treeCopy();
+
+    syn InstValueModification DummyModification.newInstModification() = new InstValueModification(this);
+
+    syn nta Modification InstFunctionArgument.getModification() {
+        DummyModification res = new DummyModification();
+        copyLocationTo(res);
+        return res;
+    }
+
+    inh FExp DummyModification.myFExp();
+    eq InstFunctionArgument.getChild().myFExp() = null;
+    eq InstPositionalArgument.getChild().myFExp() = getFExp();
+    eq InstNamedArgument.getChild().myFExp() = getFExp();
+
+    public FRecordConstructor InstRecordConstructor.flatten(FQName prefix) {
+        List args = new List();
         int i = 0;
         for (InstComponentDecl icd : getRecord().myInstClassDecl().allInstComponentDecls()) {
             if (icd.isModifiable()) {
@@ -2543,9 +2637,9 @@ aspect FlatExpressions {
                 args.add(icd.flattenInRecordConstructor(prefix));
             }
         }
-		return new FRecordConstructor(getRecord().flattenRecordName(), args);		
-	}
-	
+        return new FRecordConstructor(getRecord().flattenRecordName(), args);
+    }
+
     public FExp InstComponentDecl.flattenInRecordConstructor(FQName prefix) {
         return null;
     }

trunk/Compiler/ModelicaFrontEnd/src/jastadd/instance/InstLookupClasses.jrag
@@ -537,7 +537,9 @@ aspect InstLookupClasses {
     }
     eq InstDot.myInstClassDecl()          = getLastInstAccess().myInstClassDecl();
     eq InstGlobalAccess.myInstClassDecl() = getInstAccess().myInstClassDecl();
-    syn InstClassDecl InstFunctionCall.myInstClassDecl() = getName().myInstClassDecl(); 
+
+    syn InstClassDecl InstFunctionCall.myInstClassDecl()      = getName().myInstClassDecl(); 
+    syn InstClassDecl InstRecordConstructor.myInstClassDecl() = getRecord().myInstClassDecl(); 
 
     syn InstClassDecl InstNode.myInstClass()  = unknownInstClassDecl(); 
     eq InstComponentDecl.myInstClass()        = getClassName().myInstClassDecl();

trunk/Compiler/ModelicaFrontEnd/src/jastadd/instance/InstLookupComponents.jrag
@@ -510,6 +510,17 @@ aspect InstLookupComponents {
                 return InstLookupResult.<InstComponentDecl>found(((InstForIndex) fi).getInstPrimitive());
         return lookupInstComponent(name);
     }
+    
+    inh lazy InstLookupResult<InstComponentDecl> InstRecordConstructor.lookupInstComponent(String name);
+    eq InstRecordConstructor.getChild().lookupInstComponent(String name) = lookupInstComponent(name);
+    eq InstRecordConstructor.getInstComponentDecl().lookupInstComponent(String name) {
+        for (InstComponentDecl icd : getInstComponentDecls()) {
+            if (icd.matches(name)) {
+                return InstLookupResult.<InstComponentDecl>found(icd);
+            }
+        }
+        return lookupInstComponent(name);
+    }
 
     syn lazy InstLookupResult<InstComponentDecl> InstNode.genericLookupInstComponent(String name) {
         InstLookupResult<InstComponentDecl> res = memberInstComponent(name);

trunk/Compiler/ModelicaFrontEnd/src/jastadd/instance/InstanceTree.jrag
@@ -398,6 +398,8 @@ aspect Environments {
     */
     syn lazy Environment InstNode.getMergedEnvironment() = 
         filteredEnvironment().mergeInnerClone(getElementInstModifications(), localInstModifications());
+    syn lazy Environment InstRecordConstructor.getMergedEnvironment() = 
+        myInstClassDecl().getMergedEnvironment().mergeOuterClone(getInstModifications());
 
 	syn Environment InstNode.filteredEnvironment() = nameScope() ? myEnvironment(name()) : myEnvironment();
 	
@@ -439,9 +441,12 @@ aspect Environments {
      * the environment is filtered on that name.
      */
     eq InstNode.getChild().myEnvironment(String name) = myEnvironment_def(name); // This is just to get caching at the right place.
+    eq InstRecordConstructor.getInstComponentDecl().myEnvironment(String name) = myEnvironment_def(name);
 
     syn lazy Environment InstNode.myEnvironment_def(String name) = 
         getMergedEnvironment().filter(name, retrieveReplacingComponent(name), retrieveReplacingClass(name));
+    syn lazy Environment InstRecordConstructor.myEnvironment_def(String name) = 
+        getMergedEnvironment().filter(name);
 
     eq InstReplacingShortClassDecl.getOriginalInstClass().myEnvironment()            = filteredEnvironment();
     eq InstReplacingShortClassDecl.getOriginalInstClass().myEnvironment(String name) = filteredEnvironment().filter(name);
@@ -1609,11 +1614,13 @@ aspect InstaceConstrainingClauses {
         return classToExpand.newInstClassDecl();
     }
 
-	syn lazy FExp InstValueModification.getFExp() = 
-		getValueModification().getExp().instantiate();
-	eq InstRecordModification.getFExp() = getValue().unboundCopy();
-	eq InstArrayModification.getFExp()  = getCell().unboundCopy();
-	
+    syn lazy FExp InstValueModification.getFExp() = getValueModification().instantiateExp();
+    eq InstRecordModification.getFExp()           = getValue().unboundCopy();
+    eq InstArrayModification.getFExp()            = getCell().unboundCopy();
+
+    syn FExp ValueModification.instantiateExp() = getExp().instantiate();
+    eq DummyModification.instantiateExp()       = myFExp().treeCopy();
+
 	public List InstNode.buildFAbstractEquationList() { 
 	    List l = new List();
 	    for (AbstractEquation e : equations()) {

commit 39577216f77c71836b9bcd421772ecc4931f7cdf

Fixed lookup issue for classes in records caused by r8438. (#4777)

trunk/Compiler/ModelicaFrontEnd/src/jastadd/flattening/Flattening.jrag
@@ -2549,79 +2549,6 @@ aspect FlatExpressions {
     eq FlatRoot.getChild().flatPrefix() = new FQNameEmpty();
 
     // TODO: move stuff to InstanceTree.jrag & InstFunctionBindin.jrag as appropriate
-    private boolean InstRecordConstructor.constructorArgsRebound = false;
-    rewrite InstRecordConstructor {
-        when (!constructorArgsRebound) to InstRecordConstructor {
-            for (int i = 0; i < getNumArg(); i++) {
-                getArg(i).updateBoundInput(getInstComponentDecl(i));
-            }
-            constructorArgsRebound = true;
-            return this;
-        }
-    }
-
-    public void InstFunctionArgument.updateBoundInput(InstComponentDecl icd) {
-        setBoundInput(icd);
-    }
-
-    public void InstDefaultArgument.updateBoundInput(InstComponentDecl icd) {
-        setBoundInput(icd);
-        setFExp(icd.myInstValueMod().getFExp());
-    }
-
-    syn nta List<InstComponentDecl> InstRecordConstructor.getInstComponentDeclList() {
-        InstClassDecl icd = getRecord().myInstClassDecl().actualInstClass();
-        List<InstComponentDecl> l = new List<InstComponentDecl>();
-        icd.populateRecordConstructorComponentList(l, new HashSet<String>());
-        return l;
-    }
-
-    protected void InstNode.populateRecordConstructorComponentList(
-            List<InstComponentDecl> list, Set<String> names) {
-        for (InstExtends ie : getInstExtendss()) {
-            ie.populateRecordConstructorComponentList(list, names);
-        }
-        for (ComponentDecl cd : components()) {
-            if (names.add(cd.name())) {
-                list.add(createInstComponentDecl(cd));
-            }
-        }
-    }
-
-    syn nta List<InstModification> InstRecordConstructor.getInstModificationList() {
-        List<InstModification> res = new List<InstModification>();
-        for (InstFunctionArgument ifa : getArgs()) {
-            ifa.populateInstModifications(res);
-        }
-        return res;
-    }
-
-    public void InstFunctionArgument.populateInstModifications(List<InstModification> res) {}
-
-    public void InstPositionalArgument.populateInstModifications(List<InstModification> res) {
-        res.add(newInstModification());
-    }
-
-    public void InstNamedArgument.populateInstModifications(List<InstModification> res) {
-        res.add(newInstModification());
-    }
-
-    syn InstModification InstFunctionArgument.newInstModification() = 
-            new InstComponentModification(getModification(), false, false,
-                boundInput.createInstAccess(),
-                new Opt<InstModification>(getModification().newInstModification()));
-
-    //syn InstAccess InstFunctionArgument.createComponentAccess() = boundInput.createAccess();
-    //eq InstNamedArgument.createComponentAccess() = getName().treeCopy();
-
-    syn InstValueModification DummyModification.newInstModification() = new InstValueModification(this);
-
-    syn nta Modification InstFunctionArgument.getModification() {
-        DummyModification res = new DummyModification();
-        copyLocationTo(res);
-        return res;
-    }
-
     inh FExp DummyModification.myFExp();
     eq InstFunctionArgument.getChild().myFExp() = null;
     eq InstPositionalArgument.getChild().myFExp() = getFExp();

trunk/Compiler/ModelicaFrontEnd/src/jastadd/instance/InstFunctionBinding.jrag
@@ -758,7 +758,30 @@ aspect InstFunctionBinding {
         }
         return res;
     }
-    
+
+    /**
+     * Re-bind arguments to virtual components under constructor call.
+     */
+    rewrite InstRecordConstructor {
+        when (!constructorArgsRebound) to InstRecordConstructor {
+            for (int i = 0; i < getNumArg(); i++) {
+                getArg(i).updateBoundInput(getInstComponentDecl(i));
+            }
+            constructorArgsRebound = true;
+            return this;
+        }
+    }
+    private boolean InstRecordConstructor.constructorArgsRebound = false;
+
+    public void InstFunctionArgument.updateBoundInput(InstComponentDecl icd) {
+        setBoundInput(icd);
+    }
+
+    public void InstDefaultArgument.updateBoundInput(InstComponentDecl icd) {
+        setBoundInput(icd);
+        setFExp(icd.myInstValueMod().getFExp());
+    }
+
     /**
      * Calculate if this is a vectorized function call.
      */

trunk/Compiler/ModelicaFrontEnd/src/jastadd/instance/InstLookupClasses.jrag
@@ -102,6 +102,9 @@ aspect InstLookupClasses {
 
     eq InstGeneratedInner.getChild().lookupInstClass(String name) = getCopiedOuter().lookupInstClass(name);
 
+    eq InstDefaultRecordModification.getChild().lookupInstClass(String name) = 
+        (getSource() != null) ? getSource().lookupInstClass(name) : lookupInstClass(name);
+
 
     syn InstLookupResult<InstClassDecl> InstClassDecl.lookupInstClassInInstClassRedeclare(String name) = InstLookupResult.notFound();
     eq InstReplacingShortClassDecl.lookupInstClassInInstClassRedeclare(String name) = getInstClassRedeclare().lookupInstClass(name);

trunk/Compiler/ModelicaFrontEnd/src/jastadd/instance/InstLookupComponents.jrag
@@ -522,6 +522,12 @@ aspect InstLookupComponents {
         return lookupInstComponent(name);
     }
 
+    /**
+     * Lookup name in the record declaration.
+     */
+    syn InstComponentDecl InstRecordConstructor.declarationInstComponent(String name) = 
+        myInstClassDecl().memberInstComponent(name).target(INST_UNKNOWN_COMPONENT, this);
+
     syn lazy InstLookupResult<InstComponentDecl> InstNode.genericLookupInstComponent(String name) {
         InstLookupResult<InstComponentDecl> res = memberInstComponent(name);
         if (res.successful())

trunk/Compiler/ModelicaFrontEnd/src/jastadd/instance/InstanceTree.jrag
@@ -399,7 +399,7 @@ aspect Environments {
     syn lazy Environment InstNode.getMergedEnvironment() = 
         filteredEnvironment().mergeInnerClone(getElementInstModifications(), localInstModifications());
     syn lazy Environment InstRecordConstructor.getMergedEnvironment() = 
-        myInstClassDecl().getMergedEnvironment().mergeOuterClone(getInstModifications());
+        new Environment().mergeOuterClone(getInstModifications());
 
 	syn Environment InstNode.filteredEnvironment() = nameScope() ? myEnvironment(name()) : myEnvironment();
 	
@@ -446,7 +446,7 @@ aspect Environments {
     syn lazy Environment InstNode.myEnvironment_def(String name) = 
         getMergedEnvironment().filter(name, retrieveReplacingComponent(name), retrieveReplacingClass(name));
     syn lazy Environment InstRecordConstructor.myEnvironment_def(String name) = 
-        getMergedEnvironment().filter(name);
+        declarationInstComponent(name).filteredEnvironment().mergeOuterClone(getMergedEnvironment().filter(name));
 
     eq InstReplacingShortClassDecl.getOriginalInstClass().myEnvironment()            = filteredEnvironment();
     eq InstReplacingShortClassDecl.getOriginalInstClass().myEnvironment(String name) = filteredEnvironment().filter(name);
@@ -907,6 +907,60 @@ aspect InstanceTreeConstruction {
 	eq InstPrimitive.getInstComponentDeclList() = new List();
 	eq InstPrimitive.getInstClassDeclList() = new List();
 
+    syn InstComponentDecl InstRecordConstructor.getInstComponentDeclNoTransform(int i) =
+        getInstComponentDeclListNoTransform().getChildNoTransform(i);
+
+    syn nta List<InstComponentDecl> InstRecordConstructor.getInstComponentDeclList() {
+        InstClassDecl icd = getRecord().myInstClassDecl().actualInstClass();
+        List<InstComponentDecl> l = new List<InstComponentDecl>();
+        icd.populateRecordConstructorComponentList(l, new HashSet<String>());
+        return l;
+    }
+
+    protected void InstNode.populateRecordConstructorComponentList(
+            List<InstComponentDecl> list, Set<String> names) {
+        for (InstExtends ie : getInstExtendss()) {
+            ie.populateRecordConstructorComponentList(list, names);
+        }
+        for (InstComponentDecl icd : getInstComponentDecls()) {
+            if (names.add(icd.name())) {
+                list.add(createInstComponentDecl(icd.getComponentDecl(), icd));
+            }
+        }
+    }
+
+    syn nta List<InstModification> InstRecordConstructor.getInstModificationList() {
+        List<InstModification> res = new List<InstModification>();
+        for (InstFunctionArgument ifa : getArgs()) {
+            ifa.populateInstModifications(res);
+        }
+        return res;
+    }
+
+    public void InstFunctionArgument.populateInstModifications(List<InstModification> res) {}
+
+    public void InstPositionalArgument.populateInstModifications(List<InstModification> res) {
+        res.add(newInstModification());
+    }
+
+    public void InstNamedArgument.populateInstModifications(List<InstModification> res) {
+        res.add(newInstModification());
+    }
+
+    syn InstModification InstFunctionArgument.newInstModification() = 
+            new InstComponentModification(getModification(), false, false,
+                boundInput.createInstAccess(),
+                new Opt<InstModification>(getModification().newInstModification()));
+
+    syn InstValueModification DummyModification.newInstModification() = new InstValueModification(this);
+
+    syn nta Modification InstFunctionArgument.getModification() {
+        DummyModification res = new DummyModification();
+        copyLocationTo(res);
+        return res;
+    }
+
+
 	syn lazy List<InstModification> InstNode.getElementInstModificationList() {
         List<InstModification> l = new List<InstModification>();
         for (Modification m : elementModifications()) 
@@ -916,6 +970,10 @@ aspect InstanceTreeConstruction {
 
 
     public InstComponentDecl InstNode.createInstComponentDecl(ComponentDecl cd) {
+        return createInstComponentDecl(cd, null);
+    }
+
+    public InstComponentDecl InstNode.createInstComponentDecl(ComponentDecl cd, InstLookupRedirectNode ln) {
         // Check if the component is redeclared. 
         // -> Yes: Create an InstReplacingComponent component
         // -> No: Create an InstComponent 
@@ -926,7 +984,7 @@ aspect InstanceTreeConstruction {
             return new InstCreateReplacingComponentDecl(replacingComp, irc, cd, irc, cicr);
             // TODO: handle expandable connectors in this case
         } else { 
-            return new InstCreateComponentDecl(cd, null);       
+            return new InstCreateComponentDecl(cd, ln);
         }
      }
 

trunk/Compiler/ModelicaFrontEnd/src/test/RecordTests.mo
@@ -2465,6 +2465,125 @@ end RecordTests.RecordConstructor16;
 end RecordConstructor16;
 
 
+model RecordConstructor17
+    package A
+        type B = Real;
+        
+        record C
+            B b1 = 1;
+            B b2 = 2;
+        end C;
+    end A;
+    
+    model D
+        A.C c = A.C(3);
+    end D;
+    
+    D d;
+
+    annotation(__JModelica(UnitTesting(tests={
+        FlatteningTestCase(
+            name="RecordConstructor17",
+            description="",
+            flatModel="
+fclass RecordTests.RecordConstructor17
+ RecordTests.RecordConstructor17.A.C d.c = RecordTests.RecordConstructor17.A.C(3, 2);
+
+public
+ record RecordTests.RecordConstructor17.A.C
+  Real b1;
+  Real b2;
+ end RecordTests.RecordConstructor17.A.C;
+
+end RecordTests.RecordConstructor17;
+")})));
+end RecordConstructor17;
+
+
+model RecordConstructor18
+    package A
+        type B = Real;
+        
+        record C
+            B b = 1;
+        end C;
+    end A;
+    
+    package D
+        type E = Real;
+        
+        record F
+            extends A.C(b = 2);
+            E e = 3;
+        end F;
+    end D;
+    
+    model G
+        D.F f = D.F(e = 4);
+    end G;
+    
+    G g;
+
+    annotation(__JModelica(UnitTesting(tests={
+        FlatteningTestCase(
+            name="RecordConstructor18",
+            description="",
+            flatModel="
+fclass RecordTests.RecordConstructor18
+ RecordTests.RecordConstructor18.D.F g.f(b = 2) = RecordTests.RecordConstructor18.D.F(2, 4);
+
+public
+ record RecordTests.RecordConstructor18.D.F
+  Real b;
+  Real e;
+ end RecordTests.RecordConstructor18.D.F;
+
+end RecordTests.RecordConstructor18;
+")})));
+end RecordConstructor18;
+
+
+model RecordConstructor19
+    package A
+        record B
+            Real x = 1;
+            Real y = 2;
+        end B;
+    end A;
+    
+    package C
+        package D
+            constant Real c = 3;
+        end D;
+        
+        record E = A.B(y = D.c);
+    end C;
+
+    model F
+        C.E b = C.E(4);
+    end F;
+    
+    F f;
+
+    annotation(__JModelica(UnitTesting(tests={
+        FlatteningTestCase(
+            name="RecordConstructor19",
+            description="",
+            flatModel="
+fclass RecordTests.RecordConstructor19
+ RecordTests.RecordConstructor19.C.E f.b(y = 3.0) = RecordTests.RecordConstructor19.C.E(4, 3.0);
+
+public
+ record RecordTests.RecordConstructor19.C.E
+  Real x;
+  Real y;
+ end RecordTests.RecordConstructor19.C.E;
+
+end RecordTests.RecordConstructor19;
+")})));
+end RecordConstructor19;
+
+
 model RecordScalarize1
  record A
   Real a;