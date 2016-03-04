jnorthrup/jmodelica

commit 68997986ebad5d6a451f7c49022b44ed159be91a

#4839 Updated constant evaluation of uninitialized function variables to work similar to runtime evaluation
git-svn-id: https://svn.jmodelica.org@8524 7ae51c65-13d5-4921-9d80-6511ddd1419c

trunk/Compiler/ModelicaFlatTree/src/jastadd/ConstantEvaluation.jrag
@@ -1743,6 +1743,7 @@ aspect ConstantEvaluation {
     public CValue FType.zeroCValueScalar()        { return CValue.UNSUPPORTED; }
     public CValue FRealType.zeroCValueScalar()    { return new CValueReal(0.0); }
     public CValue FIntegerType.zeroCValueScalar() { return new CValueInteger(0); }
+    public CValue FEnumType.zeroCValueScalar()    { return new CValueEnum(this, 1); }
     public CValue FStringType.zeroCValueScalar()  { return new CValueString(""); }
     public CValue FBooleanType.zeroCValueScalar() { return new CValueBoolean(false); }
     
@@ -3251,7 +3252,6 @@ aspect ConstantEvaluation {
         public String  name_C();
         public boolean isInput();
         public boolean isOutput();
-        public boolean canBeUnknown();
         
         public FAbstractVariable asFAbstractVariable();
     }
@@ -3261,10 +3261,6 @@ aspect ConstantEvaluation {
     
     syn FAbstractVariable FAbstractVariable.asFAbstractVariable() = this;
     syn FAbstractVariable InstComponentDecl.asFAbstractVariable() = null;
-    
-    syn boolean InstComponentDecl.canBeUnknown() = !isInput() && !hasBindingExp();
-    syn boolean FAbstractVariable.canBeUnknown() = !isInput() && !hasBindingExp();
-    eq FFunctionVariable.canBeUnknown()          = super.canBeUnknown() && !hasBindingAssignment;
   
   syn FExp FAbstractFunctionCall.argument(int i) {
 	  for (FExp e : childFExps())
@@ -3600,7 +3596,7 @@ aspect ConstantEvaluation {
     public void FAbstractVariable.readEvaluationValue(Map<CommonVariableDecl, CValue> map) {
         CValue val = map.get(this);
         if (val == null) {
-            val = cevalBindingExp();
+            val = cevalFunctionBindingExp(ASTNode.defaultVariableEvaluator());
         }
         if (!isInput()) {
             val = val.cached();
@@ -3617,7 +3613,7 @@ aspect ConstantEvaluation {
     public void InstComponentDecl.readEvaluationValue(Map<CommonVariableDecl, CValue> map) {
         CValue val = map.get(this);
         if (val == null) {
-            val = cevalBindingExp();
+            val = cevalFunctionBindingExp(ASTNode.defaultVariableEvaluator());
         }
         if (!isInput()) {
             val = val.cached();
@@ -3627,31 +3623,23 @@ aspect ConstantEvaluation {
     }
     
     
-    protected CValue FAbstractVariable.cevalBindingExp() {
-        return CValue.UNKNOWN;
-    }
-    
-    protected CValue FFunctionVariable.cevalBindingExp() {
-        return cevalBindingExp(defaultVariableEvaluator());
-    }
-
-    protected CValue FFunctionVariable.cevalBindingExp(VariableEvaluator evaluator) {
-        return hasBindingExp() ? getBindingExp().ceval(evaluator) : CValue.UNKNOWN;
-    }
-
-    protected CValue InstComponentDecl.cevalBindingExp() {
-        return cevalBindingExp(defaultVariableEvaluator());
-    }
-
-    protected CValue InstComponentDecl.cevalBindingExp(VariableEvaluator evaluator) {
+    protected CValue FAbstractVariable.cevalFunctionBindingExp(VariableEvaluator evaluator) {
         return CValue.UNKNOWN;
     }
 
-    protected CValue InstAssignable.cevalBindingExp(VariableEvaluator evaluator) {
-        return hasBindingFExp() ? myBindingInstExp().ceval(evaluator) : CValue.UNKNOWN;
+    protected CValue FFunctionVariable.cevalFunctionBindingExp(VariableEvaluator evaluator) {
+        return hasBindingExp() ? getBindingExp().ceval(evaluator) : type().zeroCValue();
     }
 
-    protected CValue InstRecord.cevalBindingExp(VariableEvaluator evaluator) {
+    protected CValue InstComponentDecl.cevalFunctionBindingExp(VariableEvaluator evaluator) {
+        return CValue.UNKNOWN;
+    }
+
+    protected CValue InstAssignable.cevalFunctionBindingExp(VariableEvaluator evaluator) {
+        return hasBindingFExp() ? myBindingInstExp().ceval(evaluator) : type().zeroCValue();
+    }
+
+    protected CValue InstRecord.cevalFunctionBindingExp(VariableEvaluator evaluator) {
         if (hasBindingFExp())
             return myBindingInstExp().ceval(evaluator);
         FRecordType type = (FRecordType) type().scalarType();
@@ -3684,7 +3672,7 @@ aspect ConstantEvaluation {
         CValueRecord res = new CValueRecord(type);
         evaluationValue = res;
         for (InstComponentDecl ch : allInstComponentDecls())
-            res.setMember(ch.name(), ch.cevalBindingExp(evaluator));
+            res.setMember(ch.name(), ch.cevalFunctionBindingExp(evaluator));
         return res;
     }
 

trunk/Compiler/ModelicaFlatTree/src/jastadd/ExternalConstantEvaluation.jrag
@@ -381,7 +381,7 @@ aspect ExternalConstantEvaluationCaching {
                 com.startTimer(timeout);
                 com.accept("START");
                 for (CommonVariableDecl eo : ext.externalObjectsToSerialize()) {
-                    com.put(values.get(eo), eo.type(), eo.canBeUnknown());
+                    com.put(values.get(eo), eo.type());
                 }
                 com.accept("READY");
                 com.cancelTimer();
@@ -392,7 +392,7 @@ aspect ExternalConstantEvaluationCaching {
                 com.startTimer(timeout);
                 com.check("EVAL");
                 for (CommonVariableDecl arg : ext.functionArgsToSerialize()) {
-                    com.put(values.get(arg), arg.type(), arg.canBeUnknown());
+                    com.put(values.get(arg), arg.type());
                 }
                 com.accept("CALC");
                 com.accept("DONE");
@@ -635,9 +635,9 @@ aspect ExternalProcessCommunication {
         /**
          * Print <code>val</code>, serialized, to the process 
          */
-        public void put(CValue val, FType type, boolean allowUninitialized) throws IOException {
+        public void put(CValue val, FType type) throws IOException {
 //            ModelicaCompiler.log.debug("ProcessCommunicator WRITE: " + val.toString() + " of type: "+type.toString());
-            val.serialize(out, type, allowUninitialized);
+            val.serialize(out, type);
             out.flush();
         }
         
@@ -793,53 +793,47 @@ aspect ExternalProcessCommunication {
     /**
      * Print this constant value to <code>buff</code>
      */
-    public void CValue.serialize(BufferedWriter buff, FType type, boolean allowUninitialized) throws IOException {
+    public void CValue.serialize(BufferedWriter buff, FType type) throws IOException {
         throw new IOException("Unsupported type to serialize '" + getClass().getSimpleName() + "'");
     }
     
-    public void CValueUnknown.serialize(BufferedWriter buff, FType type, boolean allowUninitialized) throws IOException {
-        if (allowUninitialized) {
-            type.uninitialized().serialize(buff, type, false);
-        } else {
-            throw new IOException("Uninitialized value when expecting initialized");
-        }
+    public void CValueUnknown.serialize(BufferedWriter buff, FType type) throws IOException {
+        throw new IOException("Uninitialized value when expecting initialized");
     }
-    public void CValueArray.serialize(BufferedWriter buff, FType type, boolean allowUninitialized) throws IOException {
+    public void CValueArray.serialize(BufferedWriter buff, FType type) throws IOException {
         for (int s : size().size) {
             buff.write("" + s + "\n");
         }
         for (Index i : indices()) {
-            getCell(i).serialize(buff, type.scalarType(), allowUninitialized);
+            getCell(i).serialize(buff, type.scalarType());
         }
     }
-    public void CValueReal.serialize(BufferedWriter buff, FType type, boolean allowUninitialized) throws IOException {
+    public void CValueReal.serialize(BufferedWriter buff, FType type) throws IOException {
         buff.write(Double.toString(realValue()));
         buff.write("\n");
     }
-    public void CValueInteger.serialize(BufferedWriter buff, FType type, boolean allowUninitialized) throws IOException {
+    public void CValueInteger.serialize(BufferedWriter buff, FType type) throws IOException {
         buff.write(Integer.toString(intValue()));
         buff.write("\n");
     }
-    public void CValueBoolean.serialize(BufferedWriter buff, FType type, boolean allowUninitialized) throws IOException {
+    public void CValueBoolean.serialize(BufferedWriter buff, FType type) throws IOException {
         buff.write(booleanValue() ? "1\n" : "0\n");
     }
-    public void CValueString.serialize(BufferedWriter buff, FType type, boolean allowUninitialized) throws IOException {
+    public void CValueString.serialize(BufferedWriter buff, FType type) throws IOException {
         String s = stringValue();
         buff.write("" + s.length() + " ");
         buff.write(s);
         buff.write("\n");
     }
-    public void CValueEnum.serialize(BufferedWriter buff, FType type, boolean allowUninitialized) throws IOException {
+    public void CValueEnum.serialize(BufferedWriter buff, FType type) throws IOException {
         buff.write(Integer.toString(intValue()));
         buff.write("\n");
     }
-    public void CValueExternalObject.serialize(BufferedWriter buff, FType type, boolean allowUninitialized) throws IOException {
-        for (CValue v : values)
-            v.serialize(buff, type, allowUninitialized);
+    public void CValueExternalObject.serialize(BufferedWriter buff, FType type) throws IOException {
+        for (CValue v : values) {
+            v.serialize(buff, type);
+        }
     }
-    
-    public CValue FType.uninitialized() { return zeroCValue(); }
-    public CValue FEnumType.uninitialized() { return (new FIntegerType(size())).zeroCValue(); }
     
     
     /**

trunk/Compiler/ModelicaFrontEnd/src/test/ArrayTests.mo
@@ -7908,36 +7908,8 @@ model ArraySize4
             description="",
             flatModel="
 fclass ArrayTests.Other.ArraySize4
- parameter Boolean y[1,1];
- parameter Boolean y[1,2];
- parameter Boolean y[1,3];
- parameter Boolean y[2,1];
- parameter Boolean y[2,2];
- parameter Boolean y[2,3];
- parameter Boolean y[3,1];
- parameter Boolean y[3,2];
- parameter Boolean y[3,3];
-parameter equation
- ({{y[1,1], y[1,2], y[1,3]}, {y[2,1], y[2,2], y[2,3]}, {y[3,1], y[3,2], y[3,3]}}) = ArrayTests.Other.ArraySize4.f({1, 2, 3, 4}, {1, 2, 3, 4});
-
-public
- function ArrayTests.Other.ArraySize4.f
-  input Real[:] x1;
-  input Real[:] x2;
-  output Boolean[:,:] y;
-  Integer n1;
-  Integer n2;
- algorithm
-  size(y) := {size(x1, 1) - 1, size(x2, 1) - 1};
-  n1 := size(x1, 1) - 1;
-  n2 := size(x1, 1) - 1;
-  for i in 1:n1 loop
-  end for;
-  return;
- end ArrayTests.Other.ArraySize4.f;
-
+ constant Boolean y[1,1] = false;
 end ArrayTests.Other.ArraySize4;
-
 ")})));
 end ArraySize4;
 

trunk/Compiler/ModelicaMiddleEnd/src/test/VariabilityPropagationTests.mo
@@ -1846,26 +1846,8 @@ model StructParam1
             description="Test propagation of structural parameters",
             flatModel="
 fclass VariabilityPropagationTests.StructParam1
- eval parameter Real temp_1.b;
- structural parameter Real a = 1.0 /* 1.0 */;
- eval parameter Real r.b;
-parameter equation
- (VariabilityPropagationTests.StructParam1.R(, temp_1.b)) = VariabilityPropagationTests.StructParam1.f();
- r.b = temp_1.b;
-
-public
- function VariabilityPropagationTests.StructParam1.f
-  output VariabilityPropagationTests.StructParam1.R r;
- algorithm
-  r.a := 1;
-  return;
- end VariabilityPropagationTests.StructParam1.f;
-
- record VariabilityPropagationTests.StructParam1.R
-  Real a;
-  Real b;
- end VariabilityPropagationTests.StructParam1.R;
-
+ eval parameter Real r.b = 0.0 /* 0.0 */;
+ constant Real a = 1;
 end VariabilityPropagationTests.StructParam1;
 ")})));
 end StructParam1;

commit 3fae1e567f4daf1885c0608b5e8a58558dca9a04

#4839 Fixes bug introduced in r8524

trunk/Compiler/ModelicaFlatTree/src/jastadd/ConstantEvaluation.jrag
@@ -3628,7 +3628,7 @@ aspect ConstantEvaluation {
     }
 
     protected CValue FFunctionVariable.cevalFunctionBindingExp(VariableEvaluator evaluator) {
-        return hasBindingExp() ? getBindingExp().ceval(evaluator) : type().zeroCValue();
+        return hasBindingExp() ? getBindingExp().ceval(evaluator) : CValue.UNKNOWN;
     }
 
     protected CValue InstComponentDecl.cevalFunctionBindingExp(VariableEvaluator evaluator) {
@@ -4202,6 +4202,7 @@ aspect ConstantEvaluation {
   
   @Override
   public int FInitArrayStmt.evaluate(AlgorithmEvaluator evaluator) {
+      getFIdUseExp().assignToInFuncEval(evaluator, getFIdUseExp().type().zeroCValue());
 	  return EVAL_CONT;
   }
 

trunk/Compiler/ModelicaFlatTree/src/test/EvaluationTests.mo
@@ -3947,6 +3947,28 @@ end EvaluationTests.AssigningCached4;
 ")})));
 end AssigningCached4;
 
+model AssigningCached5
+    function f
+        input Integer n;
+        Real[n2] k = 1:n2;
+        Integer n2 = n;
+        output Real u = sum(k);
+    algorithm
+        annotation(Inline=false);
+    end f;
+
+    Real y = f(1);
+    
+    annotation(__JModelica(UnitTesting(tests={
+        TransformCanonicalTestCase(
+            name="AssigningCached5",
+            description="",
+            flatModel="
+fclass EvaluationTests.AssigningCached5
+ constant Real y = 1.0;
+end EvaluationTests.AssigningCached5;
+")})));
+end AssigningCached5;
 
 
 model ParameterMinMax1