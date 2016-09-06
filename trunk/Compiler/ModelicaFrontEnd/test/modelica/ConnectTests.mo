jnorthrup/jmodelica

commit 348bbd8fa7173f28efe17340a7d01aecfc57777b

Fixed crash due to cached size for range expression in for loop in function. (#5102)
git-svn-id: https://svn.jmodelica.org@9165 7ae51c65-13d5-4921-9d80-6511ddd1419c

trunk/Compiler/ModelicaFlatTree/src/jastadd/Arrays.jrag
@@ -726,7 +726,7 @@ aspect Arrays {
     }
 
     public FExp FEndExp.makeRangeSizeExp() {
-        return type().wrapRangeSizeExp(mySize().createFExp(0));
+        return type().wrapRangeSizeExp(mySize().createFExpForced(0));
     }
 
  	/**

trunk/Compiler/ModelicaFrontEnd/test/modelica/FunctionTests.mo
@@ -6915,6 +6915,47 @@ end FunctionTests.ArrayExpInFunc45;
 ")})));
 end ArrayExpInFunc45;
 
+
+model ArrayExpInFunc46
+    function f
+        input Real x[:];
+        output Real y[size(x,1)-1];
+    algorithm
+        for i in 1:2 loop
+            y := x[2:end];
+        end for;
+	end f;
+    
+    parameter Real[:] x1 = f({1,2,3,4,5}) annotation(Evaluate=true);
+    parameter Real[:] x2 = f({6,7,8}) annotation(Evaluate=true);
+
+    annotation(__JModelica(UnitTesting(tests={
+        FlatteningTestCase(
+            name="ArrayExpInFunc46",
+            description="Check that we don't crash due to cached size for range expression in for loop in function",
+            flatModel="
+fclass FunctionTests.ArrayExpInFunc46
+ eval parameter Real x1[4] = {2, 3, 4, 5} /* { 2, 3, 4, 5 } */;
+ eval parameter Real x2[2] = {7, 8} /* { 7, 8 } */;
+
+public
+ function FunctionTests.ArrayExpInFunc46.f
+  input Real[:] x;
+  output Real[:] y;
+ algorithm
+  init y as Real[size(x, 1) - 1];
+  for i in 1:2 loop
+   y[:] := x[2:size(x, 1)];
+  end for;
+  return;
+ end FunctionTests.ArrayExpInFunc46.f;
+
+end FunctionTests.ArrayExpInFunc46;
+")})));
+end ArrayExpInFunc46;
+
+
+
 model ArrayOutputScalarization1
  function f
   output Real x[2] = {1,2};

commit 5cc9d5a51addf9e02e85f2fb23ca875c252557e7

Fixed errors in size calculation caused by r9165. (#5102)

trunk/Compiler/ModelicaFlatTree/src/jastadd/Arrays.jrag
@@ -725,10 +725,6 @@ aspect Arrays {
         return type().wrapRangeSizeExp(fullCopy());
     }
 
-    public FExp FEndExp.makeRangeSizeExp() {
-        return type().wrapRangeSizeExp(mySize().createFExpForced(0));
-    }
-
  	/**
  	 * Wrap the given expression so it is suitable for use in an expression 
  	 *        calculating the array size of a range expression.

trunk/Compiler/ModelicaFlatTree/src/jastadd/ConstantEvaluation.jrag
@@ -4646,17 +4646,19 @@ aspect ArrayConstantEvaluation {
 }
 
 aspect LiteralExpressions {
-	
-	syn boolean FExp.hasOnlyLiterals() {
-		for (FExp e : childFExps())
-			if (!e.hasOnlyLiterals())
-				return false;
-		return true;
-	}
-	
-	eq FIdUseExp.hasOnlyLiterals()      = false;
-	eq FFunctionCall.hasOnlyLiterals()  = false;
-	
+
+    syn boolean FExp.hasOnlyLiterals() {
+        for (FExp e : childFExps())
+            if (!e.hasOnlyLiterals())
+                return false;
+        return true;
+    }
+
+    eq FIdUseExp.hasOnlyLiterals()      = false;
+    eq FFunctionCall.hasOnlyLiterals()  = false;
+    eq FTimeExp.hasOnlyLiterals()       = false;
+    eq FEndExp.hasOnlyLiterals()        = !inFunction();
+
 }
 
 aspect CircularExpressions {

trunk/Compiler/ModelicaFrontEnd/test/modelica/ConnectTests.mo
@@ -1299,6 +1299,41 @@ end ConnectTests.ConnectTest29;
 end ConnectTest29;
 
 
+model ConnectTest30
+    connector C
+        Real a;
+    end C;
+    
+    model A
+        parameter Integer n = 2;
+        C c[n];
+    end A;
+    
+    A a1(c(a={1,2}));
+    A a2;
+equation
+    connect(a1.c, a2.c[1:end]);
+
+    annotation(__JModelica(UnitTesting(tests={
+        FlatteningTestCase(
+            name="ConnectTest30",
+            description="Size of range exp using end in connect, where size is discribed by parameter in other scope",
+            flatModel="
+fclass ConnectTests.ConnectTest30
+ structural parameter Integer a1.n = 2 /* 2 */;
+ Real a1.c[1].a = 1;
+ Real a1.c[2].a = 2;
+ structural parameter Integer a2.n = 2 /* 2 */;
+ Real a2.c[1].a;
+ Real a2.c[2].a;
+equation
+ a1.c[1].a = a2.c[1].a;
+ a1.c[2].a = a2.c[2].a;
+end ConnectTests.ConnectTest30;
+")})));
+end ConnectTest30;
+
+
 
 model ConnectOuterTest1
     connector C = Real;