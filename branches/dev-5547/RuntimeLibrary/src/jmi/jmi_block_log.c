jnorthrup/jmodelica

commit 6d4f11431ea72cadc0ce3e55f1e98e69dac508ef

#5547 Refactored illegal_value checks and added work vectors to jmi struct.
git-svn-id: https://svn.jmodelica.org@10645 7ae51c65-13d5-4921-9d80-6511ddd1419c

branches/dev-5547/RuntimeLibrary/src/jmi/jmi_block_log.c
@@ -29,149 +29,185 @@
 
 #define Ith(v,i)    NV_Ith_S(v,i)
 
-int jmi_check_and_log_illegal_residual_output(jmi_block_solver_t *block, double* f, double* ivs, double* residual_heuristic_nominal, int N) {
-    int i, ret=0;
-    jmi_log_t *log = block->log;
-    int nansPresent = FALSE;
-    int infsPresent = FALSE;
-    int limValsPresent = FALSE;
-
-    for (i=0;i<N;i++) {
-        double v = f[i];
-        /* Recoverable error*/
-        if (v != v) { /* NaN */
-            ret = 1;
-            nansPresent = TRUE;
-            block->residual_error_indicator[i] = 1;
-        } else if(v - v != 0) { /* Inf */
-            ret = 1;
-            infsPresent = TRUE;
-            block->residual_error_indicator[i] = 2;
-        } else if (v >  JMI_LIMIT_VALUE * residual_heuristic_nominal[i] ||
-            v < -JMI_LIMIT_VALUE * residual_heuristic_nominal[i]) {
-                ret = 1;
-                limValsPresent = TRUE;
-                block->residual_error_indicator[i] = 3;
-        } else {
-            block->residual_error_indicator[i] = 0;
-        }
-
-    }
-
-    if(ret && N==1) {
-        if (nansPresent)
-            jmi_log_node(log, logWarning, "IllegalResidualOutput", "Not a number as output from <block: %s> with <input: %g>", block->label, ivs[0]);
-        if (infsPresent)
-            jmi_log_node(log, logWarning, "IllegalResidualOutput", "INF as output from <block: %s> with <input: %g>", block->label, ivs[0]);
-        if( limValsPresent)
-            jmi_log_node(log, logWarning, "IllegalResidualOutput", "Absolute value of <output: %g> too big in <block: %s> with <input: %g>", f[i], block->label, ivs[0]);
-    } else if (ret) {
-        jmi_log_node_t outer;
-        jmi_log_node_t inner;
-        outer = jmi_log_enter_fmt(log, logWarning, "IllegalResidualOutput", "The residual output is illegal in <block: %s>", block->label);    
-        if (nansPresent) {
-            inner = jmi_log_enter_index_vector_(log, outer, logWarning, "NaNResidualIndices", 'R');
-            for(i=0; i<N; i++) {
-                if(block->residual_error_indicator[i] == 1) 
-                    jmi_log_int_(log, i);
-            }
-            jmi_log_leave(log, inner);
-        }
-        if (infsPresent) {
-            inner = jmi_log_enter_index_vector_(log, outer, logWarning, "INFResidualIndices", 'R');
-            for(i=0; i<N; i++) {
-                if(block->residual_error_indicator[i] == 2) 
-                    jmi_log_int_(log, i);
-            }
-            jmi_log_leave(log, inner);
-        }
-        if( limValsPresent) {
-            inner = jmi_log_enter_index_vector_(log, outer, logWarning, "LimitingValueIndices", 'R');
-            for(i=0; i<N; i++) {
-                if(block->residual_error_indicator[i] == 3) 
-                    jmi_log_int_(log, i);
-            }
-            jmi_log_leave(log, inner);
-        }
-        if(block->callbacks->log_options.log_level >= 3) {
-            jmi_log_reals(log, outer, logWarning, "ivs", ivs, block->n);
-            jmi_log_reals(log, outer, logWarning, "residuals", f, block->n);                   
-        }
-        jmi_log_leave(log, outer);
-    }
-    return ret;
+int jmi_check_illegal_values(int *error_indicator, jmi_real_t *nominal, jmi_real_t *inputs, int n, int* nans_present, int *infs_present, int *lim_vals_present) {
+	int i, ret = 0;
+	nans_present[0] = FALSE;
+	infs_present[0] = FALSE;
+	lim_vals_present[0] = FALSE;
+	for (i=0;i<n;i++) {
+		double v = inputs[i];
+		/* Recoverable error*/
+		if (v != v) { /* NaN */
+			ret = -1;
+			nans_present[0] = TRUE;
+			error_indicator[i] = 1;
+		} else if(v - v != 0) { /* Inf */
+			ret = -1;
+			infs_present[0] = TRUE;
+			error_indicator[i] = 2;
+		} else if (v >  JMI_LIMIT_VALUE * nominal[i] ||
+			v < -JMI_LIMIT_VALUE * nominal[i]) {
+				ret = -1;
+				lim_vals_present[0] = TRUE;
+				error_indicator[i] = 3;
+		} else {
+			error_indicator[i] = 0;
+		}
+	}
 }
 
-int jmi_check_and_log_illegal_iv_input(jmi_block_solver_t *block, double* ivs, int N) {
-    int i, ret = 0;
-    jmi_log_t *log = block->log;
-    int nansPresent = FALSE;
-    int infsPresent = FALSE;
-    int limValsPresent = FALSE;
+void jmi_log_illegal_input(jmi_log_t *log, int *error_indicator, int n, int nans_present, int infs_present, int lim_vals_present, jmi_real_t *inputs,
+	jmi_string_t label, int is_iter_var_flag, int* value_references, int log_level, const char* label_type) {
+	int i;
+	int ret = (nans_present+infs_present+lim_vals_present) > 0 ? TRUE: FALSE;
+	char* warn_input_type;
+	if (is_iter_var_flag) {
+		warn_input_type = "IllegalIterationVariableInput";
+	} else {
+		warn_input_type = "IllegalVariableInput";
+	}
 
-    for (i=0;i<N;i++) {
-        double v = ivs[i];
-        /* Recoverable error*/
-        if (v != v) { /* NaN */
-            ret = -1;
-            nansPresent = TRUE;
-            block->residual_error_indicator[i] = 1;
-        } else if(v - v != 0) { /* Inf */
-            ret = -1;
-            infsPresent = TRUE;
-            block->residual_error_indicator[i] = 2;
-        } else if (v >  JMI_LIMIT_VALUE * block->nominal[i] ||
-            v < -JMI_LIMIT_VALUE * block->nominal[i]) {
-                ret = -1;
-                limValsPresent = TRUE;
-                block->residual_error_indicator[i] = 3;
-        } else {
-            block->residual_error_indicator[i] = 0;
-        }
-
-    }
-
-    if(ret && N==1) {
-        if (nansPresent)
-            jmi_log_node(log, logWarning, "IllegalIterationVariableInput", "Not a number as input to <block: %s>", block->label);
-        if (infsPresent)
-            jmi_log_node(log, logWarning, "IllegalIterationVariableInput", "INF as input to <block: %s>", block->label);
-        if( limValsPresent)
-            jmi_log_node(log, logWarning, "IllegalIterationVariableInput", "Absolute value of input too big in <block: %s>", block->label);
+	if(ret && n==1) {
+        if (nans_present)
+            jmi_log_node(log, logWarning, warn_input_type, "Not a number as input to <%s: %s>", label_type, label);
+        if (infs_present)
+            jmi_log_node(log, logWarning, warn_input_type, "INF as input to <%s: %s>", label_type, label);
+        if( lim_vals_present)
+            jmi_log_node(log, logWarning, warn_input_type, "Absolute value of input too big in <%s: %s>", label_type, label);
     } else if (ret) {
         jmi_log_node_t outer;
         jmi_log_node_t inner;
-        outer = jmi_log_enter_fmt(log, logWarning, "IllegalIterationVariableInput", "The iteration variable input is illegal in <block: %s>", block->label);
+        outer = jmi_log_enter_fmt(log, logWarning, warn_input_type, "The iteration variable input is illegal in <%s: %s>", label_type, label);
 
-        if (nansPresent) {
-            inner = jmi_log_enter_vector_(log, outer, logWarning, "NaNIterationVariableIndices");
-            for(i=0; i<N; i++) {
-                if(block->residual_error_indicator[i] == 1) 
-                    jmi_log_vref_(log, 'r', block->value_references[i]);
+        if (nans_present) {
+            inner = jmi_log_enter_vector_(log, outer, logWarning, is_iter_var_flag? "NaNIterationVariableIndices": "NaNVariableIndices");
+            for(i=0; i<n; i++) {
+                if(error_indicator[i] == 1) {
+					if (is_iter_var_flag)
+						jmi_log_vref_(log, 'r', value_references[i]);
+					else
+						jmi_log_int_(log, i);
+				}
             }
             jmi_log_leave(log, inner);
         }
-        if (infsPresent) {
-            inner = jmi_log_enter_vector_(log, outer, logWarning, "INFIterationVariableIndices");
-            for(i=0; i<N; i++) {
-                if(block->residual_error_indicator[i] == 2) 
-                    jmi_log_vref_(log, 'r', block->value_references[i]);
+        if (infs_present) {
+            inner = jmi_log_enter_vector_(log, outer, logWarning,  is_iter_var_flag? "INFIterationVariableIndices": "INFVariableIndices");
+            for(i=0; i<n; i++) {
+                if(error_indicator[i] == 2) {
+					if (is_iter_var_flag)
+						jmi_log_vref_(log, 'r', value_references[i]);
+					else
+						jmi_log_int_(log, i);
+				}
             }
             jmi_log_leave(log, inner);
         }
-        if( limValsPresent) {
+        if( lim_vals_present) {
             inner = jmi_log_enter_vector_(log, outer, logWarning, "LimitingValueIndices");
-            for(i=0; i<N; i++) {
-                if(block->residual_error_indicator[i] == 3) 
-                    jmi_log_vref_(log, 'r', block->value_references[i]);
+            for(i=0; i<n; i++) {
+                if(error_indicator[i] == 3) {
+					if (is_iter_var_flag)
+						jmi_log_vref_(log, 'r', value_references[i]);
+					else
+						jmi_log_int_(log, i);
+				}
             }
             jmi_log_leave(log, inner);
         }
-        if(block->callbacks->log_options.log_level >= 3) {
-            jmi_log_reals(log, outer, logWarning, "ivs", ivs, block->n);
+        if(log_level >= 3) {
+            jmi_log_reals(log, outer, logWarning, is_iter_var_flag?"ivs":"inputs", inputs, n);
                
         }
         jmi_log_leave(log, outer);
     }
+}
+
+void jmi_log_illegal_output(jmi_log_t *log, int *error_indicator, int n_outputs, int n_inputs, jmi_real_t *inputs, jmi_real_t *outputs, int nans_present, int infs_present, int lim_vals_present, 
+	jmi_string_t label, int is_iter_var_flag, int log_level, const char* label_type) {
+		int i;
+		int ret = (nans_present+infs_present+lim_vals_present) > 0 ? TRUE: FALSE;
+		char* warn_output_type;
+		if (is_iter_var_flag) {
+			warn_output_type = "IllegalIterationVariableInput";
+		} else {
+			warn_output_type = "IllegalVariableInput";
+		}
+
+		if(ret && n_outputs==1 && n_inputs==1) {
+			if (nans_present)
+				jmi_log_node(log, logWarning, warn_output_type, "Not a number as output from <%s: %s> with <input: %g>", label_type, label, inputs[0]);
+			if (infs_present)
+				jmi_log_node(log, logWarning, warn_output_type, "INF as output from <%s: %s> with <input: %g>", label_type, label, inputs[0]);
+			if( lim_vals_present)
+				jmi_log_node(log, logWarning, warn_output_type, "Absolute value of <output: %g> too big in <%s: %s> with <input: %g>", label_type, label, inputs[0]);
+		} else if (ret) {
+			jmi_log_node_t outer;
+			jmi_log_node_t inner;
+			outer = jmi_log_enter_fmt(log, logWarning, warn_output_type, "The output is illegal in <%s: %s>", label_type, label);
+
+			if (nans_present) {
+				inner = jmi_log_enter_vector_(log, outer, logWarning, is_iter_var_flag? "NaNResidualIndices": "NaNOutputIndices");
+				for(i=0; i<n_outputs; i++) {
+					if(error_indicator[i] == 1) {
+							jmi_log_int_(log, i);
+					}
+				}
+				jmi_log_leave(log, inner);
+			}
+			if (infs_present) {
+				inner = jmi_log_enter_vector_(log, outer, logWarning,  is_iter_var_flag? "INFResidualIndices": "INFOutputIndices");
+				for(i=0; i<n_outputs; i++) {
+					if(error_indicator[i] == 2) {
+							jmi_log_int_(log, i);
+					}
+				}
+				jmi_log_leave(log, inner);
+			}
+			if( lim_vals_present) {
+				inner = jmi_log_enter_vector_(log, outer, logWarning, "LimitingValueIndices");
+				for(i=0; i<n_outputs; i++) {
+					if(error_indicator[i] == 3) {
+							jmi_log_int_(log, i);
+					}
+				}
+				jmi_log_leave(log, inner);
+			}
+			if(log_level >= 3) {
+				jmi_log_reals(log, outer, logWarning, is_iter_var_flag?"ivs":"inputs", inputs, n_inputs);
+				jmi_log_reals(log, outer, logWarning,  is_iter_var_flag?"residuals":"outputs", outputs, n_outputs); 
+
+			}
+			jmi_log_leave(log, outer);
+		}
+}
+
+int jmi_check_and_log_illegal_residual_output(jmi_block_solver_t *block, double* f, double* ivs, double* residual_heuristic_nominal, int N) {
+    int i, ret=0;
+    jmi_log_t *log = block->log;
+    int nans_present = FALSE;
+    int infs_present = FALSE;
+    int lim_vals_present = FALSE;
+
+	ret = jmi_check_illegal_values(block->residual_error_indicator, block->residual_heuristic_nominal, f, N, &nans_present, &infs_present, &lim_vals_present);
+	jmi_log_illegal_output(log, block->residual_error_indicator, N, N, nans_present, infs_present, lim_vals_present, ivs, f, block->label, TRUE, block->callbacks->log_options.log_level, "block");
+
+    return ret;
+}
+
+
+
+int jmi_check_and_log_illegal_iv_input(jmi_block_solver_t *block, double* ivs, int N) {
+    int i, ret = 0;
+    jmi_log_t *log = block->log;
+    int nans_present = FALSE;
+    int infs_present = FALSE;
+    int lim_vals_present = FALSE;
+
+	ret = jmi_check_illegal_values(block->residual_error_indicator, block->nominal, ivs, N, &nans_present, &infs_present, &lim_vals_present);
+
+	jmi_log_illegal_input(log, block->residual_error_indicator, N, nans_present, infs_present, 
+		lim_vals_present, ivs, block->label, TRUE, block->value_references, block->callbacks->log_options.log_level, "block");
+
     return ret;
 }

branches/dev-5547/RuntimeLibrary/src/jmi/CMakeLists.txt
@@ -92,6 +92,7 @@ set(JMISources
     jmi_delay_impl.h
     jmi_dynamic_state.h
     jmi_chattering.h
+    jmi_work_array.h
     jmi_math.h
     jmi_types.h
     
@@ -108,6 +109,7 @@ set(JMISources
     jmi_delay.c
     jmi_dynamic_state.c
     jmi_chattering.c
+    jmi_work_array.c
     jmi_math.c
     
     ${JMICommonSources}

branches/dev-5547/RuntimeLibrary/src/jmi/jmi.c
@@ -246,7 +246,8 @@ int jmi_init(jmi_t** jmi,
     /* Work arrays */
     jmi_->real_x_work = (jmi_real_t*)calloc(jmi_->n_real_x,sizeof(jmi_real_t));
     jmi_->real_u_work = (jmi_real_t*)calloc(jmi_->n_real_u,sizeof(jmi_real_t));
-    
+	jmi_->int_work = jmi_create_int_work_array(10); /* TODO: set relevant size here */
+	jmi_->real_work = jmi_create_real_work_array(30); /* TODO: set relevant size here */
 
     jmi_->n_initial_relations = n_initial_relations;
     jmi_->n_relations = n_relations;
@@ -335,6 +336,9 @@ int jmi_delete(jmi_t* jmi){
     free(jmi->dz);
     free(jmi->real_x_work);
     free(jmi->real_u_work);
+	jmi_delete_real_work_array(jmi->real_work);
+	jmi_delete_int_work_array(jmi->int_work);
+
     free(jmi->initial_relations);
     free(jmi->relations);
     free(jmi->nominals);

branches/dev-5547/RuntimeLibrary/src/jmi/jmi.h
@@ -29,6 +29,7 @@
 #include "jmi_global.h"
 #include "jmi_block_solver.h"
 #include "jmi_delay.h"
+#include "jmi_work_array.h"
 
 
 /* @{ */
@@ -259,6 +260,7 @@ typedef struct jmi_z {
     jmi_z_strings_t strings;
 } jmi_z_t;
 
+
 /**
  * \brief The main struct of the JMI Model interface containing
  * dimension information and model function pointers in jmi_model_t.
@@ -447,6 +449,8 @@ struct jmi_t {
 
     jmi_real_t* real_x_work;             /**< \brief Work array for the real x variables */
     jmi_real_t* real_u_work;             /**< \brief Work array for the real u variables */
+	jmi_real_work_array_t* real_work;		 /**< \brief Work array for real variables */
+	jmi_int_work_array_t* int_work;		 /**< \brief Work array for int variables */
     
     jmp_buf try_location[JMI_MAX_EXCEPTION_DEPTH+1];                /**< \brief Buffer for setjmp/longjmp, for exception handling. */
     jmi_int_t current_try_depth;

branches/dev-5547/RuntimeLibrary/src/jmi/jmi_block_log.h
@@ -27,6 +27,17 @@
 
 #include "jmi_block_solver.h"
 
+/** \brief Check illegal inputs or outputs */
+int jmi_check_illegal_values(int *error_indicator, jmi_real_t *nominal, jmi_real_t *inputs, int n, int* nans_present, int *infs_present, int *lim_vals_present);
+
+/** \brief Log illegal input values detected by checl_illegal_values */
+void jmi_log_illegal_input(jmi_log_t *log, int *error_indicator, int n, int nans_present, int infs_present, int lim_vals_present, jmi_real_t *inputs,
+	jmi_string_t label, int is_iter_var_flag, int* value_references, int log_level, const char* label_type);
+
+/** \brief Log illegal otput values detected by checl_illegal_values */
+void jmi_log_illegal_output(jmi_log_t *log, int *error_indicator, int n_outputs, int n_inputs, jmi_real_t *inputs, jmi_real_t *outputs, int nans_present, int infs_present, int lim_vals_present, 
+	jmi_string_t label, int is_iter_var_flag, int log_level, const char* label_type);
+
 /** \brief Check and log illegal iv inputs */
 int jmi_check_and_log_illegal_iv_input(jmi_block_solver_t* block, double* ivs, int N);
 

branches/dev-5547/RuntimeLibrary/src/jmi/jmi_util.c
@@ -24,6 +24,8 @@
 #include <stdio.h>
 #include <assert.h>
 #include "jmi_math.h"
+#include "jmi_block_log.h"
+#include "jmi_work_array.h"
 
 void jmi_min_time_event(jmi_time_event_t* event, int def, int phase, jmi_real_t time) {
     if (JMI_TRUE == LOG_EXP_OR(
@@ -528,6 +530,7 @@ void jmi_free_directional_derivative_callbacks(jmi_directional_derivative_callba
 
 int jmi_evaluate_directional_derivative(jmi_t* jmi, jmi_directional_derivative_callbacks_t* dd_callback, void* args) {
 	int i,j;
+	int nans_present, infs_present, lim_vals_present;
 	int ef = 0;
 	int n_input = dd_callback->n_input;
 	int n_output = dd_callback->n_output;
@@ -536,69 +539,100 @@ int jmi_evaluate_directional_derivative(jmi_t* jmi, jmi_directional_derivative_c
 	jmi_real_t* output = dd_callback->output;
 	jmi_real_t* d_output = dd_callback->d_output;
 	jmi_real_t input_temp, inc, delta, sign;
-	jmi_real_t* input_max = (jmi_real_t*)calloc(n_input, sizeof(jmi_real_t));
-	jmi_real_t* input_min = (jmi_real_t*)calloc(n_input, sizeof(jmi_real_t));
-	jmi_real_t* input_nominal = (jmi_real_t*)calloc(n_input, sizeof(jmi_real_t));
-	jmi_real_t* output_temp = (jmi_real_t*)calloc(n_output, sizeof(jmi_real_t));
+	jmi_real_t* work_array = jmi_get_real_work_array(jmi->real_work, n_input*3+2*n_output);
+	jmi_real_t* input_max = work_array;
+	jmi_real_t* input_min = work_array + n_input;
+	jmi_real_t* input_nominal = work_array + 2*n_input;
+	jmi_real_t* output_nominal = work_array + 3*n_input;
+	jmi_real_t* output_temp = work_array + 3*n_input+n_output;
+	jmi_int_t* error_indicator = jmi_get_int_work_array(jmi->int_work, JMI_MAX(n_output, n_input));
 
 	/* Setup max/min/nominal values for the inputs */
 	ef = dd_callback->F_max(jmi, input_max);
+	if (ef !=0) {
+		jmi_log_node(jmi->log, logError, "DirectionalDerivative", "Could not retrieve max values for inputs in <function_finite_dir_der: %s>.", dd_callback->label);
+		return ef;
+	} 
 	ef = dd_callback->F_min(jmi, input_min);
+	if (ef !=0) {
+		jmi_log_node(jmi->log, logError, "DirectionalDerivative", "Could not retrieve min values for inputs in <function_finite_dir_der: %s>.", dd_callback->label);
+		return ef;
+	} 
 	ef = dd_callback->F_nominal(jmi, input_nominal);
+	if (ef !=0) {
+		jmi_log_node(jmi->log, logError, "DirectionalDerivative", "Could not retrieve nominal values for inputs in <function_finite_dir_der: %s>.", dd_callback->label);
+		return ef;
+	} 
 
+	/* Check if input values are ok */
+	ef = jmi_check_illegal_values(error_indicator, input_nominal, input, n_input, &nans_present, &infs_present, &lim_vals_present);
+	jmi_log_illegal_input(jmi->log, error_indicator, n_input, nans_present, infs_present, lim_vals_present, input, dd_callback->label, 
+		FALSE, NULL, jmi->jmi_callbacks.log_options.log_level, "function_finite_dir_der");
+	if (ef !=0) {
+		jmi_log_node(jmi->log, logError, "DirectionalDerivative", "Cannot evaluate directional derivatives with illegal input in <function_finite_dir_der: %s>.", dd_callback->label);
+		return ef;
+	} 
 
 	/* get current output values */
 	ef = dd_callback->F(args, input, output);
 	if (ef != 0 ) {
-		jmi_log_node(jmi->log, logError, "DirectionalDerivative", "Could not evaluate callback function for inputs given.");
-	} else {
-		/* Make d_output be filled with zeroes from start */
-		for (i = 0; i < n_output; i++) {
-			d_output[i] = 0.0;
+		jmi_log_node(jmi->log, logError, "DirectionalDerivative", "Could not evaluate callback function for inputs given in <function_finite_dir_der: %s>", dd_callback->label);
+		return ef;
+	}
+
+	/* Make d_output be filled with zeroes from start */
+	for (i = 0; i < n_output; i++) {
+		d_output[i] = 0.0;
+		output_nominal[i] = 1.0;
+	}
+
+	delta = sqrt(JMI_EPS);
+	for (j = 0; j < n_input; j++) {
+		jmi_real_t input_orig = input[j];
+		/* No need to evaluate derivative in this direction since it will be multiplied by 0 anyway */
+		if (d_input[j] == 0) {
+			continue;
+		}
+		input_temp = input_orig;
+		sign = (input_orig >= 0) ? 1 : -1;
+		inc = JMI_MAX(JMI_ABS(input_orig), input_nominal[j])*sign*delta;
+		input[j] += inc;
+
+		/* Make sure we're inside the bounds */
+		if( input[j] > input_max[j] || input[j] < input_min[j]) {
+			inc = -inc;
+			input[j] = input_orig + inc;
+		}
+		ef = dd_callback->F(args, input, output_temp);
+		/* If failure, try other direction */
+		if ( ef > 0 ) {
+			jmi_log_node(jmi->log, logWarning, "DirectionalDerivative", "Could not evaluate callback function in <function_finite_dir_der: %s>. Trying other direction. ", dd_callback->label);
+			input[j] = input_orig - inc;
+			ef = dd_callback->F(args, input, output_temp);
 		}
 
-		delta = sqrt(JMI_EPS);
-		for (j = 0; j < n_input; j++) {
-			jmi_real_t input_orig = input[j];
-			/* No need to evaluate derivative in this direction since it will be multiplied by 0 anyway */
-			if (d_input[j] == 0) {
-				continue;
-			}
-			input_temp = input_orig;
-			sign = (input_orig >= 0) ? 1 : -1;
-			inc = JMI_MAX(JMI_ABS(input_orig), input_nominal[j])*sign*delta;
-			input[j] += inc;
-		
-			/* Make sure we're inside the bounds */
-			if( input[j] > input_max[j] || input[j] < input_min[j]) {
-				inc = -inc;
-				input[j] = input_orig + inc;
-			}
-			ef = dd_callback->F(args, input, output_temp);
-			/* If failure, try other direction */
-			if ( ef > 0 ) {
-				jmi_log_node(jmi->log, logError, "DirectionalDerivative", "Could not evaluate callback function. Trying other direction. ");
-				input[j] = input_orig - inc;
-				ef = dd_callback->F(args, input, output_temp);
-			}
+		if (ef !=0) {
+			jmi_log_node(jmi->log, logError, "DirectionalDerivative", "Could not evaluate callback in <function_finite_dir_der: %s>.", dd_callback->label);
+			return ef;
+		}
 
-			/* Reset input vector */
-			input[j] = input_orig;
+		/* Check if input values are ok */
+		ef = jmi_check_illegal_values(error_indicator, input_nominal, output_temp, n_output, &nans_present, &infs_present, &lim_vals_present);
+		jmi_log_illegal_output(jmi->log, error_indicator, n_output, n_input, input, output, nans_present, infs_present, lim_vals_present, dd_callback->label, 
+			FALSE, jmi->jmi_callbacks.log_options.log_level, "function_finite_dir_der");
+		if (ef !=0) {
+			jmi_log_node(jmi->log, logError, "DirectionalDerivative", "Illegal output from <function_finite_dir_der: %s>.", dd_callback->label);
+			return ef;
+		} 
 
-			if (ef == 0) {
-				for (i = 0; i < n_output; i++) {
-					d_output[i] += (output[i]-output_temp[i])/inc*d_input[j]; 
-				}
-			} else {
-				jmi_log_node(jmi->log, logError, "DirectionalDerivative", "Could not evaluate callback function.");
-				break;
-			}
+
+		/* Reset input vector */
+		input[j] = input_orig;
+
+		for (i = 0; i < n_output; i++) {
+			d_output[i] += (output[i]-output_temp[i])/inc*d_input[j]; 
 		}
 	}
 
-	free(input_max);
-	free(input_min);
-	free(input_nominal);
-	free(output_temp);
 	return ef;
 }

branches/dev-5547/RuntimeLibrary/src/jmi/jmi_util.h
@@ -742,6 +742,7 @@ struct jmi_directional_derivative_callbacks_t {
 	jmi_directional_derivative_attributes_func_t F_min; /*  Callback for min values for input variables */
 	jmi_directional_derivative_attributes_func_t F_nominal; /* Callback for nominal values for input variables */
 	jmi_directional_derivative_base_func_t F;	
+	jmi_string_t label; /* Label for origin of directional derivative callback */
 };
 jmi_directional_derivative_callbacks_t* jmi_create_directional_derivative_callbacks(size_t n_input, size_t n_output);
 void jmi_free_directional_derivative_callbacks(jmi_directional_derivative_callbacks_t* dd);

branches/dev-5547/RuntimeLibrary/src/jmi/jmi_work_array.c
@@ -1,0 +1,69 @@
+ /*
+    Copyright (C) 2018 Modelon AB
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License version 3 as published
+    by the Free Software Foundation, or optionally, under the terms of the
+    Common Public License version 1.0 as published by IBM.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License, or the Common Public License, for more details.
+
+    You should have received copies of the GNU General Public License
+    and the Common Public License along with this program.  If not,
+    see <http://www.gnu.org/licenses/> or
+    <http://www.ibm.com/developerworks/library/os-cpl.html/> respectively.
+*/
+
+#include "jmi_work_array.h"
+
+jmi_real_work_array_t* jmi_create_real_work_array(jmi_int_t n) {
+	jmi_real_work_array_t* real_work_array;
+	real_work_array = calloc(1, sizeof(jmi_real_work_array_t));
+	real_work_array->n = n;
+	real_work_array->rwork = calloc(n, sizeof(jmi_real_t));
+}
+
+jmi_int_work_array_t* jmi_create_int_work_array(jmi_int_t n) {
+	jmi_int_work_array_t* int_work_array;
+	int_work_array = calloc(1, sizeof(jmi_int_work_array_t));
+	int_work_array->n = n;
+	int_work_array->iwork = calloc(n, sizeof(jmi_int_t));
+}
+
+jmi_real_t* jmi_get_real_work_array(jmi_real_work_array_t* real_work_array, jmi_int_t n) {
+	if (n > real_work_array->n) { /* Not enough memory allocated */
+		free(real_work_array->rwork);
+		real_work_array->rwork = calloc(n, sizeof(jmi_real_t));
+		real_work_array->n=n;
+	}
+	return real_work_array->rwork;
+}
+jmi_int_t* jmi_get_int_work_array(jmi_int_work_array_t* int_work_array, jmi_int_t n) {
+	if (n > int_work_array->n) { /* Not enough memory allocated */
+		free(int_work_array->iwork);
+		int_work_array->iwork = calloc(n, sizeof(jmi_int_t));
+		int_work_array->n=n;
+	}
+	return int_work_array->iwork;
+}
+
+
+void jmi_delete_real_work_array(jmi_real_work_array_t* real_work_array) {
+	if (real_work_array == NULL) {
+		return;
+	}
+	
+	free(real_work_array->rwork);
+	free(real_work_array);
+}
+void jmi_delete_int_work_array(jmi_int_work_array_t* int_work_array) {
+	if (int_work_array == NULL) {
+		return;
+	}
+	
+	free(int_work_array->iwork);
+	free(int_work_array);
+}

commit 0588da164bb972b68743f97f1cb7c1cecddd14aa

#5547 Fixed return flag problem introduced in r10645.

branches/dev-5547/RuntimeLibrary/src/jmi/jmi_block_log.c
@@ -202,7 +202,8 @@ int jmi_check_and_log_illegal_residual_output(jmi_block_solver_t *block, double*
     ret = jmi_check_illegal_values(block->residual_error_indicator, residual_heuristic_nominal, f, N, &nans_present, &infs_present, &lim_vals_present);
     jmi_log_illegal_output(log, block->residual_error_indicator, N, N, ivs, f, nans_present, infs_present, lim_vals_present, block->label, TRUE, block->callbacks->log_options.log_level, "block");
 
-    return ret;
+    /* Set illegal residual output as a recoverable error from Kinsol */
+    return -ret;
 }
 
 