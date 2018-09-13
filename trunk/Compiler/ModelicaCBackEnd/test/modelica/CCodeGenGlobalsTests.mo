jnorthrup/jmodelica

commit 29afbafc6d388456af918d4452525f2bb0ea0d26

#5670 Basic support for external object package constants. Not including in arrays or records.
git-svn-id: https://svn.jmodelica.org@11578 7ae51c65-13d5-4921-9d80-6511ddd1419c

trunk/Compiler/ModelicaMiddleEnd/src/jastadd/optimizations/InlineFunctions.jrag
@@ -1550,6 +1550,14 @@ aspect FunctionInlining {
         return exp;
     }
 
+    @Override
+    public FExp FGlobalAccessExp.replaceReferences(AbstractFunctionInliner fi) {
+        FExp res = dynamicFExp(treeCopy());
+        for (ASTNode n : res) {
+            n.replaceReferences(fi);
+        }
+        return res;
+    }
 
     public FIdUseExp FIdUseExp.copyWithNonLiteralSubscriptsAsColon() {
         return createNode(getCommonAccess().copyWithNonLiteralSubscriptsAsColon());

trunk/Compiler/ModelicaCBackEnd/test/modelica/CCodeGenGlobalsTests.mo
@@ -1980,7 +1980,7 @@ void func_CCodeGenGlobalsTests_GlobalVariables_GlobalConstantForPowInt1_f_def0(j
     y_v = x_v;
     i_0ie = 2 + 1 / 2.0;
     for (i_0i = 1; i_0i < i_0ie; i_0i += 1) {
-        y_v = jmi_pow_function(\"CCodeGenGlobalsTests.GlobalVariables.GlobalConstantForPowInt1.f\", y_v, jmi_array_val_1(JMI_GLOBAL(CCodeGenGlobalsTests_GlobalVariables_GlobalConstantForPowInt1_f_c), i_0i), \"y ^ CCodeGenGlobalsTests.GlobalVariables.GlobalConstantForPowInt1.f.c[i]\");
+        y_v = jmi_pow_function(\"CCodeGenGlobalsTests.GlobalVariables.GlobalConstantForPowInt1.f\", y_v, jmi_array_val_1(JMI_GLOBAL(CCodeGenGlobalsTests_GlobalVariables_GlobalConstantForPowInt1_f_c), i_0i), \"y ^ global(CCodeGenGlobalsTests.GlobalVariables.GlobalConstantForPowInt1.f.c[i])\");
     }
     JMI_RET(GEN, y_o, y_v)
     JMI_DYNAMIC_FREE()
@@ -2192,6 +2192,132 @@ jmi_real_t func_CCodeGenGlobalsTests_GlobalVariables_GlobalConstantRef2_f_exp0(j
 }
 ")})));
 end GlobalConstantRef2;
+
+model GlobalConstantExternalObject1
+    
+    package P
+        model EO
+            extends ExternalObject;
+            function constructor
+                input Real[:] x;
+                output EO eo;
+                external;
+            end constructor;
+            function destructor
+                input EO eo;
+                external;
+            end destructor;
+        end EO;
+        
+        function f
+            output Real y;
+            external "C" y = f(eo1);
+        end f;
+        
+        constant Real x = 1;
+        constant EO eo = EO({x});
+        constant EO eo1 = eo;
+    end P;
+    
+    function f
+        input Real x;
+        output Real y = x + P.f();
+    algorithm
+    end f;
+
+    Real y = f(time);
+
+annotation(__JModelica(UnitTesting(tests={
+    CCodeGenTestCase(
+        name="GlobalConstantExternalObject1",
+        description="",
+        template="
+$C_global_temps$
+$C_set_globals_start$
+$C_functions$
+$C_destruct_external_object$
+",
+        generatedCode="
+    jmi_extobj_t CCodeGenGlobalsTests_GlobalVariables_GlobalConstantExternalObject1_P_eo;
+    jmi_extobj_t CCodeGenGlobalsTests_GlobalVariables_GlobalConstantExternalObject1_P_eo1;
+
+jmi_extobj_t jmi_global_tmp_1(jmi_t* jmi) {
+    JMI_DYNAMIC_INIT()
+    jmi_extobj_t tmp_1;
+    JMI_ARR(STAT, jmi_real_t, jmi_array_t, tmp_2, 1, 1)
+    JMI_GLOBALS_INIT()
+    JMI_ARRAY_INIT_1(STAT, jmi_real_t, jmi_array_t, tmp_2, 1, 1, 1)
+    jmi_array_ref_1(tmp_2, 1) = 1.0;
+    tmp_1 = func_CCodeGenGlobalsTests_GlobalVariables_GlobalConstantExternalObject1_P_EO_constructor_exp2(tmp_2);
+    JMI_GLOBALS_FREE()
+    JMI_DYNAMIC_FREE()
+    return tmp_1;
+}
+
+int jmi_set_globals_start_0(jmi_t* jmi) {
+    int ef = 0;
+    JMI_DYNAMIC_INIT()
+    JMI_GLOBAL(CCodeGenGlobalsTests_GlobalVariables_GlobalConstantExternalObject1_P_eo) = jmi_global_tmp_1(jmi);
+    JMI_GLOBAL(CCodeGenGlobalsTests_GlobalVariables_GlobalConstantExternalObject1_P_eo1) = JMI_GLOBAL(CCodeGenGlobalsTests_GlobalVariables_GlobalConstantExternalObject1_P_eo);
+    JMI_DYNAMIC_FREE()
+    return ef;
+}
+
+int jmi_set_globals_start_0(jmi_t* jmi);
+
+int jmi_set_globals_start(jmi_t* jmi) {
+    int ef = 0;
+    JMI_DYNAMIC_INIT()
+    ef |= jmi_set_globals_start_0(jmi);
+    JMI_DYNAMIC_FREE()
+    return ef;
+}
+
+void func_CCodeGenGlobalsTests_GlobalVariables_GlobalConstantExternalObject1_P_f_def0(jmi_real_t* y_o) {
+    JMI_DYNAMIC_INIT()
+    JMI_DEF(REA, y_v)
+    extern double f(void*);
+    y_v = f(JMI_GLOBAL(CCodeGenGlobalsTests_GlobalVariables_GlobalConstantExternalObject1_P_eo1));
+    JMI_RET(GEN, y_o, y_v)
+    JMI_DYNAMIC_FREE()
+    return;
+}
+
+jmi_real_t func_CCodeGenGlobalsTests_GlobalVariables_GlobalConstantExternalObject1_P_f_exp0() {
+    JMI_DEF(REA, y_v)
+    func_CCodeGenGlobalsTests_GlobalVariables_GlobalConstantExternalObject1_P_f_def0(&y_v);
+    return y_v;
+}
+
+void func_CCodeGenGlobalsTests_GlobalVariables_GlobalConstantExternalObject1_P_EO_destructor_def1(jmi_extobj_t eo_v) {
+    JMI_DYNAMIC_INIT()
+    extern void destructor(void*);
+    destructor(eo_v);
+    JMI_DYNAMIC_FREE()
+    return;
+}
+
+void func_CCodeGenGlobalsTests_GlobalVariables_GlobalConstantExternalObject1_P_EO_constructor_def2(jmi_array_t* x_a, jmi_extobj_t* eo_o) {
+    JMI_DYNAMIC_INIT()
+    JMI_DEF(EXO, eo_v)
+    extern void* constructor(double*, size_t);
+    eo_v = constructor(x_a->var, jmi_array_size(x_a, 0));
+    JMI_RET(GEN, eo_o, eo_v)
+    JMI_DYNAMIC_FREE()
+    return;
+}
+
+jmi_extobj_t func_CCodeGenGlobalsTests_GlobalVariables_GlobalConstantExternalObject1_P_EO_constructor_exp2(jmi_array_t* x_a) {
+    JMI_DEF(EXO, eo_v)
+    func_CCodeGenGlobalsTests_GlobalVariables_GlobalConstantExternalObject1_P_EO_constructor_def2(x_a, &eo_v);
+    return eo_v;
+}
+
+
+    func_CCodeGenGlobalsTests_GlobalVariables_GlobalConstantExternalObject1_P_EO_destructor_def1(JMI_GLOBAL(CCodeGenGlobalsTests_GlobalVariables_GlobalConstantExternalObject1_P_eo));
+
+")})));
+end GlobalConstantExternalObject1;
 
 end GlobalVariables;
 

trunk/Compiler/ModelicaMiddleEnd/test/modelica/FunctionInlining.mo
@@ -5050,11 +5050,102 @@ model GlobalConst2
 fclass FunctionInlining.GlobalConst2
  parameter Integer temp_2;
  parameter Real y.x;
+package constant
+ constant FunctionInlining.GlobalConst2.R2 FunctionInlining.GlobalConst2.f.a = FunctionInlining.GlobalConst2.R2({FunctionInlining.GlobalConst2.R1(2), FunctionInlining.GlobalConst2.R1(3)});
 parameter equation
  temp_2 = integer(time);
- y.x = ({2, 3})[temp_2];
+ y.x = global(FunctionInlining.GlobalConst2.f.a.r1[temp_2].x);
+
+public
+ record FunctionInlining.GlobalConst2.R1
+  parameter Real x;
+ end FunctionInlining.GlobalConst2.R1;
+
+ record FunctionInlining.GlobalConst2.R2
+  parameter FunctionInlining.GlobalConst2.R1 r1[2];
+ end FunctionInlining.GlobalConst2.R2;
+
 end FunctionInlining.GlobalConst2;
 ")})));
 end GlobalConst2;
+
+model GlobalConstExtObj1
+    package P
+        model EO
+            extends ExternalObject;
+            function constructor
+                input Real[:] x;
+                output EO eo;
+                external;
+            end constructor;
+            function destructor
+                input EO eo;
+                external;
+            end destructor;
+        end EO;
+        
+        function f
+            input EO eo;
+            output Real y;
+            external;
+        end f;
+        
+        constant Real x = 1;
+        constant EO eo = EO({x});
+        constant EO eo1 = eo;
+    end P;
+    
+    function f
+        input Real x;
+        input P.EO eo;
+        output Real y = x + P.f(eo);
+    algorithm
+    end f;
+
+    Real y = f(time, P.eo1);
+
+
+    annotation(__JModelica(UnitTesting(tests={
+        TransformCanonicalTestCase(
+            name="GlobalConstExtObj1",
+            description="Inlining global constant",
+            variability_propagation=false,
+            flatModel="
+fclass FunctionInlining.GlobalConstExtObj1
+ Real y;
+package constant
+ constant FunctionInlining.GlobalConstExtObj1.P.EO FunctionInlining.GlobalConstExtObj1.P.eo = FunctionInlining.GlobalConstExtObj1.P.EO.constructor({1.0});
+ constant FunctionInlining.GlobalConstExtObj1.P.EO FunctionInlining.GlobalConstExtObj1.P.eo1 = global(FunctionInlining.GlobalConstExtObj1.P.eo);
+equation
+ y = time + FunctionInlining.GlobalConstExtObj1.P.f(global(FunctionInlining.GlobalConstExtObj1.P.eo1));
+
+public
+ function FunctionInlining.GlobalConstExtObj1.P.EO.destructor
+  input FunctionInlining.GlobalConstExtObj1.P.EO eo;
+ algorithm
+  external \"C\" destructor(eo);
+  return;
+ end FunctionInlining.GlobalConstExtObj1.P.EO.destructor;
+
+ function FunctionInlining.GlobalConstExtObj1.P.EO.constructor
+  input Real[:] x;
+  output FunctionInlining.GlobalConstExtObj1.P.EO eo;
+ algorithm
+  external \"C\" eo = constructor(x, size(x, 1));
+  return;
+ end FunctionInlining.GlobalConstExtObj1.P.EO.constructor;
+
+ function FunctionInlining.GlobalConstExtObj1.P.f
+  input FunctionInlining.GlobalConstExtObj1.P.EO eo;
+  output Real y;
+ algorithm
+  external \"C\" y = f(eo);
+  return;
+ end FunctionInlining.GlobalConstExtObj1.P.f;
+
+ type FunctionInlining.GlobalConstExtObj1.P.EO = ExternalObject;
+end FunctionInlining.GlobalConstExtObj1;
+")})));
+end GlobalConstExtObj1;
 
 end FunctionInlining;

trunk/Compiler/ModelicaCBackEnd/src/jastadd/CCodeGen/CCodeGenGlobals.jrag
@@ -282,7 +282,7 @@ aspect CCodeGenGlobalVariables {
                 
                 ArrayList<FGlobalVariable> globals = new ArrayList<>();
                 for (FGlobalVariable fv : fclass.getFGlobalVariables()) {
-                    fv.getBindingExp().genGlobalVariableInit_C(p, str, indent);
+                    fv.getBindingExp().genGlobalVariableInitFunc_C(p, str, indent, false);
                     globals.add(fv);
                 }
                 
@@ -294,7 +294,7 @@ aspect CCodeGenGlobalVariables {
                     }
                     @Override
                     public void gen(FGlobalVariable element) {
-                        element.genGlobalVariableInit_C(p, str, indent);
+                        element.genGlobalVariableInitCall_C(p, str, indent);
                     }
                 };
                 splitter.genFuncImpls();
@@ -312,73 +312,93 @@ aspect CCodeGenGlobalVariables {
         str.print(indent, type().type_C(p), " ", nameUnderscore(), ";\n");
     }
 
-    public void FGlobalVariable.genGlobalVariableInit_C(CodePrinter p, CodeStream str, String indent) {
-        getBindingExp().genGlobalVariableInitScalar_C(p, str, indent, FExp.global_C(nameUnderscore()), false);
+    public void FGlobalVariable.genGlobalVariableInitCall_C(CodePrinter p, CodeStream str, String indent) {
+        getBindingExp().genGlobalVariableInitCall_C(p, str, indent, FExp.global_C(nameUnderscore()), false);
     }
 
-    public void ASTNode.genGlobalVariableInit_C(CodePrinter p, CodeStream str, String indent) {
-        genGlobalVariableInitChildren_C(p, str, indent);
-    }
-    
-    public void ASTNode.genGlobalVariableInitChildren_C(CodePrinter p, CodeStream str, String indent) {
-        for (ASTNode n : this) {
-            n.genGlobalVariableInit_C(p, str, indent);
+    public void FExp.genGlobalVariableInitFunc_C(CodePrinter p, CodeStream str, String indent, boolean inArray) {
+        if (!type().isExternalObject()) {
+            for (FExp arg : childFExps()) {
+                arg.genGlobalVariableInitFunc_C(p, str, indent, isArray());
+            }
+            if (inArray || !isComposite()) {
+                return;
+            }
         }
-    }
-    
-    public void FRecordConstructor.genGlobalVariableInit_C(CodePrinter p, CodeStream str, String indent) {
-        super.genGlobalVariableInit_C(p, str, indent);
         
         String next = p.indent(indent);
-        str.print(indent, type().nameScalar_C(p), "* jmi_global_", tempName_C(), "(jmi_t* jmi) {\n");
+        String returnType = isArray() ? type().nameArray_C(p) : type().nameScalar_C(p);
+        if (!type().isExternalObject()) {
+            returnType = returnType + "*";
+        }
+        str.print(indent, returnType, " jmi_global_", tempName_C(), "(jmi_t* jmi) {\n");
         str.print(next, "JMI_DYNAMIC_INIT()\n");
         
-        String typeName = type().nameScalar_C(p);
-        str.print(next, typeName, "* ", tempName_C(), ";\n");
+        genGlobalVariableInitFuncDecl_C(p, str, next);
         
         str.print(next, "JMI_GLOBALS_INIT()\n");
         
-        str.print(next, tempName_C(), " = jmi_dynamic_function_pool_alloc(&dyn_mem, 1*sizeof(", typeName, "), TRUE);\n");
-        
-        genGlobalVariableInitScalar_C(p, str, next, tempName_C(), true);
+        genGlobalVariableInitFuncInit_C(p, str, next);
         
         str.print(next, "JMI_GLOBALS_FREE()\n");
         str.print(next, "JMI_DYNAMIC_FREE()\n");
         str.print(next, "return ", tempName_C(), ";\n");
         str.print(indent, "}\n\n");
     }
-    
-    public void FArray.genGlobalVariableInit_C(CodePrinter p, CodeStream str, String indent) {
-        for (Index i : indices()) {
-            fArrayCell(i, 0).genGlobalVariableInitChildren_C(p, str, indent);
-        }
+
+    public void FIdUseExp.genGlobalVariableInitFunc_C(CodePrinter p, CodeStream str, String indent, boolean inArray) {
         
-        String next = p.indent(indent);
-        str.print(indent, type().nameArray_C(p), "* jmi_global_", tempName_C(), "(jmi_t* jmi) {\n");
-        str.print(next, "JMI_DYNAMIC_INIT()\n");
-        
-        str.print(next, "JMI_ARR(",
+    }
+
+    public void FExp.genGlobalVariableInitFuncDecl_C(CodePrinter p, CodeStream str, String indent) {
+        str.print(indent, type().nameScalar_C(p), " ", tempName_C(), ";\n");
+        p.printVarDecls(this, str, indent);
+    }
+
+    public void FExp.genGlobalVariableInitFuncInit_C(CodePrinter p, CodeStream str, String indent) {
+        p.printPreSteps(this, str, indent);
+        str.print(indent, tempName_C(), " = ");
+        p.print(this, str, indent);
+        p.printPostSteps(this, str, indent);
+        str.print(";\n");
+    }
+
+    @Override
+    public void FRecordConstructor.genGlobalVariableInitFuncDecl_C(CodePrinter p, CodeStream str, String indent) {
+        str.print(indent, type().nameScalar_C(p), "* ", tempName_C(), ";\n");
+    }
+
+    @Override
+    public void FRecordConstructor.genGlobalVariableInitFuncInit_C(CodePrinter p, CodeStream str, String indent) {
+        str.print(indent, tempName_C(), " = jmi_dynamic_function_pool_alloc(&dyn_mem, 1*sizeof(", type().nameScalar_C(p), "), TRUE);\n");
+        genGlobalVariableInitCall_C(p, str, indent, tempName_C(), true);
+    }
+
+    @Override
+    public void FArray.genGlobalVariableInitFuncDecl_C(CodePrinter p, CodeStream str, String indent) {
+        str.print(indent, "JMI_ARR(",
                 "DYNA", ", ", 
                 type().nameScalar_C(p, false), ", ",
                 type().nameArray_C(p, false), ", ",
                 tempName_C(), ", ");
-        type().size().printNumElements_C(p, str, next, this);
+        type().size().printNumElements_C(p, str, indent, this);
         str.print(", ");
         str.print(type().ndims());
         str.print(")\n");
-        
-        str.print(next, "JMI_GLOBALS_INIT()\n");
-        
-        str.print(next, "JMI_ARRAY_INIT_", ndims(), "(",
+    }
+
+    @Override
+    public void FArray.genGlobalVariableInitFuncInit_C(CodePrinter p, CodeStream str, String indent) {
+        str.print(indent, "JMI_ARRAY_INIT_", ndims(), "(",
                 "DYNA", ", ", 
                 type().nameScalar_C(p, false), ", ",
                 type().nameArray_C(p, false), ", ",
                 tempName_C(), ", ");
-        type().size().printNumElements_C(p, str, next, this);
+        type().size().printNumElements_C(p, str, indent, this);
         str.print(", ");
         str.print(type().ndims());
         str.print(", ");
-        type().size().printDimensions_C(p, str, next, this);
+        type().size().printDimensions_C(p, str, indent, this);
         str.print(")\n");
         
         String name = tempName_C();
@@ -387,36 +407,54 @@ aspect CCodeGenGlobalVariables {
         for (Index i : indices()) {
             String cellName = pre + i.toUnclosedString() + ")";
             FExp cellExp = fArrayCell(i, 0);
-            cellExp.genGlobalVariableInitScalar_C(p, str, next, cellName, true);
+            cellExp.genGlobalVariableInitCall_C(p, str, indent, cellName, true);
         }
         
-        str.print(next, "JMI_GLOBALS_FREE()\n");
-        str.print(next, "JMI_DYNAMIC_FREE()\n");
-        str.print(next, "return ", tempName_C(), ";\n");
-        str.print(indent, "}\n\n");
     }
-    
-    public void FExp.genGlobalVariableInitScalar_C(CodePrinter p, CodeStream str, String indent, String name, boolean expand) {
-        if (isComposite()) {
-            str.print(indent, name, " = ");
-            str.print("jmi_global_", tempName_C(), "(jmi)");
-            str.print(";\n");
-        } else if (!isLiteralZero()) {
+
+    public void FExp.genGlobalVariableInitCall_C(CodePrinter p, CodeStream str, String indent, String name, boolean expand) {
+        if (!isLiteralZero()) {;
             str.print(indent, name, " = ");
             p.print(this, str, indent);
             str.print(";\n");
         }
     }
 
-    public void FRecordConstructor.genGlobalVariableInitScalar_C(CodePrinter p, CodeStream str, String indent, String name, boolean expand) {
+    public void FFunctionCall.genGlobalVariableInitCall_C(CodePrinter p, CodeStream str, String indent, String name, boolean expand) {
+        str.print(indent, name, " = jmi_global_", tempName_C(), "(jmi);\n");
+    }
+
+    public void FArray.genGlobalVariableInitCall_C(CodePrinter p, CodeStream str, String indent, String name, boolean expand) {
+        str.print(indent, name, " = jmi_global_", tempName_C(), "(jmi);\n");
+    }
+
+    public void FRecordConstructor.genGlobalVariableInitCall_C(CodePrinter p, CodeStream str, String indent, String name, boolean expand) {
         if (expand) {
             for (FRecordComponentType comp : getRecord().recordType().getComponents()) {
                 String compName = name + "->" + comp.getName();
                 FExp compExp = component(comp.getName());
-                compExp.genGlobalVariableInitScalar_C(p, str, indent, compName, false);
+                compExp.genGlobalVariableInitCall_C(p, str, indent, compName, false);
             }
         } else {
-            super.genGlobalVariableInitScalar_C(p, str, indent, name, expand);
+            str.print(indent, name, " = jmi_global_", tempName_C(), "(jmi);\n");
+        }
+    }
+    
+    @Override
+    public void FGlobalVariable.genDestructorCall_C(CodePrinter p, CodeStream str, String indent) {
+        getBindingExp().genDestructorCall_C(p, str, indent, this);
+    }
+    
+    public void FExp.genDestructorCall_C(CodePrinter p, CodeStream str, String indent, FGlobalVariable v) {
+    
+    }
+    
+    @Override
+    public void FFunctionCall.genDestructorCall_C(CodePrinter p, CodeStream str, String indent, FGlobalVariable v) {
+        if (isConstructorCall()) {
+            String strName = v.type().name() + ".destructor";
+            FCallable c = getName().lookupFCallable(strName);
+            str.print(indent, c.funcNameUnderscore(C_SUFFIX_DEF), "(", FExp.global_C(v.nameUnderscore()), ");\n");
         }
     }
 }

trunk/Compiler/ModelicaCBackEnd/src/jastadd/CCodeGen/CGenerator.jrag
@@ -1225,6 +1225,9 @@ public class CGenerator extends GenericGenerator {
             for (FVariable eo : fclass.externalObjectVariables()) {
                 eo.genDestructorCall_C(p, str, p.indent(""));
             }
+            for (FGlobalVariable eo : fclass.getFGlobalVariables()) {
+                eo.genDestructorCall_C(p, str, p.indent(""));
+            }
         }
     }
     

trunk/Compiler/ModelicaFlatTree/src/jastadd/ConstantEvaluation/ConstantEvaluation.jrag
@@ -1649,6 +1649,11 @@ aspect ConstantEvaluation {
         }
 
         @Override
+        public FExp buildInstLiteral() {
+            return new FEnumLitExp(type, index);
+        }
+
+        @Override
         public boolean hasBuildLiteral() {
             return true;
         }

trunk/Compiler/ModelicaFlatTree/src/jastadd/Constructors.jrag
@@ -972,6 +972,7 @@ aspect InheritedFactoryMethods {
 
     // Identifier expressions
     public FIdUseExp      FIdUseExp.createEmptyNode()      { return new FIdUseExp();      }
+    public FGlobalAccessExp FGlobalAccessExp.createEmptyNode() { return new FGlobalAccessExp(); }
     public InstDerExp     InstDerExp.createEmptyNode()     { return new InstDerExp();     }
     public FDerExp        FDerExp.createEmptyNode()        { return new FDerExp();        }
     public FDummyDerExp   FDummyDerExp.createEmptyNode()   { return new FDummyDerExp();   }

commit 70b9f4cc119650e1704c6cbf0bf20b4e141b5fcb

#5670 Fixed function inlining and code generation bug introduced in r11578

trunk/Compiler/ModelicaCBackEnd/src/jastadd/CCodeGen/CCodeGenExpressions.jrag
@@ -700,12 +700,27 @@ aspect CCodeGenExpressions {
     eq FAccess.toString_C(CodePrinter p) = toString_C(p.getExecStep());
 
     syn String FAccess.toString_C(CodePrinter.ExecuteCodePrinter p) {
-        String type = myFV().funcArrayType(isAssignUse());
-        if (myFV().inRecord() && inFunction()) {
-            return asFAccess().genRecordUse_C(p, asFAccess().myOutermostFV(), type);
-        } else {
-            return myFV().genUse_C(p, asFAccess().getFArraySubscripts(), type);
+        return myFV().genUse_C(p, asFAccess().getFArraySubscripts(), myFV().funcArrayType(isAssignUse()));
+    }
+
+    syn String FAccessFull.toString_C(CodePrinter.ExecuteCodePrinter p) {
+        if (getNumFAccessPart() < 2) {
+            return super.toString_C(p);
         }
+        
+        String type = myFV().funcArrayType(isAssignUse());
+        FAccessPart part = getFAccessPart(0);
+        FArraySubscripts fas = part.hasFArraySubscripts() ? part.getFArraySubscripts() : null;
+        String res = myOutermostFV().genUse_C(p, fas, C_ARRAY_RECORD);
+        for (int i = 1, n = getNumFAccessPart(); i < n; i++) {
+            part = getFAccessPart(i);
+            res = res + "->" + part.getName();
+            if (part.hasFArraySubscripts()) {
+                String curType = (n - i > 1 ? C_ARRAY_RECORD : type);
+                res = part.getFArraySubscripts().genFunctionArrayUse_C(p.codePrinter(), res, curType);
+            }
+        }
+        return res;
     }
 
     /**
@@ -733,32 +748,6 @@ aspect CCodeGenExpressions {
 
     public void FArraySubscripts.prettyPrint_C(CodePrinter p, CodeStream str, String indent) {
         getFSubscripts().prettyPrintWithSep(p, str, "", ", ");
-    }
-
-    /**
-     * Helper, generates an record variable use
-     */
-    public String FAccess.genRecordUse_C(CodePrinter.ExecuteCodePrinter p, FAbstractVariable fv, String type) {
-        throw new UnsupportedOperationException();
-    }
-
-    /**
-     * Helper, generates an record variable use
-     */
-    @Override
-    public String FAccessFull.genRecordUse_C(CodePrinter.ExecuteCodePrinter p, FAbstractVariable fv, String type) {
-        FAccessPart part = getFAccessPart(0);
-        FArraySubscripts fas = part.hasFArraySubscripts() ? part.getFArraySubscripts() : null;
-        String res = fv.genUse_C(p, fas, C_ARRAY_RECORD);
-        for (int i = 1, n = getNumFAccessPart(); i < n; i++) {
-            part = getFAccessPart(i);
-            res = res + "->" + part.getName();
-            if (part.hasFArraySubscripts()) {
-                String curType = (n - i > 1 ? C_ARRAY_RECORD : type);
-                res = part.getFArraySubscripts().genFunctionArrayUse_C(p.codePrinter(), res, curType);
-            }
-        }
-        return res;
     }
 
     /*=========================================================================

trunk/Compiler/ModelicaMiddleEnd/src/jastadd/optimizations/InlineFunctions.jrag
@@ -1552,11 +1552,8 @@ aspect FunctionInlining {
 
     @Override
     public FExp FGlobalAccessExp.replaceReferences(AbstractFunctionInliner fi) {
-        FExp res = dynamicFExp(treeCopy());
-        for (ASTNode n : res) {
-            n.replaceReferences(fi);
-        }
-        return res;
+        replaceReferencesInSubscripts(fi);
+        return this;
     }
 
     public FIdUseExp FIdUseExp.copyWithNonLiteralSubscriptsAsColon() {

trunk/Compiler/ModelicaCBackEnd/test/modelica/CCodeGenGlobalsTests.mo
@@ -2193,6 +2193,82 @@ jmi_real_t func_CCodeGenGlobalsTests_GlobalVariables_GlobalConstantRef2_f_exp0(j
 ")})));
 end GlobalConstantRef2;
 
+model GlobalConstantRef3
+    
+    constant R1[1] r = {R1(2)};
+    record R1
+        parameter Real x;
+    end R1;
+    function f
+        input Integer i;
+        output Real y = 1 + r[i].x;
+    algorithm
+    end f;
+    
+    Real y = f(integer(time));
+
+annotation(__JModelica(UnitTesting(tests={
+    CCodeGenTestCase(
+        name="GlobalConstantRef3",
+        description="",
+        template="
+$C_global_temps$
+$C_set_globals_start$
+$C_functions$
+$C_ode_derivatives$
+",
+        generatedCode="
+    R1_0_ra* CCodeGenGlobalsTests_GlobalVariables_GlobalConstantRef3_r;
+
+R1_0_ra* jmi_global_tmp_1(jmi_t* jmi) {
+    JMI_DYNAMIC_INIT()
+    JMI_ARR(DYNA, R1_0_r, R1_0_ra, tmp_1, 1, 1)
+    JMI_GLOBALS_INIT()
+    JMI_ARRAY_INIT_1(DYNA, R1_0_r, R1_0_ra, tmp_1, 1, 1, 1)
+    jmi_array_rec_1(tmp_1, 1)->x = AD_WRAP_LITERAL(2);
+    JMI_GLOBALS_FREE()
+    JMI_DYNAMIC_FREE()
+    return tmp_1;
+}
+
+int jmi_set_globals_start_0(jmi_t* jmi) {
+    int ef = 0;
+    JMI_DYNAMIC_INIT()
+    JMI_GLOBAL(CCodeGenGlobalsTests_GlobalVariables_GlobalConstantRef3_r) = jmi_global_tmp_1(jmi);
+    JMI_DYNAMIC_FREE()
+    return ef;
+}
+
+int jmi_set_globals_start_0(jmi_t* jmi);
+
+int jmi_set_globals_start(jmi_t* jmi) {
+    int ef = 0;
+    JMI_DYNAMIC_INIT()
+    ef |= jmi_set_globals_start_0(jmi);
+    JMI_DYNAMIC_FREE()
+    return ef;
+}
+
+
+
+int model_ode_derivatives_base(jmi_t* jmi) {
+    int ef = 0;
+    JMI_DYNAMIC_INIT()
+    if (jmi->atInitial || jmi->atEvent) {
+        _sw(0) = jmi_turn_switch_time(jmi, _time - (pre_temp_1_2), _sw(0), JMI_REL_LT);
+    }
+    if (jmi->atInitial || jmi->atEvent) {
+        _sw(1) = jmi_turn_switch_time(jmi, _time - (pre_temp_1_2 + AD_WRAP_LITERAL(1)), _sw(1), JMI_REL_GEQ);
+    }
+    _temp_1_2 = COND_EXP_EQ(LOG_EXP_OR(LOG_EXP_OR(_sw(0), _sw(1)), _atInitial), JMI_TRUE, floor(_time), pre_temp_1_2);
+    pre_temp_1_2 = _temp_1_2;
+    _y_1 = 1 + jmi_array_rec_1(JMI_GLOBAL(CCodeGenGlobalsTests_GlobalVariables_GlobalConstantRef3_r), _temp_1_2)->x;
+    JMI_DYNAMIC_FREE()
+    return ef;
+}
+")})));
+end GlobalConstantRef3;
+
 model GlobalConstantExternalObject1
     
     package P

trunk/Compiler/ModelicaMiddleEnd/test/modelica/FunctionInlining.mo
@@ -5069,6 +5069,46 @@ end FunctionInlining.GlobalConst2;
 ")})));
 end GlobalConst2;
 
+model GlobalConst3
+    constant R1[1] r = {R1(2)};
+    record R1
+        parameter Real x;
+    end R1;
+    function f
+        input Integer i;
+        output Real y = 1 + r[i].x;
+    algorithm
+    end f;
+    
+    Real y = f(integer(time));
+
+    annotation(__JModelica(UnitTesting(tests={
+        TransformCanonicalTestCase(
+            name="GlobalConst3",
+            description="Inlining global constant",
+            variability_propagation=false,
+            flatModel="
+fclass FunctionInlining.GlobalConst3
+ constant Real r[1].x = 2;
+ Real y;
+ discrete Integer temp_1;
+package constant
+ constant FunctionInlining.GlobalConst3.R1 FunctionInlining.GlobalConst3.r[1] = {FunctionInlining.GlobalConst3.R1(2)};
+initial equation
+ pre(temp_1) = 0;
+equation
+ y = 1 + global(FunctionInlining.GlobalConst3.r[temp_1].x);
+ temp_1 = if time < pre(temp_1) or time >= pre(temp_1) + 1 or initial() then integer(time) else pre(temp_1);
+
+public
+ record FunctionInlining.GlobalConst3.R1
+  parameter Real x;
+ end FunctionInlining.GlobalConst3.R1;
+
+end FunctionInlining.GlobalConst3;
+")})));
+end GlobalConst3;
+
 model GlobalConstExtObj1
     package P
         model EO