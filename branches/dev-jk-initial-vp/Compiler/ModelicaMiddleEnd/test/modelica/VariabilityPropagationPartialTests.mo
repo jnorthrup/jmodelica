jnorthrup/jmodelica

commit 8b478e827a29cd617c29401f62b06b4d28418d1a

#5515 Fixed infinite loop bug in initial variability propagation.
git-svn-id: https://svn.jmodelica.org@12038 7ae51c65-13d5-4921-9d80-6511ddd1419c

branches/dev-jk-initial-vp/Compiler/ModelicaMiddleEnd/src/jastadd/optimizations/VariabilityPropagation.jrag
@@ -493,10 +493,7 @@ aspect VariabilityPropagation {
             variability = variability.combine(Variability.FIXEDPARAMETER);
         }
         
-        if (variability.parameterOrLess()) {
-            myFV().variabilityPropagation(propagator, variability, bexp);
-        }
-        
+        myFV().variabilityPropagation(propagator, variability, bexp);
         taggedByVProp = variability.knownParameterOrLess();
         return taggedByVProp;
     }
@@ -515,6 +512,10 @@ aspect VariabilityPropagation {
             FClass.VariabilityPropagator propagator,
             TypePrefixVariability variability,
             FExp bindingExp) {
+        
+        if (variability().lessOrEqual(variability)) {
+            return;
+        }
         
         FAbstractEquation equation;
         boolean isZero = variability.constantVariability() && bindingExp.isLiteralZero();

branches/dev-jk-initial-vp/Compiler/ModelicaMiddleEnd/test/modelica/VariabilityPropagationInitialTests.mo
@@ -900,4 +900,55 @@ end VariabilityPropagationInitialTests.FailedInitialPartialEval1;
 ")})));
 end FailedInitialPartialEval1;
 
+model InitialSystemPropagatePartial1
+    function f
+        input Real x1;
+        input Real x2;
+        output Real y1 = x1 + 1;
+        output Real y2 = x2 + 1;
+        algorithm
+        annotation(Inline=false);
+    end f;
+    
+    parameter Real p1 = 1;
+    parameter Real p2(fixed=false);
+    parameter Real p3(fixed=false);
+    parameter Real x1(fixed=false);
+initial equation
+    (p1,p2) = f(x1, p3);
+    p3 = p1;
+    annotation(__JModelica(UnitTesting(tests={
+        TransformCanonicalTestCase(
+            name="InitialSystemPropagatePartial1",
+            description="Test infinite loop bug due to partial propagation in initial system",
+            variability_propagation_initial=true,
+            eliminate_alias_variables=false,
+            flatModel="
+fclass VariabilityPropagationInitialTests.InitialSystemPropagatePartial1
+ parameter Real p1 = 1 /* 1 */;
+ initial parameter Real p2(fixed = false);
+ parameter Real p3(fixed = true);
+ initial parameter Real x1(fixed = false);
+initial equation
+ (p1, p2) = VariabilityPropagationInitialTests.InitialSystemPropagatePartial1.f(x1, p3);
+parameter equation
+ p3 = p1;
+
+public
+ function VariabilityPropagationInitialTests.InitialSystemPropagatePartial1.f
+  input Real x1;
+  input Real x2;
+  output Real y1;
+  output Real y2;
+ algorithm
+  y1 := x1 + 1;
+  y2 := x2 + 1;
+  return;
+ annotation(Inline = false);
+ end VariabilityPropagationInitialTests.InitialSystemPropagatePartial1.f;
+
+end VariabilityPropagationInitialTests.InitialSystemPropagatePartial1;
+")})));
+end InitialSystemPropagatePartial1;
+
 end VariabilityPropagationInitialTests;

commit 35110b8eddebaf8ef73f3fb3d06b784d18021226

#5515 Fixed bug introduced in r12038 where discrete variability was propagated under some circumstances.

branches/dev-jk-initial-vp/Compiler/ModelicaMiddleEnd/src/jastadd/optimizations/VariabilityPropagation.jrag
@@ -513,7 +513,7 @@ aspect VariabilityPropagation {
             TypePrefixVariability variability,
             FExp bindingExp) {
         
-        if (variability().lessOrEqual(variability)) {
+        if (variability().lessOrEqual(variability) || !variability.parameterOrLess()) {
             return;
         }
         

branches/dev-jk-initial-vp/Compiler/ModelicaMiddleEnd/test/modelica/VariabilityPropagationPartialTests.mo
@@ -972,4 +972,45 @@ end VariabilityPropagationPartialTests.PartiallyKnownComposite12;
 ")})));
     end PartiallyKnownComposite12;
 
+    model PartiallyKnownDiscrete1
+       function f
+           input Real x1,x2,x3;
+           output Real y1,y2;
+       algorithm
+           y1 := x1;
+           y2 := x2;
+           annotation(Inline=false);
+       end f;
+    
+        Real y1,y2,y4;
+        Integer y3;
+    equation
+        (y1,y2) = f(1,y3,y4);
+        when time > 1 then
+            y4 = time;
+        end when;
+        y3 = 2;
+
+    annotation(__JModelica(UnitTesting(tests={
+        TransformCanonicalTestCase(
+            name="PartiallyKnownDiscrete1",
+            description="Partial evaluation of discrete function call",
+            flatModel="
+fclass VariabilityPropagationPartialTests.PartiallyKnownDiscrete1
+ constant Real y1 = 1;
+ constant Real y2 = 2;
+ discrete Real y4;
+ constant Integer y3 = 2;
+ discrete Boolean temp_1;
+initial equation
+ pre(y4) = 0.0;
+ pre(temp_1) = false;
+equation
+ temp_1 = time > 1;
+ y4 = if temp_1 and not pre(temp_1) then time else pre(y4);
+end VariabilityPropagationPartialTests.PartiallyKnownDiscrete1;
+")})));
+    end PartiallyKnownDiscrete1;
+
+
 end VariabilityPropagationPartialTests;