012874e8c3cd81ae898f6e3b3d5531a458a340df
jmattsson <jmattsson@7ae51c65-13d5-4921-9d80-6511ddd1419c>
2019-06-10 13:59:28 +0000

    Merged branch dev-1663 to trunk. (#5752)

--- trunk/Compiler/ModelicaFrontEnd/src/java/org/jmodelica/util/collections/ParallelIterable.java
+++ trunk/Compiler/ModelicaFrontEnd/src/java/org/jmodelica/util/collections/ParallelIterable.java
@@ -40,6 +40,7 @@ public class ParallelIterable<T> implements Iterable<T[]> {
 	}
 
     public Iterator<T[]> iterator() {
+        @SuppressWarnings("unchecked")
         Iterator<? extends T>[] iterators = new Iterator[iters.length];
         for (int i = 0; i < iters.length; i++)
             iterators[i] = iters[i].iterator();
