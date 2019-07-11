e1c4c9c06cb629fd6e0c12a1094c5fe14f02ff41
tgutzmann <tgutzmann@7ae51c65-13d5-4921-9d80-6511ddd1419c>
2019-07-11 20:20:54 +0000

    Merge from trunk

--- branches/dev-tg-5804/Compiler/ModelicaFrontEnd/src/java/org/jmodelica/common/ast/prefixes/TypePrefixInputOutput.java
+++ branches/dev-tg-5804/Compiler/ModelicaFrontEnd/src/java/org/jmodelica/common/ast/prefixes/TypePrefixInputOutput.java
@@ -1,10 +1,15 @@
 package org.jmodelica.common.ast.prefixes;
 
+// TODO Rename class
 public enum TypePrefixInputOutput {
 
     NONE(""),
     INPUT("input"),
-    OUTPUT("output");
+    OUTPUT("output"),
+    POTENTIAL("potential"),
+    FLOW("flow"),
+    STREAM("stream"),
+    INSTREAM("inStream");
     
     private String toString;
     
@@ -17,11 +22,15 @@ public enum TypePrefixInputOutput {
     }
     
     public boolean inputCausality() {
-        return this == INPUT;
+        return this == INPUT || this == FLOW || this == INSTREAM;
     }
     
     public boolean outputCausality() {
-        return this == OUTPUT;
+        return this == OUTPUT || this == POTENTIAL || this == STREAM;
+    }
+    
+    public boolean isConnectorPrefix() {
+        return this == POTENTIAL || this == FLOW || this == STREAM || this == INSTREAM;
     }
     
     @Override
