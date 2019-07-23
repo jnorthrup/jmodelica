c271b439edd5c17849dc2939073bf23a130fd863
jkampe <jkampe@7ae51c65-13d5-4921-9d80-6511ddd1419c>
2019-07-23 09:14:45 +0000

    #5810 Recommiting r13203,r13193,r13174 with bugfix from r13219

--- trunk/Python/src/pymodelica/compiler_interface.py
+++ trunk/Python/src/pymodelica/compiler_interface.py
@@ -33,7 +33,7 @@ if not jpype.isJVMStarted():
     _jvm_class_path = pym.environ['COMPILER_JARS']
     _jvm_ext_dirs = pym.environ['BEAVER_PATH']
     jpype.startJVM(pym.environ['JPYPE_JVM'], 
-        '-Djava.class.path=%s' % os.pathsep.join([_jvm_class_path,os.path.join(_jvm_ext_dirs, 'beaver-rt.jar')]),
+        '-Djava.class.path=%s' % os.pathsep.join([_jvm_class_path]),
         *_jvm_args)
     org = jpype.JPackage('org')
     print "JVM started."
