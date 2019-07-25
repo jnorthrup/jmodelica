56b2a95d2c243330874d47c7b1b170a34e55ac45
joqvist <joqvist@7ae51c65-13d5-4921-9d80-6511ddd1419c>
2019-07-25 12:18:05 +0000

    #5812 Improve Java exception handling in CasADi interface (SWIG)

--- ModelicaCasADiInterface/src/jcc/jccutils.cpp
+++ ModelicaCasADiInterface/src/jcc/jccutils.cpp
@@ -7,14 +7,44 @@
 
 using namespace java::lang;
 
-void describeAndClearJavaException(JavaError e) {
-    std::cout << "Java error occurred: " << std::endl;
-    describeJavaException(); clearJavaException();
+// Returns the stack trace of a Java exception.
+static std::string getStackTraceString(jthrowable exception) {
+    jclass swClass = vm_env->FindClass("java/io/StringWriter");
+    jmethodID swConstructor = vm_env->GetMethodID(swClass, "<init>", "()V");
+    jobject sw = vm_env->NewObject(swClass, swConstructor);
+    jclass pwClass = vm_env->FindClass("java/io/PrintWriter");
+    jmethodID pwConstructor = vm_env->GetMethodID(pwClass, "<init>", "(Ljava/io/Writer;)V");
+    jobject pw = vm_env->NewObject(pwClass, pwConstructor, sw);
+	jclass exClass = vm_env->GetObjectClass(exception);
+	jmethodID printStackTrace = vm_env->GetMethodID(exClass, "printStackTrace", "(Ljava/io/PrintWriter;)V");
+	vm_env->CallObjectMethod(exception, printStackTrace, pw);
+	jmethodID toString = vm_env->GetMethodID(swClass, "toString", "()Ljava/lang/String;");
+	jstring message = (jstring) vm_env->CallObjectMethod(sw, toString);
+	const char *mstr = vm_env->GetStringUTFChars(message, NULL);
+    std::string trace = mstr;
+    vm_env->ReleaseStringUTFChars(message, mstr);
+    vm_env->DeleteLocalRef(pw);
+    vm_env->DeleteLocalRef(pwClass);
+    vm_env->DeleteLocalRef(sw);
+    vm_env->DeleteLocalRef(swClass);
+    vm_env->DeleteLocalRef(message);
+    vm_env->DeleteLocalRef(exClass);
+    return trace;
+}
+
+std::string describeAndClearJavaException(JavaError e) {
+    jthrowable ex = vm_env->ExceptionOccurred();
+    if (ex == NULL) {
+        return "unknown Java exception (already cleared?)";
+    }
+    vm_env->ExceptionClear();
+    std::string message = getStackTraceString(ex);
+    vm_env->DeleteLocalRef(ex);
+    return message;
 }
 
 void rethrowJavaException(JavaError e) {
-    describeAndClearJavaException(e);
-    throw std::runtime_error("a java error occurred; details were printed");
+    throw std::runtime_error(describeAndClearJavaException(e));
 }
 
 jstring fromUTF(const char *bytes) {
