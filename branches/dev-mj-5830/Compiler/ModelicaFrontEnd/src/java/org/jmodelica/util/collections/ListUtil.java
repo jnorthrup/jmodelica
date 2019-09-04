b726cae5dc2b58383aad8a3aca2c3c57fe23f956
mjohnsson <mjohnsson@7ae51c65-13d5-4921-9d80-6511ddd1419c>
2019-09-04 08:22:02 +0000

    #5830 Change up with trunk. + Changelog

--- branches/dev-mj-5830/Compiler/ModelicaFrontEnd/src/java/org/jmodelica/util/collections/ListUtil.java
+++ branches/dev-mj-5830/Compiler/ModelicaFrontEnd/src/java/org/jmodelica/util/collections/ListUtil.java
@@ -2,6 +2,7 @@ package org.jmodelica.util.collections;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
 
@@ -100,12 +101,23 @@ public final class ListUtil {
      */
     @SafeVarargs
     public static <T> ArrayList<T> concatenate(List<T>... lists) {
+        return buildList(lists);
+    }
+    
+    /**
+     * Create a new list that is the concatenation of the given collections, in the order given.
+     * 
+     * @param collections  any number of collections to concatenate
+     * @return       a newly created ArrayList that contains the concatenation of the given collections
+     */
+    @SafeVarargs
+    public static <T> ArrayList<T> buildList(Collection<T>... collections) {
         int size = 0;
-        for (List<T> list : lists) {
+        for (Collection<T> list : collections) {
             size += list.size();
         }
         ArrayList<T> res = new ArrayList<T>(size);
-        for (List<T> list : lists) {
+        for (Collection<T> list : collections) {
             res.addAll(list);
         }
         return res;
