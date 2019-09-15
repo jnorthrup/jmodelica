169628116888d13f5b7c0ae8bfdb291ef545e2ec
chria <chria@7ae51c65-13d5-4921-9d80-6511ddd1419c>
2019-09-15 11:12:24 +0000

    Added a test that verifies that the signature check is working. Related to ticket:5837

--- Python/src/tests_jmodelica/files/Modelica/Resources/Include/addNumbers.h
+++ Python/src/tests_jmodelica/files/Modelica/Resources/Include/addNumbers.h
@@ -9,6 +9,7 @@
 
 DllExport double add(double a, double b);
 DllExport void add_output(double a, double b, double *c);
+DllExport double multiple_add(double a, double b, double c, double d, double e, double f, double g, double h, double i);
 void multiplyAnArray(int* inputs, int* outputs, int size, int m);
 
 #endif
