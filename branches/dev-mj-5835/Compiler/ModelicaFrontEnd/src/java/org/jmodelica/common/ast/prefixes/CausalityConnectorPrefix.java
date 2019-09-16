296de22cf3f220a08a9a8a61b1fa6fe29d714082
mjohnsson <mjohnsson@7ae51c65-13d5-4921-9d80-6511ddd1419c>
2019-09-16 12:56:31 +0000

    #5835 Dev branch for Jenkins test.

---
+++ branches/dev-mj-5835/Compiler/ModelicaFrontEnd/src/java/org/jmodelica/common/ast/prefixes/CausalityConnectorPrefix.java
@@ -0,0 +1,70 @@
+package org.jmodelica.common.ast.prefixes;
+
+// TODO Rename class
+public enum CausalityConnectorPrefix {
+
+    NONE(""),
+    INPUT("input"),
+    OUTPUT("output"),
+    POTENTIAL("potential"),
+    FLOW("flow"),
+    STREAM("stream"),
+    INSTREAM("inStream");
+    
+    private String toString;
+    
+    private CausalityConnectorPrefix(String toString) {
+        this.toString = toString;
+    }
+    
+    public boolean isNone() {
+        return this == NONE;
+    }
+    
+    public boolean isStream() {
+        return this == STREAM;
+    }
+    
+    public interface CausalPortsEvaluator {
+        boolean useCausalPorts();
+    }
+    
+    /**
+     * We use a {@link CausalPortsEvaluator} here instead of an evaluated boolean
+     * to reduce the number of calls to useCausalPorts().
+     * Replace with a Predicate when updating to Java 8.
+     */
+    public boolean inputCausality(CausalPortsEvaluator evaluator) {
+        if (this == INPUT) {
+            return true;
+        }
+        if (this == FLOW || this == INSTREAM) {
+            return evaluator.useCausalPorts();
+        }
+        return false;
+    }
+    
+    public boolean outputCausality(CausalPortsEvaluator evaluator) {
+        if (this == OUTPUT) {
+            return true;
+        }
+        if (this == POTENTIAL || this == STREAM) {
+            return evaluator.useCausalPorts();
+        }
+        return false;
+    }
+    
+    public boolean isConnectorPrefix() {
+        return this == POTENTIAL || this == FLOW || this == STREAM || this == INSTREAM;
+    }
+    
+    @Override
+    public String toString() {
+        return toString;
+    }
+    
+    public VisibilityType functionVisibility() {
+        return isNone() ? VisibilityType.PROTECTED : VisibilityType.PUBLIC;
+    }
+    
+}
