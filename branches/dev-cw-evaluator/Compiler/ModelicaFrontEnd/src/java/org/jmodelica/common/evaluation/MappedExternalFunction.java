a42d015903413ac4752ff8d07ccf73e47b9916a6
jkampe <jkampe@7ae51c65-13d5-4921-9d80-6511ddd1419c>
2019-10-01 13:27:12 +0000

    #5837 Moved some classes

--- branches/dev-cw-evaluator/Compiler/ModelicaFrontEnd/src/java/org/jmodelica/common/evaluation/MappedExternalFunction.java
+++ branches/dev-cw-evaluator/Compiler/ModelicaFrontEnd/src/java/org/jmodelica/common/evaluation/MappedExternalFunction.java
@@ -17,9 +17,9 @@ import org.jmodelica.util.values.ConstantEvaluationException;
  */
 class MappedExternalFunction<K extends Variable<V, T>, V extends Value, T extends Type<V>, E extends External<K>> extends ExternalFunctionImpl<K,V,T,E> {
 
-    private Map<String, ExternalFunction<K, V>> lives = new HashMap<>();
+    private final Map<String, ExternalFunction<K, V>> lives = new HashMap<>();
 
-    private LinkedHashSet<ExternalFunction<K, V>> livingCachedExternals;
+    private final LinkedHashSet<ExternalFunction<K, V>> livingCachedExternals;
     private final int externalConstantEvaluationMaxProc;
 
     public MappedExternalFunction(ExternalFunctionCompiler<K,E> compiler, External<K> ext, ExternalFunctionExecutable extFunctionExecutable, LinkedHashSet<ExternalFunction<K, V>> livingCachedExternals) {
@@ -38,7 +38,7 @@ class MappedExternalFunction<K extends Variable<V, T>, V extends Value, T extend
         String name = cvd == null ? "" : cvd.ceval().getMarkedExternalObject();
         ExternalFunction<K, V> ef = lives.get(name);
         if (ef == null) {
-            LiveExternalFunction lef = new LiveExternalFunction();
+            LiveExternalFunction<K,V,T,E> lef = new LiveExternalFunction<>(compiler, this, livingCachedExternals, externalConstantEvaluationMaxProc);
             try {
                 lef.ready(ext, values, timeout);
                 ef = lef;
@@ -67,84 +67,4 @@ class MappedExternalFunction<K extends Variable<V, T>, V extends Value, T extend
         lives.clear();
     }
 
-    /**
-     * Represents a (possible) living external function process.
-     */
-    private class LiveExternalFunction implements ExternalFunction<K, V> {
-
-        protected ProcessCommunicator<V, T> com;
-
-        public LiveExternalFunction() {
-            super();
-        }
-
-        @Override
-        public String getMessage() {
-            return MappedExternalFunction.this.getMessage();
-        }
-
-        @Override
-        public int evaluate(External<K> ext, Map<K, V> values, int timeout) throws IOException {
-            compiler.log().debug("Evaluating live external function: " + ext.getName());
-            try {
-                ready(ext, values, timeout);
-                long time = System.currentTimeMillis();
-                MappedExternalFunction.this.evaluate(ext, values, timeout, com);
-                time = System.currentTimeMillis() - time;
-                compiler.log().debug("Finished evaluating live external function, time: " + time + "ms");
-            } catch (ProcessCommunicator.AbortConstantEvaluationException e) {
-
-            } catch (ConstantEvaluationException e) {
-                destroyProcess();
-                throw e;
-            } catch (IOException e) {
-                destroyProcess();
-                throw e;
-            }
-            return 0;
-        }
-
-        /**
-         * Make sure process is ready for evaluation call.
-         */
-        protected void ready(External<K> ext, Map<K, V> values, int timeout) throws IOException {
-            if (com == null) {
-                long time1 = System.currentTimeMillis();
-                // Start process if not live.
-                com = createProcessCommunicator();
-                long time2 = System.currentTimeMillis();
-                // Send external object constructor inputs
-                MappedExternalFunction.this.setup(ext, values, timeout, com);
-                long time3 = System.currentTimeMillis();
-                compiler.log().debug("Setup live external function: " + ext.getName()
-                          + ", createProcessCommunicator() time: " + (time2 - time1)
-                          + "ms, setup time: " + (time3 - time2) + "ms");
-            }
-
-            // Mark as most recently used
-            livingCachedExternals.remove(this);
-            livingCachedExternals.add(this);
-
-            // If we are over the allowed number of cached processes
-            // we kill the least recently used.
-            if (livingCachedExternals.size() > externalConstantEvaluationMaxProc) {
-                livingCachedExternals.iterator().next().destroyProcess();
-            }
-        }
-
-        @Override
-        public void destroyProcess() {
-            if (com != null) {
-                livingCachedExternals.remove(this);
-                com.destroy();
-                com = null;
-            }
-        }
-
-        @Override
-        public void remove() {
-            // Removing this executable is handled by surrounding MappedExternalFunction
-            throw new UnsupportedOperationException();
-        }
-    }
 }
