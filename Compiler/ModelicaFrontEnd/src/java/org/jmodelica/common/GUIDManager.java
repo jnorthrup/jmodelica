f2053914981eef4d8b64065e5f3c230eaa000b03
molsson <molsson@7ae51c65-13d5-4921-9d80-6511ddd1419c>
2019-10-11 09:16:18 +0000

    Merged revision(s) 13673-13711 from branches/dev-mo-2265: #5843 Replaced some uses of File with Path. ........ #5843 Lookup can now find libraries with incorrect file name casing. ........ #5843 Added some methods to FileUtil.java. ........ #5843 Extracted calls to templates.generate*Files() to a separate method. ........ #5843 Moved extracted computation of flatName and moved it after instantiation. ........ #5843 Fixed CasADi build error after r13686. ........ #5843 Refactored ModelicaCompiler.packUnit(). ........ #5843 Fixed NullPointerException when compile target is null. Replaced some File objects with Path. ........

--- Compiler/ModelicaFrontEnd/src/java/org/jmodelica/common/GUIDManager.java
+++ Compiler/ModelicaFrontEnd/src/java/org/jmodelica/common/GUIDManager.java
@@ -1,19 +1,18 @@
 package org.jmodelica.common;
 
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
+import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.FileWriter;
 import java.io.IOException;
-import java.io.OutputStreamWriter;
-import java.io.Reader;
-import java.io.StringReader;
-import java.io.Writer;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.PrintWriter;
 import java.math.BigInteger;
 import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.text.SimpleDateFormat;
@@ -64,6 +63,10 @@ public class GUIDManager {
     }
 
     public void setSourceFile(File source) {
+        this.source = new FileOpenable(source.toPath());
+    }
+
+    public void setSourceFile(Path source) {
         this.source = new FileOpenable(source);
     }
 
@@ -72,10 +75,14 @@ public class GUIDManager {
     }
 
     public void addDependentFile(File dependentFile) {
+        dependentFiles.add(new FileOpenable(dependentFile.toPath()));
+    }
+
+    public void addDependentFile(Path dependentFile) {
         dependentFiles.add(new FileOpenable(dependentFile));
     }
 
-    public void addDependentString(String input, StringBuilder output) {
+    public void addDependentString(String input, ByteArrayOutputStream output) {
         dependentFiles.add(new StringOpenable(input, output));
     }
 
@@ -84,7 +91,7 @@ public class GUIDManager {
         try {
             final MessageDigest md5 = MessageDigest.getInstance("MD5");
 
-            try (final BufferedReader reader = new BufferedReader(source.openInput())) {
+            try (final BufferedReader reader = new BufferedReader(new InputStreamReader(source.openInput()))) {
                 String line = reader.readLine();
                 while (line != null) {
                     // A naive implementation that is expected to create a digest different from what a command
@@ -117,13 +124,11 @@ public class GUIDManager {
         
         for (final Openable openable : dependentFiles) {
             try {
-                ByteArrayOutputStream os = new ByteArrayOutputStream();
-                try (final Writer tmp = new OutputStreamWriter(os)) {
-                    processFiles(openable.openInput(), tmp);
-                }
-
-                try (BufferedWriter writer = new BufferedWriter(openable.openOutput())) {
-                    writer.append(os.toString());
+                try (ByteArrayOutputStream os = new ByteArrayOutputStream()) {
+                    processFiles(openable.openInput(), os);
+                    try (PrintWriter writer = new PrintWriter(openable.openOutput())) {
+                        writer.append(os.toString());
+                    }
                 }
             } catch (IOException e) {
                 throw new RuntimeException(e);
@@ -131,9 +136,9 @@ public class GUIDManager {
         }
     }
 
-    private void processFiles(Reader source, Writer destination) throws IOException {
-        try (final BufferedReader reader = new BufferedReader(source);
-                final BufferedWriter writer = new BufferedWriter(destination)) {
+    private void processFiles(InputStream source, OutputStream destination) throws IOException {
+        try (final BufferedReader reader = new BufferedReader(new InputStreamReader(source));
+             final PrintWriter writer = new PrintWriter(destination)) {
 
             String line =  reader.readLine();
             while (line != null) {
@@ -199,30 +204,30 @@ public class GUIDManager {
     }
 
     private static interface Openable {
-        public Reader openInput();
-        public Writer openOutput();
+        public InputStream openInput();
+        public OutputStream openOutput();
     }
     
-    private static class FileOpenable implements Openable{
-        private File file;
+    private static class FileOpenable implements Openable {
+        private Path path;
         
-        public FileOpenable(File file) {
-            this.file = file;
+        public FileOpenable(Path file) {
+            this.path = file;
         }
         
         @Override
-        public Reader openInput() {
+        public InputStream openInput() {
             try {
-                return new FileReader(file);
-            } catch (FileNotFoundException e) {
+                return Files.newInputStream(path);
+            } catch (IOException e) {
                 throw new RuntimeException(e);
             }
         }
         
         @Override
-        public Writer openOutput() {
+        public OutputStream openOutput() {
             try {
-                return new FileWriter(file);
+                return Files.newOutputStream(path);
             } catch (IOException e) {
                 throw new RuntimeException(e);
             }
@@ -230,44 +235,28 @@ public class GUIDManager {
         
         @Override
         public String toString() {
-            return file.toString();
+            return path.toString();
         }
     }
     
     private static class StringOpenable implements Openable {
         
         private String input;
-        private StringBuilder output;
+        private ByteArrayOutputStream output;
         
-        public StringOpenable(String input, StringBuilder output) {
+        public StringOpenable(String input, ByteArrayOutputStream output) {
             this.input = input;
             this.output = output;
         }
         
         @Override
-        public Reader openInput() {
-            return new StringReader(input);
+        public InputStream openInput() {
+            return new ByteArrayInputStream(input.getBytes());
         }
         
         @Override
-        public Writer openOutput() {
-            return new Writer() {
-                
-                @Override
-                public void write(char[] cbuf, int off, int len) throws IOException {
-                    output.append(cbuf, off, len);
-                }
-                
-                @Override
-                public void flush() throws IOException {
-                    // Do nothing
-                }
-                
-                @Override
-                public void close() throws IOException {
-                    // Do nothing
-                }
-            };
+        public OutputStream openOutput() {
+            return output;
         }
     }
 }
