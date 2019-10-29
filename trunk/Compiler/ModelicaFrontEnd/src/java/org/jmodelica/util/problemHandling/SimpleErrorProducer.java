c3f189885c031db7e078949e723fc076a8cc9fba
jmattsson <jmattsson@7ae51c65-13d5-4921-9d80-6511ddd1419c>
2019-10-29 12:11:49 +0000

    Merged to trunk. (#5835)

--- trunk/Compiler/ModelicaFrontEnd/src/java/org/jmodelica/util/problemHandling/SimpleErrorProducer.java
+++ trunk/Compiler/ModelicaFrontEnd/src/java/org/jmodelica/util/problemHandling/SimpleErrorProducer.java
@@ -5,7 +5,7 @@ import org.jmodelica.api.problemHandling.ProblemSeverity;
 
 /**
  * If you have a error message which only consists of a string and format
- * arguments, then this is the right class for you. Simple create a static
+ * arguments, then this is the right class for you. Simply create a static
  * final field instantiating this class and then invoke the error as needed!
  */
 public class SimpleErrorProducer extends SimpleProblemProducer {
