8b0deb0b6e555b2981c887cb18cc43fff25621a8
jkampe <jkampe@7ae51c65-13d5-4921-9d80-6511ddd1419c>
2019-11-06 07:37:28 +0000

    AUTO creating branch branches/dev-jk-2396

---
+++ branches/dev-jk-2396/Compiler/ModelicaFrontEnd/src/java/org/jmodelica/common/evaluation/ExternalFunctionCompiler.java
@@ -0,0 +1,14 @@
+package org.jmodelica.common.evaluation;
+
+import java.io.FileNotFoundException;
+
+import org.jmodelica.common.LogContainer;
+import org.jmodelica.common.evaluation.ExternalProcessMultiCache.External;
+import org.jmodelica.util.ccompiler.CCompilerDelegator;
+import org.jmodelica.util.exceptions.CcodeCompilationException;
+
+public interface ExternalFunctionCompiler<K, E extends External<K>> extends LogContainer {
+    public String compileExternal(E ext) throws FileNotFoundException, CcodeCompilationException;
+
+    public CCompilerDelegator getCCompiler();
+}
