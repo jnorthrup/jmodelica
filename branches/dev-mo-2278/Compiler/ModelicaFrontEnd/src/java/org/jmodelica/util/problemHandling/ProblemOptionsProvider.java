2337a406bd2863b8b5b326b42778182619e6cd3d
molsson <molsson@7ae51c65-13d5-4921-9d80-6511ddd1419c>
2019-11-08 09:17:40 +0000

    AUTO deleting branch branches/dev-mo-2278

--- branches/dev-mo-2278/Compiler/ModelicaFrontEnd/src/java/org/jmodelica/util/problemHandling/ProblemOptionsProvider.java
+++
@@ -1,8 +0,0 @@
-package org.jmodelica.util.problemHandling;
-
-import org.jmodelica.common.options.AbstractOptionRegistry;
-
-public interface ProblemOptionsProvider {
-    public AbstractOptionRegistry getOptionRegistry();
-    public boolean filterThisWarning(String identifier);
-}
