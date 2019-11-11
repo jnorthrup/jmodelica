f7c70bc4d562067c1a5ba4ced746a44638d10858
randersson <randersson@7ae51c65-13d5-4921-9d80-6511ddd1419c>
2019-11-11 14:59:08 +0000

    #5819 Updated groovy script with some changes in order to build all the different versions

--- external/build_externals/Jenkins/ci/vars/buildAssimulo.groovy
+++ external/build_externals/Jenkins/ci/vars/buildAssimulo.groovy
@@ -24,28 +24,30 @@ def call(JM_CHECKOUT_PATH, JM_BRANCH, INSTALL_PATH, TARGET, bitness=["32", "64"]
         msvs_suffix=""
     }
     for (bit in bitness) {
-        stage ("assimulo_${TARGET} ${bit}${msvs_suffix} bit") {
+        stage ("assimulo_${TARGET} ${bit}${msvs_suffix}") {
             runMSYSWithEnv("""\
             export JM_HOME="\$(pwd)/JModelica/"
             JENKINS_BUILD_DIR="\$(pwd)/build"
             cd \${JM_HOME}/external/build_externals/build/assimulo
             
             make clean USER_CONFIG=${USER_CONFIG} BUILD_DIR=\${JENKINS_BUILD_DIR}
-            make ${TARGET} USER_CONFIG=${USER_CONFIG} JM_HOME=\${JM_HOME} BUILD_DIR=\${JENKINS_BUILD_DIR} WORKSPACE=\${JM_HOME}/.. INSTALL_DIR_FOLDER=${INSTALL_PATH_UNIX}/assimulo/${TARGET}/Python_${bit}
+            make ${TARGET} USER_CONFIG=${USER_CONFIG} JM_HOME=\${JM_HOME} BUILD_DIR=\${JENKINS_BUILD_DIR} WORKSPACE=\${JM_HOME}/.. INSTALL_DIR_FOLDER=${INSTALL_PATH_UNIX}/${msvs_suffix}
             """, extra_bat);
             if ("${TARGET}" == "folder") {
                 runMSYSWithEnv("""\
                 export JM_HOME="\$(pwd)/JModelica/"
-                nosetests ${INSTALL_PATH_UNIX}/assimulo/${TARGET}/Python_${bit}/folder/assimulo/tests/*.py
+                nosetests ${INSTALL_PATH_UNIX}/assimulo/tests/*.py
                 """, "", false, bit); 
             }
             if (stash || archive) {
-                dir("${INSTALL_PATH}/assimulo/${TARGET}") {
+                dir("${INSTALL_PATH}") {
                     if (stash) {
                         stash includes: "Python_${bit}/**", name: "Python_${bit}_assimulo_${TARGET}"
                     }
-                    if (archive) {
-                        archiveArtifacts artifacts: "Python_${bit}/**", fingerprint: false
+                    if (msvs_suffix == "msvs") {
+                        archiveArtifacts artifacts: "${msvs_suffix}/assimulo/**", fingerprint: false
+                    } else {
+                        archiveArtifacts artifacts: "assimulo/**", fingerprint: false
                     }
                 }
             }
