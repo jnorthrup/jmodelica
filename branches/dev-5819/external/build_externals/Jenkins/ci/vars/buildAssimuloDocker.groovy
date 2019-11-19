8383bac3f9df362fca30c257b85e2615482ae267
randersson <randersson@7ae51c65-13d5-4921-9d80-6511ddd1419c>
2019-11-19 15:10:58 +0000

    #5819 Fixed editing of string in order to have correct upload pattern

--- branches/dev-5819/external/build_externals/Jenkins/ci/vars/buildAssimuloDocker.groovy
+++ branches/dev-5819/external/build_externals/Jenkins/ci/vars/buildAssimuloDocker.groovy
@@ -10,6 +10,14 @@ def call(PLATFORM, TARGET, USER_CONFIG) {
                 sh "make docker_install_${TARGET} ${MAKE_ARGS}"
                 dir("${WORKSPACE}/JModelica") {
                     artifact_list = sh returnStdout: true, script: "cat ${ARTIFACT_FILE}"
+                    if (TARGET == "wheel") {
+                        // need to remove the trailing **\n from the string and append *.whl
+                        artifact_pattern = artifact_list.replace("*", "")
+                        artifact_pattern = artifact_pattern.replace("\n", "")
+                        artifact_pattern = artifact_pattern + "*.whl"
+                        echo "artifact_pattern = ${artifact_pattern}"
+                        uploadToArtifactory(artifact_pattern, "externals/assimulo/release/3.1/unix/${PLATFORM}/", false, "true")
+                    }
                     archiveArtifacts artifacts: artifact_list, fingerprint: false
                     sh "rm ${ARTIFACT_FILE}"
                 }
