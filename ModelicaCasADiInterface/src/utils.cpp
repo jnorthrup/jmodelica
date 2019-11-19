d533e22144c8fd47ff41258d70de34ac29a599ff
randersson <randersson@7ae51c65-13d5-4921-9d80-6511ddd1419c>
2019-11-19 08:18:56 +0000

    #5819 Fixed issue with case sensitivity

--- ModelicaCasADiInterface/src/utils.cpp
+++ ModelicaCasADiInterface/src/utils.cpp
@@ -24,7 +24,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #include <string>
 #include "JCCEnv.h"
 
-#include "Utils.hpp"
+#include "utils.hpp"
 
 char *jobjectoToString(jobject obj)
 {
