5f2485229f004cbc8ccd46a1740148f22fa3bc28
jmattsson <jmattsson@7ae51c65-13d5-4921-9d80-6511ddd1419c>
2019-11-20 13:05:44 +0000

    Changed serialization of real return values in constant evaluation of external C functions to use 17 significant digits. (#5881)

--- branches/dev-jm-2710/RuntimeLibrary/src/evaluator/jmi_evaluator_util.h
+++ branches/dev-jm-2710/RuntimeLibrary/src/evaluator/jmi_evaluator_util.h
@@ -62,7 +62,7 @@
 #define JMCEVAL_DBGP(x) if (JMCEVAL_DEBUG) { printf(x); fflush(stdout);}
 
 /* Format specifier when printing jmi_real_t */
-#define JMCEVAL_realFormat "%.16f"
+#define JMCEVAL_realFormat "%.17f"
 
 /* Parses ND dimensions into dimension buffer d*/
 #define JMCEVAL_parseArrayDims(ND) \
