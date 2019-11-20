c84d2681d096d011e3adcadb9ab0b2cb957dbb99
randersson <randersson@7ae51c65-13d5-4921-9d80-6511ddd1419c>
2019-11-20 08:10:30 +0000

    #5819 Added another clean of wheel from workspace before archiving in order to make sure it is removed

--- external/build_externals/Jenkins/ci/vars/buildAssimuloDocker.groovy
+++ external/build_externals/Jenkins/ci/vars/buildAssimuloDocker.groovy
@@ -1,9 +1,9 @@
-def call(PLATFORM, TARGET, USER_CONFIG) {
+def call(PLATFORM, TARGET, USER_CONFIG, ASSIMULO_VERSION) {
     stage("Assimulo ${TARGET} ${PLATFORM}") {
         
         def ARTIFACT_FILE="${WORKSPACE}/JModelica/artifacts_assimulo"
         def MAKE_ARGS="JM_HOME=${WORKSPACE}/JModelica PATH_TO_MOUNT=${WORKSPACE}/JModelica USER_CONFIG=${USER_CONFIG} ARTIFACT_FILE=${ARTIFACT_FILE}"
-        
+        def artifact_pattern = ""
         dir ('JModelica/external/build_externals/docker/src/components/Assimulo') {
             try{
                 sh "make docker_${TARGET} ${MAKE_ARGS}"
@@ -16,7 +16,7 @@ def call(PLATFORM, TARGET, USER_CONFIG) {
                         artifact_pattern = artifact_pattern.replace("\n", "")
                         artifact_pattern = artifact_pattern + "*.whl"
                         echo "artifact_pattern = ${artifact_pattern}"
-                        uploadToArtifactory(artifact_pattern, "externals/assimulo/release/3.1/unix/${PLATFORM}/", false, "true")
+                        uploadToArtifactory(artifact_pattern, "externals/assimulo/release/${ASSIMULO_VERSION}/unix/${PLATFORM}/", false, "true")
                     }
                     archiveArtifacts artifacts: artifact_list, fingerprint: false
                     sh "rm ${ARTIFACT_FILE}"
@@ -24,6 +24,15 @@ def call(PLATFORM, TARGET, USER_CONFIG) {
                 sh "make docker_test_${TARGET} ${MAKE_ARGS}"
             } finally {
                 sh "make clean_in_docker ${MAKE_ARGS}"
+                if (TARGET == "wheel") {
+                    try {
+                        dir ("${WORKSPACE}/JModelica") {
+                            sh "rm -rfv ${artifact_pattern}"   
+                        }
+                    } catch (Exception e) {
+                        sh "DEBUG: Could not remove ${WORKSPACE}/JModelica/${artifact_pattern}"
+                    }
+                }
             }
         }
     }
